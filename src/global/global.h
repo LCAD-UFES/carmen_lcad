6986fb74933c0d994a426222948d30b2acb3cc3c
Ulysses Rangel Ribeiro <ulyssesrr@gmail.com>
2016-01-29 01:41:32 -0200

    Rudimentary scene graph transformations impl

--- src/global/global.h
+++ src/global/global.h
@@ -150,12 +150,12 @@ typedef struct {
 	double q3;
 } carmen_quaternion_t, *carmen_quaternion_3D_p;
 
-
+// Ulysses: Setar parent como NULL para retrocompatibilidade
 typedef struct {
 	carmen_vector_3D_t position;
 	carmen_orientation_3D_t orientation;
-} carmen_pose_3D_t, *carmen_pose_3D_p;
-
+	carmen_pose_3D_t *parent;
+} carmen_pose_3D_t { .parent = NULL }, *carmen_pose_3D_p;
 
 typedef struct {
 	int x, y;
