3e02f5a1bbb7acb0d459fe47f5630897854dc89e
vinibc <viniciusbc1@gmail.com>
2016-07-11 00:05:30 -0300

    Update tracker_opentld_main.cpp

--- src/tracker_opentld/tracker_opentld_main.cpp
+++ src/tracker_opentld/tracker_opentld_main.cpp
@@ -17,7 +17,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/legacy/legacy.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <carmen/fused_odometry_interface.h>
 
 //OPENTLD
 #include "TLD.h"
