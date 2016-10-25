30577a3ad9c1b7baa6b6a6fbc1461dee293e8f98
Thomas <thomasjteixeira@gmail.com>
2016-10-25 19:17:14 -0200

    /o/ fusão \o\ do navigator_spline com goturn_tracker. Versão beta

--- src/tracker_opentld/goturn/goturn_main.cpp
+++ src/tracker_opentld/goturn/goturn_main.cpp
@@ -4,15 +4,21 @@
 #include <list>
 #include <string>
 
+
 #include <carmen/carmen.h>
 #include <carmen/bumblebee_basic_interface.h>
 #include <carmen/bumblebee_basic_messages.h>
 #include <carmen/velodyne_interface.h>
+#include <carmen/stereo_util.h>
 #include <tf.h>
 
 #include <carmen/visual_tracker_interface.h>
 #include <carmen/visual_tracker_messages.h>
 #include <carmen/velodyne_camera_calibration.h>
+
+#include <carmen/rddf_interface.h>
+#include <carmen/rddf_messages.h>
+
 
 // OpenCV
 #include <opencv2/core/core.hpp>
@@ -25,6 +31,8 @@
 #include "tracker/tracker.h"
 #include "regressor/regressor_train.h"
 #include "gui.h"
+#include "spline.h"
+
 //using namespace std;
 
 //
@@ -90,6 +98,44 @@ struct Image_Box{
 int num_prev_frames = 2;
 
 std::vector<Image_Box> last_track;
+stereo_util camera_parameters;
+carmen_vector_3D_t trackerPoint;
+std::vector<carmen_localize_ackerman_globalpos_message>  localizeVector;
+
+
+
+carmen_vector_3D_t
+compute_3d_point(bounding_box box, double range)
+{
+	double xcenter = (box.x + box.width) / 2;
+	double ycenter = (box.y + box.height) / 2;
+
+	double hor_angle, vert_angle;
+
+	assert(camera_parameters.fx > 0);
+	assert(camera_parameters.fy > 0);
+	assert(range > 0 && range < 1000);
+
+	hor_angle = atan((xcenter - (((double) tld_image_width) / 2.0)) / camera_parameters.fx);
+	vert_angle = -atan((ycenter - (((double) tld_image_height) / 2.0)) / camera_parameters.fy);
+
+	double cos_rot_angle = cos(hor_angle);
+	double sin_rot_angle = sin(hor_angle);
+
+	double cos_vert_angle = cos(vert_angle);
+	double sin_vert_angle = sin(vert_angle);
+
+	double xy_distance = range * cos_vert_angle;
+
+	carmen_vector_3D_t point3d;
+
+	point3d.x = (xy_distance * cos_rot_angle);
+	point3d.y = (xy_distance * sin_rot_angle);
+	point3d.z = (range * sin_vert_angle);
+
+	return point3d;
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                           //
@@ -150,10 +196,9 @@ build_and_publish_message(char *host, double timestamp)
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 void
-calculate_box_average(BoundingBox &box, std::vector<Image_Box> &last_track, Mat* img, int step, int cont)
+calculate_box_average(BoundingBox &box, std::vector<Image_Box> &last_track, Mat* img, int cont)
 {
 	BoundingBox average_box;
-	int repeat = 0;
 	average_box.x1_ = 0.0;
 	average_box.x2_ = 0.0;
 	average_box.y1_ = 0.0;
@@ -163,10 +208,9 @@ calculate_box_average(BoundingBox &box, std::vector<Image_Box> &last_track, Mat*
 	average_box.x2_ += box.x2_;
 	average_box.y1_ += box.y1_;
 	average_box.y2_ += box.y2_;
-	int limit = step/last_track.size();
 
 //	for(int i = cont%last_track.size(); repeat < limit; i = (i + step)%last_track.size(), repeat++)
-	for(int i = 0; i < last_track.size(); i++)
+	for(unsigned int i = 0; i < last_track.size(); i++)
 	{
 		tracker.Init(last_track[i].prev_image, last_track[i].prev_box, &regressor);
 
@@ -196,11 +240,7 @@ process_goturn_detection(Mat *img, double time_stamp)
 {
 	char string1[128];
 	CvFont font;
-	bool recovery = false;
 	static int cont = 0;
-	static int first = 0;
-	int step = 30;
-
 
 	//cv::Mat mat_image=cvarrToMat(&img);
 	static Mat prev_image;
@@ -211,7 +251,7 @@ process_goturn_detection(Mat *img, double time_stamp)
 		tracker.Track(*img, &regressor, &box);
 		if (cont > num_prev_frames)
 		{
-//			calculate_box_average(box, last_track, img, step, cont);
+//			calculate_box_average(box, last_track, img, cont);
 //			recovery = true;
 
 		}
@@ -243,35 +283,48 @@ process_goturn_detection(Mat *img, double time_stamp)
 		mini_box.height = (box_1.height/3);
 		Scalar yellow = CV_RGB(255, 255, 0);
 		Scalar white = CV_RGB(255, 255, 255);
+		vector<double> median_ranges;
 
 		rectangle(*img, cv::Point(mini_box.x, mini_box.y),cv::Point(mini_box.x + mini_box.width, mini_box.y + mini_box.height), yellow, 1, 4 );
-		for(int i = 0; i < points_lasers_in_cam.size(); i++)
+		for(unsigned int i = 0; i < points_lasers_in_cam.size(); i++)
+		{
+			if((points_lasers_in_cam.at(i).ipx > box_1.x) && ( points_lasers_in_cam.at(i).ipx < (box_1.x + box_1.width)) &&
+					(points_lasers_in_cam.at(i).ipy > box_1.y) && (points_lasers_in_cam.at(i).ipy < (box_1.y + box_1.height)))
 			{
-				if((points_lasers_in_cam.at(i).ipx > box_1.x) && ( points_lasers_in_cam.at(i).ipx < (box_1.x + box_1.width)) &&
-						(points_lasers_in_cam.at(i).ipy > box_1.y) && (points_lasers_in_cam.at(i).ipy < (box_1.y + box_1.height)))
+				if (points_lasers_in_cam.at(i).laser_polar.length <= MIN_RANGE)
+					points_lasers_in_cam.at(i).laser_polar.length = MAX_RANGE;
+
+				if (points_lasers_in_cam.at(i).laser_polar.length > MAX_RANGE)
+					points_lasers_in_cam.at(i).laser_polar.length = MAX_RANGE;
+
+				median_ranges.push_back(points_lasers_in_cam.at(i).laser_polar.length);
+				circle(*img, cv::Point(points_lasers_in_cam.at(i).ipx, points_lasers_in_cam.at(i).ipy), 2, Scalar(0, 255, 0), -1);
+
+				if((points_lasers_in_cam.at(i).ipx > mini_box.x) && ( points_lasers_in_cam.at(i).ipx < (mini_box.x + mini_box.width)) &&
+						(points_lasers_in_cam.at(i).ipy > mini_box.y) && (points_lasers_in_cam.at(i).ipy < (mini_box.y + mini_box.height)))
 				{
-					if (points_lasers_in_cam.at(i).laser_polar.length <= MIN_RANGE)
-						points_lasers_in_cam.at(i).laser_polar.length = MAX_RANGE;
-
-					if (points_lasers_in_cam.at(i).laser_polar.length > MAX_RANGE)
-						points_lasers_in_cam.at(i).laser_polar.length = MAX_RANGE;
-
-
-					circle(*img, cv::Point(points_lasers_in_cam.at(i).ipx, points_lasers_in_cam.at(i).ipy), 2, Scalar(0, 255, 0), -1);
-
-					if((points_lasers_in_cam.at(i).ipx > mini_box.x) && ( points_lasers_in_cam.at(i).ipx < (mini_box.x + mini_box.width)) &&
-							(points_lasers_in_cam.at(i).ipy > mini_box.y) && (points_lasers_in_cam.at(i).ipy < (mini_box.y + mini_box.height)))
-					{
-		//				ranges_d.push_back(points_lasers_in_cam.at(i).laser_polar.length);
-						circle(*img, cv::Point(points_lasers_in_cam.at(i).ipx, points_lasers_in_cam.at(i).ipy), 2, white, -1);
-		//				average_range += points_lasers_in_cam.at(i).laser_polar.length;
-					}
-
-					//printf("%f\t %f\t %f\t \n",r, range, velodyne_message->partial_scan[j].distance[i] );
-
+	//				ranges_d.push_back(points_lasers_in_cam.at(i).laser_polar.length);
+					circle(*img, cv::Point(points_lasers_in_cam.at(i).ipx, points_lasers_in_cam.at(i).ipy), 2, white, -1);
+	//				average_range += points_lasers_in_cam.at(i).laser_polar.length;
 				}
 
+				//printf("%f\t %f\t %f\t \n",r, range, velodyne_message->partial_scan[j].distance[i] );
+
 			}
+
+		}
+
+		if (median_ranges.size() > 0/**/)
+		{
+			std::sort(median_ranges.begin(), median_ranges.end());
+			int middle = median_ranges.size() / 2;
+			double median = median_ranges.at(middle);
+
+			trackerPoint = compute_3d_point(box_1, median);
+
+	//		double averange_distance = average_range / count_range;
+			sprintf(string1,"range: %f x: %f y: %f z: %f ", median, trackerPoint.x, trackerPoint.y, trackerPoint.z);
+		}
 
 		last_track[(cont%num_prev_frames)].confidence = 1.0; //
 		cont++;
@@ -285,7 +338,7 @@ process_goturn_detection(Mat *img, double time_stamp)
 //	cvRectangle(&prev_image, cvPoint(0, 0), cvPoint(img.width, 50), CV_RGB(0, 0, 255), CV_FILLED, 8, 0);
 //	cvPutText(&prev_image.data, string1, cvPoint(25, 25), &font, CV_RGB(255, 255, 255));
 	// draw the box
-	sprintf(string1, "Time:%.2f, FPS:%d", time_stamp, disp_last_fps);
+	sprintf(string1, "%s Time:%.2f, FPS:%d",string1, time_stamp, disp_last_fps);
 	cv::Size s = img->size();
 	cv::Point textOrg(25,25);
 	cv::rectangle(*img, cv::Point(0, 0), cv::Point(s.width, 50), Scalar::all(0), -1);
@@ -331,13 +384,11 @@ process_image(carmen_bumblebee_basic_stereoimage_message *msg)
 {
 	static Mat *src_image = NULL;
 	static Mat *rgb_image = NULL;
-	static Mat *resized_rgb_image = NULL;
 
 	if (src_image == NULL)
 	{
 		src_image = new Mat(Size(msg->width, msg->height), CV_8UC3);
 		rgb_image = new Mat(Size(msg->width, msg->height), CV_8UC3);
-		resized_rgb_image = new Mat(Size(tld_image_width , tld_image_height), CV_8UC3);
 	}
 
 	if (camera_side == 0)
@@ -367,11 +418,114 @@ process_image(carmen_bumblebee_basic_stereoimage_message *msg)
 	//cvReleaseImage(&src_image);
 }
 
+static void
+publishSplineRDDF()
+{
+	static std::vector<double> X;
+	static std::vector<double> Y;
+	static std::vector<double> I;
+	static int index = 0;
+	static unsigned int maxPositions = 20;
+	static unsigned int extraPositions = 20;
+
+	vector<carmen_ackerman_traj_point_t> poses;
+	tk::spline x, y;
+	carmen_ackerman_traj_point_t localizePose;
+
+
+	double minTimestampDiff = 666.0;
+	int minTimestampIndex = 0;
+	for (unsigned int i = 0; i < localizeVector.size(); i++)
+	{
+		if(fabs(localizeVector[i].timestamp - velodyne_message_arrange->timestamp) < minTimestampDiff)
+		{
+			minTimestampIndex = i;
+			minTimestampDiff = fabs(localizeVector[i].timestamp - velodyne_message_arrange->timestamp);
+		}
+	}
+
+	localizePose.x = localizeVector[minTimestampIndex].globalpos.x;
+	localizePose.y = localizeVector[minTimestampIndex].globalpos.y;
+	localizePose.theta = localizeVector[minTimestampIndex].globalpos.theta;
+	localizePose.v = localizeVector[minTimestampIndex].v;
+	localizePose.phi = localizeVector[minTimestampIndex].phi;
+
+	double distancia = sqrt(pow(trackerPoint.x, 2) + pow(trackerPoint.y, 2));
+	double angulo = atan2(trackerPoint.y, trackerPoint.x);
+	//distancia -= 4; //TODO porque?
+
+	X.push_back(localizePose.x + distancia * cos(carmen_normalize_theta(localizePose.theta + carmen_degrees_to_radians(angulo))));
+	Y.push_back(localizePose.y + distancia * sin(carmen_normalize_theta(localizePose.theta + carmen_degrees_to_radians(angulo))));
+	I.push_back(index++);
+
+	if (I.size() > maxPositions)
+	{
+		X.erase(X.begin());
+		Y.erase(Y.begin());
+		I.erase(I.begin());
+	}
+
+	if (I.size() > 1)
+	{
+		x.set_points(I,X);
+		y.set_points(I,Y);
+		carmen_ackerman_traj_point_t newPose;
+		newPose.x = x(I[0]);
+		newPose.y = y(I[0]);
+		poses.push_back(newPose);
+
+		for (unsigned int i = 1; i < I.size() + (unsigned int)extraPositions; i++)
+		{
+			newPose.x = x(I[0] + i);
+			newPose.y = y(I[0] + i);
+			newPose.v = 1.0;
+			poses.back().theta = carmen_normalize_theta(atan2(poses[i - 1].x - poses[i].x, poses[i - 1].y - poses[i].y));
+			poses.push_back(newPose);
+		}
+	}
+
+	//Pose RDDF
+	if (poses.size() > 0)
+	{
+		int annotations[1000];
+	    IPC_RETURN_TYPE err;
+	    carmen_rddf_road_profile_message path_planner_road_profile_message;
+
+	    path_planner_road_profile_message.poses = &poses[0];
+	    path_planner_road_profile_message.poses_back = 0;
+	    path_planner_road_profile_message.number_of_poses = poses.size();
+	    path_planner_road_profile_message.number_of_poses_back = 0;
+	    path_planner_road_profile_message.annotations = annotations;
+	    path_planner_road_profile_message.timestamp = carmen_get_time();
+	    path_planner_road_profile_message.host = carmen_get_host();
+
+	    err = IPC_publishData(CARMEN_RDDF_ROAD_PROFILE_MESSAGE_NAME, &path_planner_road_profile_message);
+	    carmen_test_ipc_exit(err, "Could not publish", CARMEN_RDDF_ROAD_PROFILE_MESSAGE_FMT);
+	}
+
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                           //
 // Handlers                                                                                  //
 //                                                                                           //
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+static void
+localize_globalpos_handler(carmen_localize_ackerman_globalpos_message *message)
+{
+	static unsigned int maxPositions = 100;
+	localizeVector.push_back(*message);
+
+	if (localizeVector.size() > maxPositions)
+	{
+		localizeVector.erase(localizeVector.begin());
+	}
+}
+
+
+
 static void
 image_handler(carmen_bumblebee_basic_stereoimage_message* image_msg)
 {
@@ -413,6 +567,7 @@ image_handler(carmen_bumblebee_basic_stereoimage_message* image_msg)
 		process_image(image_msg);
 
 		build_and_publish_message(image_msg->host, image_msg->timestamp);
+		publishSplineRDDF();
 //		double time_f = carmen_get_time() - time_now;
 //		printf("tp: %lf \n", time_f);
 	}
@@ -489,6 +644,8 @@ main(int argc, char **argv)
 	carmen_param_check_version(argv[0]);
 	read_parameters(argc, argv);
 	last_track.resize(num_prev_frames);
+	camera_parameters = get_stereo_instance(camera, tld_image_width, tld_image_height);
+
 
 	box.x1_ = -1.0;
 	box.y1_ = -1.0;
@@ -503,6 +660,9 @@ main(int argc, char **argv)
 	carmen_bumblebee_basic_subscribe_stereoimage(camera, NULL, (carmen_handler_t) image_handler, CARMEN_SUBSCRIBE_LATEST);
 
 	carmen_velodyne_subscribe_partial_scan_message(NULL,(carmen_handler_t)velodyne_partial_scan_message_handler, CARMEN_SUBSCRIBE_LATEST);
+
+	carmen_localize_ackerman_subscribe_globalpos_message(NULL, (carmen_handler_t) localize_globalpos_handler, CARMEN_SUBSCRIBE_LATEST);
+
 
 
 	carmen_ipc_dispatch();
