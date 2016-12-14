bc27a0e327148f864b23e10450aa9907a20d2071
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2016-12-14 17:02:44 -0200

    Modificações no moving objects 3

--- src/moving_objects3/moving_objects3_particle_filter.cpp
+++ src/moving_objects3/moving_objects3_particle_filter.cpp
@@ -1,7 +1,5 @@
 #include "moving_objects3_particle_filter.h"
 
-#include <vector>
-#include "math.h"
 
 moving_objects3_particle_t
 sample_motion_model(moving_objects3_particle_t particle_t_1, double delta_time)
@@ -27,28 +25,125 @@ sample_motion_model(moving_objects3_particle_t particle_t_1, double delta_time)
 	particle_t.pose.y = particle_t_1.pose.y + delta_y;
 	particle_t.pose.theta = particle_t_1.pose.theta + delta_theta_1 + delta_theta_2;
 	particle_t.velocity = velocity_t;
+	particle_t.geometry.length = particle_t_1.geometry.length;
+	particle_t.geometry.width = particle_t_1.geometry.width;
 
 	return particle_t;
 }
 
 
-// TODO terminar implementação
+double
+get_ray_cost(carmen_vector_2D_t end_point, rectangle_points r_a, rectangle_points r_b, rectangle_points r_c)
+{
+	const double c_occ = 0.5;
+	const double c_b = 1.0;
+	const double c_s = 0.0;
+	const double c_p = 0.9;
+
+	int intersect_a, intersect_b, intersect_c;
+
+	intersect_c = check_ray_intersection(end_point, r_c);
+	if (intersect_c)
+		return c_p;
+
+	intersect_b = check_ray_intersection(end_point, r_b);
+	if (intersect_b && !intersect_c)
+		return c_s;
+
+	intersect_a = check_ray_intersection(end_point, r_a);
+	if (intersect_a && !intersect_b)
+		return c_b;
+
+	return c_occ;
+}
+
+
+double
+get_probability(double cost, double sigma)
+{
+	double p_x, variance;
+	variance = sigma*sigma;
+
+	p_x = exp(-(cost)*(cost)/(variance));
+
+	return p_x;
+}
+
+
+double
+get_sigma_from_cost(double cost)
+{
+	return 3.0; //1/cost;
+}
+
+
+//TODO incluir a parte S_t-1que considera a geometria
+double
+get_particle_weight(moving_objects3_particle_t particle_t, carmen_velodyne_projected_on_ground_message velodyne_projected_on_ground)
+{
+	double probability = 1.0;
+	double cost,sigma;
+	rectangle_points r_a, r_b, r_c;
+
+	carmen_vector_2D_t end_point;
+
+	generate_rectangles_points(particle_t.pose, particle_t.geometry.width, particle_t.geometry.length, &r_c, &r_b, &r_a);
+
+	for (int i = 0; i < velodyne_projected_on_ground.num_rays; i++)
+	{
+		transform_polar_coordinates_to_cartesian_coordinates(velodyne_projected_on_ground.ranges[i], velodyne_projected_on_ground.angles[i], &end_point.x, &end_point.y);
+		cost = get_ray_cost(end_point, r_a, r_b, r_c);
+		sigma = get_sigma_from_cost(cost);
+		probability *= get_probability(cost, sigma);
+	}
+	return probability;
+}
+
+
 double
 measurement_model(moving_objects3_particle_t particle_t, carmen_velodyne_projected_on_ground_message velodyne_projected_on_ground)
 {
-
+	(void) particle_t;
+	(void) velodyne_projected_on_ground;
 	return 1.0;
 }
 
 
-//todo checar se é assim mesmo
-double
-get_particle_weight(double cost)
+void
+normalize_weights(std::vector<moving_objects3_particle_t> &particle_set, double total_weight)
 {
-	double variance = 1.0;
-	double n = 1.0;
+	double inv_total_weight = 1/total_weight;
+	std::vector<moving_objects3_particle_t>::iterator it = particle_set.begin();
+	std::vector<moving_objects3_particle_t>::iterator end = particle_set.end();
 
-	return n*exp(-cost/(variance));
+	for (; it != end; ++it)
+	{
+		it->weight *= inv_total_weight;
+	}
+}
+
+
+std::vector<moving_objects3_particle_t>
+resample(std::vector<moving_objects3_particle_t> particle_set_t)
+{
+	std::vector<moving_objects3_particle_t> particle_set;
+	double inv_num_particles = 1/NUM_OF_PARTICLES;
+	double r = carmen_uniform_random(0.0,inv_num_particles);
+	double c = particle_set_t[0].weight;
+
+	int i = 0;
+
+	for (int m = 0; m < NUM_OF_PARTICLES; m++)
+	{
+		double U = r + (m) * inv_num_particles;
+		while (U > c)
+		{
+			i++;
+			c += particle_set_t[i].weight;
+		}
+		particle_set.push_back(particle_set_t[i]);
+	}
+	return particle_set;
 }
 
 
@@ -58,31 +153,32 @@ algorithm_particle_filter(std::vector<moving_objects3_particle_t> particle_set_t
 		double delta_time)
 {
 	std::vector<moving_objects3_particle_t> particle_set_t;
-	moving_objects3_particle_t particle_t;
+
 	double total_weight = 0.0;
-	double cost = 0.0;
 
 	std::vector<moving_objects3_particle_t>::iterator it = particle_set_t_1.begin();
 	std::vector<moving_objects3_particle_t>::iterator end = particle_set_t_1.end();
 	for (; it != end; ++it)
 	{
+		moving_objects3_particle_t particle_t;
 		// Motion Model
 		particle_t = sample_motion_model((*it), delta_time);
 
 		// Measurement Model -> RANSAC
-		cost = measurement_model(particle_t, velodyne_projected_on_ground);
+		// cost = measurement_model(particle_t, velodyne_projected_on_ground);
 
 		// Weighing particles
-		particle_t.weight = get_particle_weight(cost);
+		particle_t.weight = get_particle_weight(particle_t, velodyne_projected_on_ground);
 		total_weight += particle_t.weight;
 
 		particle_set_t.push_back(particle_t);
 	}
 
-	// Weighing particles
-	//normalize_weights(particle_set_t, total_weight);
+	// normalize particles weight
+	normalize_weights(particle_set_t, total_weight);
 
-	//resample(particle_set_t);
+	// resample
+	particle_set_t = resample(particle_set_t);
 
 	return particle_set_t;
 }
