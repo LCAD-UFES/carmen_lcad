97960280e3ff49a7396b1a8948dc2c2a14a35237
vinibc <viniciusbc1@gmail.com>
2017-03-15 21:38:00 -0300

    mudancas no view_ground_truth do traffic_light para separar anotacoes incorretas

--- src/traffic_light/tools/view_gt/view_gt.cpp
+++ src/traffic_light/tools/view_gt/view_gt.cpp
@@ -28,152 +28,211 @@ int numOfRec = 0;
 int startDraw = 0;
 string window_name = "Generate Output <ESC> Exit";
 string old_string = "";
-
+FILE* yellow;
+FILE* off;
+FILE* erro;
+FILE* green;
+FILE* red;
 string
 IntToString(int num)
 {
-    ostringstream myStream; //creates an ostringstream object
-    myStream << num << flush;
-    /*
-     * outputs the number into the string stream and then flushes
-     * the buffer (makes sure the output is put into the stream)
-     */
-    return (myStream.str()); //returns the string form of the stringstream object
+	ostringstream myStream; //creates an ostringstream object
+	myStream << num << flush;
+	/*
+	 * outputs the number into the string stream and then flushes
+	 * the buffer (makes sure the output is put into the stream)
+	 */
+	return (myStream.str()); //returns the string form of the stringstream object
 };
 
 void
 on_mouse(int event, int x, int y, int flag, void *param)
 {
-    flag = flag;
-    param = param;
-    if (event == CV_EVENT_LBUTTONDOWN)
-    {
-        if (!startDraw)
-        {
-            roi_x0 = x;
-            roi_y0 = y;
-            startDraw = 1;
-        }
-        else
-        {
-            roi_x1 = x;
-            roi_y1 = y;
-            startDraw = 0;
-        }
-    }
-    if (event == CV_EVENT_MOUSEMOVE && startDraw)
-    {
-        //redraw ROI selection
-        image2 = image.clone();
+	flag = flag;
+	param = param;
+	if (event == CV_EVENT_LBUTTONDOWN)
+	{
+		if (!startDraw)
+		{
+			roi_x0 = x;
+			roi_y0 = y;
+			startDraw = 1;
+		}
+		else
+		{
+			roi_x1 = x;
+			roi_y1 = y;
+			startDraw = 0;
+		}
+	}
+	if (event == CV_EVENT_MOUSEMOVE && startDraw)
+	{
+		//redraw ROI selection
+		image2 = image.clone();
 
-        imshow(window_name, image2);
-        image2.~Mat();
-    }
+		imshow(window_name, image2);
+		image2.~Mat();
+	}
 
 }
 
 int
 main(int argc, char** argv)
 {
-    int iKey = 0;
-    string input_file;
-    string output_file;
-    ifstream input;
-    ofstream output;
-    int num = 0;
+	int iKey = 0;
+	string input_file;
+	string output_file;
+	ifstream input;
+	ofstream output;
+	int num = 0;
+	yellow = fopen("new_yellow.txt", "a");
+	off = fopen("new_off.txt", "a");
+	erro = fopen("new_undetected.txt", "a");
+	red = fopen("new_red.txt", "a");
+	green = fopen("new_green.txt", "a");
+	int x1, x2,y1,y2;
 
-    if (argc != 3)
-    {
-        cerr << argv[0] << " input_info.txt output_info.txt" << endl;
-        return -1;
-    }
 
-    input_file = argv[1];
-    output_file = argv[2];
+	if (argc != 3)
+	{
+		cerr << argv[0] << " input_info.txt output_info.txt" << endl;
+		return -1;
+	}
 
-    cerr << "Opening input_file with names of images" << endl;
-    input.open(input_file.c_str());
-    cerr << "Done." << endl;
+	input_file = argv[1];
+	output_file = argv[2];
 
-    output.open(output_file.c_str());
+	cerr << "Opening input_file with names of images" << endl;
+	input.open(input_file.c_str());
+	cerr << "Done." << endl;
 
-    namedWindow(window_name, 1);
+	output.open(output_file.c_str());
 
-    if (input.is_open())
-    {
-        string line;
+	namedWindow(window_name, 1);
 
-        getline(input, line);
+	if (input.is_open())
+	{
+		string line;
 
-        while (!input.eof())
-        {
-            cout << "Linha: " << line << endl;
-            string s;
-            vector<string> strings;
-            istringstream iss(line);
-            while (getline(iss, s, ' '))
-            {
-                cout << "Leitura: " << s << endl;
-                strings.push_back(s);
-            }
-            numOfRec = 0;
+		getline(input, line);
 
-            cerr << "Loading image :" << strings.at(0) << endl;
+		while (!input.eof())
+		{
+			cout << "Linha: " << line << endl;
+			string s;
+			vector<string> strings;
+			istringstream iss(line);
+			while (getline(iss, s, ' '))
+			{
+				cout << "Leitura: " << s << endl;
+				strings.push_back(s);
+			}
+			numOfRec = 0;
 
-            image = imread(strings.at(0), 1);
+			cerr << "Loading image :" << strings.at(0) << endl;
 
-            if (strings.at(0) != old_string)
-            {
-                old_string = strings.at(0);
-                output << old_string << endl;
-            }
-            if (!image.empty())
-            {
-                cout << "x1 " << strings.at(1) << endl;
-                int x1 = atoi(strings.at(1).c_str());
-                cout << "y1 " << strings.at(2) << endl;
-                int y1 = atoi(strings.at(2).c_str());
-                cout << "x2 " << strings.at(3) << endl;
-                int x2 = atoi(strings.at(3).c_str());
-                cout << "y2 " << strings.at(4) << endl;
-                int y2 = atoi(strings.at(4).c_str());
+			image = imread(strings.at(0), 1);
 
-                Rect myROI(cvPoint(x1, y1), cvPoint(x2, y2));
-                cv::Mat croppedImage;
+			if (strings.at(0) != old_string)
+			{
+				old_string = strings.at(0);
+				output << old_string << endl;
+			}
+			if (!image.empty())
+			{
+//				cout << "x1 " << strings.at(1) << endl;
+				x1 = atoi(strings.at(1).c_str());
+//				cout << "y1 " << strings.at(2) << endl;
+				y1 = atoi(strings.at(2).c_str());
+//				cout << "x2 " << strings.at(3) << endl;
+				x2 = atoi(strings.at(3).c_str());
+//				cout << "y2 " << strings.at(4) << endl;
+				y2 = atoi(strings.at(4).c_str());
 
-                cv::Mat(image, myROI).copyTo(croppedImage);
-                num++;
-                ostringstream myStream;
-                myStream << num << flush;
-                imwrite("positives/image" + myStream.str() + ".png", croppedImage);
+				Rect myROI(cvPoint(x1, y1), cvPoint(x2, y2));
+				cv::Mat croppedImage;
 
-                rectangle(image, cvPoint(x1, y1), cvPoint(x2, y2), CV_RGB(255, 0, 255), 1);
-                imshow(window_name, image);
+				cv::Mat(image, myROI).copyTo(croppedImage);
+				num++;
+				ostringstream myStream;
+				myStream << num << flush;
+				imwrite("positives/image" + myStream.str() + ".png", croppedImage);
 
-                iKey = waitKey(1500);
+				rectangle(image, cvPoint(x1, y1), cvPoint(x2, y2), CV_RGB(255, 0, 255), 1);
+				imshow(window_name, image);
 
-                //key 27 ESC
-                if (iKey == 27)
-                {
-                    image.release();
-                    destroyWindow(window_name);
-                    return EXIT_SUCCESS;
-                }
-            }
-            getline(input, line);
-            image.~Mat();
-        }
-    }
-    else
-    {
-        cerr << "Failed to open: " << input_file << endl;
-    }
+				iKey = waitKey(500);
 
-    image.~Mat();
-    image2.~Mat();
-    input.close();
-    output.close();
-    destroyWindow(window_name);
+				//key 27 ESC
+				if (iKey == 27)
+				{
+					image.release();
+					image2.~Mat();
+					destroyWindow(window_name);
+					input.close();
+					output.close();
+					fclose(red);
+					fclose(green);
+					fclose(yellow);
+					fclose(erro);
+					fclose(off);
+					return EXIT_SUCCESS;
+				}
+				if (iKey == 32)
+				{
+					while(iKey != 110)
+					{
+						iKey = waitKey(0);
+						switch (iKey)
+						{
+						case 103:                     // G key 103 Save added rectangles to GREEN file
+							//cout << "AAAAAA" << iKey << endl;
+							fprintf(green, "%s\n", line.c_str());
+							cout << "Salvo em green" << endl;
+							break;
 
-    return EXIT_SUCCESS;
+						case 121:                      // Y  key 121 Save added rectangles to YELOW file
+							fprintf(yellow, "%s\n", line.c_str());
+							cout << "Salvo em Yellow" << endl;
+							break;
+
+						case 114:                      // R key 114 Save added rectangles to RED file
+							fprintf(red, "%s\n", line.c_str());
+							cout << "Salvo em red" << endl;
+							break;
+
+						case 111:                       // O key 111 Save added rectangles to OFF file
+							fprintf(off, "%s\n", line.c_str());
+							cout << "Salvo em off" << endl;
+							break;
+
+						case 117:                       // U key 117 Save added rectangles to UNDETECTED file
+							fprintf(erro, "%s\n", line.c_str());
+							cout << "Salvo em undetected" << endl;
+							break;
+						}
+					}
+				}
+			}
+			getline(input, line);
+			image.~Mat();
+		}
+	}
+	else
+	{
+		cerr << "Failed to open: " << input_file << endl;
+	}
+
+	image.~Mat();
+	image2.~Mat();
+	input.close();
+	output.close();
+	destroyWindow(window_name);
+	fclose(red);
+	fclose(green);
+	fclose(yellow);
+	fclose(erro);
+	fclose(off);
+
+	return EXIT_SUCCESS;
 }
