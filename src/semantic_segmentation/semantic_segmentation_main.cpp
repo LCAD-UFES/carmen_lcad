26a63be04d96b7a55e6a5785c90a11f5df8c2556
filipe <filipe@filipe-XPS.(none)>
2017-04-13 18:44:55 -0300

    implementacao do modulo de semantic segmentation

--- src/semantic_segmentation/semantic_segmentation_main.cpp
+++ src/semantic_segmentation/semantic_segmentation_main.cpp
@@ -5,6 +5,12 @@
 #include <carmen/carmen.h>
 #include <carmen/bumblebee_basic_interface.h>
 #include <carmen/semantic_segmentation_interface.h>
+#include <carmen/semantic_segmentation.h>
+#include <opencv/cv.h>
+
+using namespace cv;
+
+SegNet *seg_net = NULL;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                           //
@@ -12,12 +18,20 @@
 //                                                                                           //
 ///////////////////////////////////////////////////////////////////////////////////////////////
 void 
-publish_upkeymessage()
+publish_segmented_image(Mat &out, double bumblebee_time)
 {
-	//  IPC_RETURN_TYPE err;
-	//
-	//  err = IPC_publishData(CARMEN_SKELETON_MODULE_FILTER_UPKEYMESSAGE_NAME, &filter_message);
-	//  carmen_test_ipc_exit(err, "Could not publish", CARMEN_SKELETON_MODULE_FILTER_UPKEYMESSAGE_FMT);
+	IPC_RETURN_TYPE err;
+	carmen_semantic_segmentation_image_message message;
+
+	message.width = out.cols;
+	message.height = out.rows;
+	message.image_size = out.rows * out.cols * out.channels();
+	message.image = out.data;
+	message.timestamp = bumblebee_time;
+	message.host = carmen_get_host();
+
+	err = IPC_publishData(CARMEN_SEMANTIC_SEGMENTATION_IMAGE_NAME, &message);
+	carmen_test_ipc_exit(err, "Could not publish", CARMEN_SEMANTIC_SEGMENTATION_IMAGE_FMT);
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -32,8 +46,21 @@ publish_upkeymessage()
 void 
 bumblebee_handler(carmen_bumblebee_basic_stereoimage_message *message)
 {
-	printf("%lf\n", message->timestamp);
-	publish_upkeymessage();
+	printf("Message received!\n");
+
+	Mat m(Size(message->width, message->height), CV_8UC3, message->raw_right);
+	Mat res(Size(message->width / 2, message->height / 2), CV_8UC3);
+
+	resize(m, res, res.size());
+	cvtColor(res.clone(), res, CV_RGB2BGR);
+
+	Mat out = seg_net->Predict(res);
+
+	imshow("bgr", res);
+	imshow("segmented", seg_net->ClassesToColors(out));
+	waitKey(1);
+
+	publish_segmented_image(out, message->timestamp);
 }
 
 
@@ -53,23 +80,14 @@ shutdown_module(int signo)
 int 
 main(int argc, char **argv) 
 {
-	/* Connect to IPC Server */
 	carmen_ipc_initialize(argc, argv);
-
-	/* Check the param server version */
 	carmen_param_check_version(argv[0]);
-
-	/* Register shutdown cleaner handler */
 	signal(SIGINT, shutdown_module);
 
-	/* Define messages that your module publishes */
 	carmen_semantic_segmentation_define_messages();
+	seg_net = new SegNet("segnet_model_driving_webdemo.prototxt", "segnet_iter_30000_timo.caffemodel", "camvid12.png");
 
-	/* Subscribe to sensor messages */
 	carmen_bumblebee_basic_subscribe_stereoimage(8 , NULL, (carmen_handler_t) bumblebee_handler, CARMEN_SUBSCRIBE_LATEST);
-
-	/* Loop forever waiting for messages */
 	carmen_ipc_dispatch();
-
 	return (0);
 }
