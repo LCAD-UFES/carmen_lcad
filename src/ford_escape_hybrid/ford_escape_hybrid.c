f563c7c44d0800578a7e584fd745274959ec1027
ranikguidolini <ranikguidolini@gmail.com>
2017-05-31 18:05:37 -0300

    inclusao printf test ford_escape_hybrid.c

--- src/ford_escape_hybrid/ford_escape_hybrid.c
+++ src/ford_escape_hybrid/ford_escape_hybrid.c
@@ -645,8 +645,13 @@ torc_report_curvature_message_handler(OjCmpt XGV_CCU __attribute__ ((unused)), J
 			#ifdef PLOT
 					pid_plot_phi(ford_escape_hybrid_config->filtered_phi, -get_phi_from_curvature(g_atan_desired_curvature, ford_escape_hybrid_config), 0.55, "phi");
 			#endif
-
 		}
+
+		//Printf para testar a diferença das curvaturas original e modificada
+		//printf(cc %lf cc2 %lf\n", g_XGV_atan_curvature, -atan(get_curvature_from_phi(ford_escape_hybrid_config->filtered_phi, ford_escape_hybrid_config)));
+
+		//	fprintf(stdout, "(cc dc s v t) %lf %lf %lf %lf %lf\n", -atan(get_curvature_from_phi(ford_escape_hybrid_config->filtered_phi, ford_escape_hybrid_config)), g_atan_desired_curvature, g_steering_command, ford_escape_hybrid_config->filtered_v, carmen_get_time());
+		//	fflush(stdout);
 
 		previous_gear_command = g_gear_command;
 
