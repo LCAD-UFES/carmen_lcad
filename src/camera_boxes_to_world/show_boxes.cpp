61e15000c47fd0f7c4fa9ad3f60faf2fa6a28731
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2017-06-14 15:01:02 -0300

    Mostra os pontos do velodyne na imagem

--- src/camera_boxes_to_world/show_boxes.cpp
+++ src/camera_boxes_to_world/show_boxes.cpp
@@ -18,6 +18,33 @@
 int camera;
 int camera_side;
 
+const unsigned int maxPositions = 50;
+carmen_velodyne_partial_scan_message *velodyne_message_arrange;
+std::vector<carmen_velodyne_partial_scan_message> velodyne_vector;
+
+
+/*
+ This function find the closest velodyne message with the camera message
+ */
+
+carmen_velodyne_partial_scan_message
+find_velodyne_most_sync_with_cam(double bumblebee_timestamp)
+{
+	carmen_velodyne_partial_scan_message velodyne;
+	double minTimestampDiff = DBL_MAX;
+	int minTimestampIndex = -1;
+	for (unsigned int i = 0; i < velodyne_vector.size(); i++)
+	{
+		if(fabs(velodyne_vector[i].timestamp - bumblebee_timestamp) < minTimestampDiff)
+		{
+			minTimestampIndex = i;
+			minTimestampDiff = fabs(velodyne_vector[i].timestamp - bumblebee_timestamp);
+		}
+	}
+	velodyne = velodyne_vector[minTimestampIndex];
+	return (velodyne);
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                           //
@@ -47,13 +74,54 @@ image_handler(carmen_bumblebee_basic_stereoimage_message* image_msg)
 		memcpy(src_image->data, image_msg->raw_right, image_msg->image_size * sizeof(char));
 	}
 
-//	carmen_velodyne_camera_calibration_lasers_points_in_camera_with_obstacle_and_display(
-//						velodyne_message, image_msg->width, image_msg->height);
+	carmen_velodyne_partial_scan_message velodyne_sync_with_cam;
+	std::vector<carmen_velodyne_points_in_cam_with_obstacle_t> points_lasers_in_cam_with_obstacle;
+
+	velodyne_sync_with_cam = find_velodyne_most_sync_with_cam(image_msg->timestamp);
+	points_lasers_in_cam_with_obstacle =  carmen_velodyne_camera_calibration_lasers_points_in_camera_with_obstacle_and_display(
+			&velodyne_sync_with_cam, image_msg->width, image_msg->height);
 
 	cv::cvtColor(*src_image, *rgb_image, cv::COLOR_RGB2BGR);
 
-	cv::imshow("Display window", *rgb_image);
+	for (unsigned int i = 0; i < points_lasers_in_cam_with_obstacle.size(); i++)
+	{
+		if (points_lasers_in_cam_with_obstacle[i].hit_in_obstacle == true)
+		{
+			cv::circle(*rgb_image, cv::Point(points_lasers_in_cam_with_obstacle[i].velodyne_points_in_cam.ipx,
+					points_lasers_in_cam_with_obstacle[i].velodyne_points_in_cam.ipy), 2, cv::Scalar(0, 255, 0), -1);
+		}
+	}
+
+	cv::Mat resized_image(cv::Size(640, 480), CV_8UC3);
+	cv::resize(*rgb_image, resized_image, resized_image.size());
+
+	cv::imshow("Display window", resized_image);
 	cv::waitKey(1);
+}
+
+
+void
+velodyne_partial_scan_message_handler(carmen_velodyne_partial_scan_message *velodyne_message)
+{
+	velodyne_message_arrange = velodyne_message;
+	carmen_velodyne_camera_calibration_arrange_velodyne_vertical_angles_to_true_position(velodyne_message_arrange);
+
+	carmen_velodyne_partial_scan_message velodyne_copy;
+
+	velodyne_copy.host = velodyne_message_arrange->host;
+	velodyne_copy.number_of_32_laser_shots = velodyne_message_arrange->number_of_32_laser_shots;
+	velodyne_copy.partial_scan = (carmen_velodyne_32_laser_shot*)malloc(sizeof(carmen_velodyne_32_laser_shot) * velodyne_message_arrange->number_of_32_laser_shots);
+	memcpy(velodyne_copy.partial_scan, velodyne_message_arrange->partial_scan, sizeof(carmen_velodyne_32_laser_shot) * velodyne_message_arrange->number_of_32_laser_shots);
+	velodyne_copy.timestamp = velodyne_message_arrange->timestamp;
+
+	velodyne_vector.push_back(velodyne_copy);
+
+	if (velodyne_vector.size() > maxPositions)
+	{
+		free(velodyne_vector.begin()->partial_scan);
+		velodyne_vector.erase(velodyne_vector.begin());
+	}
+	//	show_velodyne(velodyne_message);
 }
 
 
@@ -78,6 +146,11 @@ subscribe_messages()
     carmen_bumblebee_basic_subscribe_stereoimage(camera,
     		NULL, (carmen_handler_t) image_handler,
 			CARMEN_SUBSCRIBE_LATEST);
+
+	carmen_velodyne_subscribe_partial_scan_message(NULL,
+			(carmen_handler_t)velodyne_partial_scan_message_handler,
+			CARMEN_SUBSCRIBE_LATEST);
+
 }
 
 
