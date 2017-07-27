91f6774cc497cc0b0810405748817f95b821c4e0
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2017-07-27 17:36:46 -0300

    intial changes in the neural car detector to filter the outliers points in bounding boxes

---
+++ src/neural_car_detector/dbscan.h
@@ -0,0 +1,33 @@
+#ifndef DBSCAN_H
+#define DBSCAN_H
+
+#include <vector>
+
+namespace dbscan
+{
+
+struct Point {
+	int x;
+
+	int y;
+};
+
+typedef std::vector<Point> Cluster;
+
+typedef std::vector<Cluster> Clusters;
+
+/**
+ * @brief Split a collection of points into clusters by distance.
+ *
+ * If a point has at least `density` neighbors with a squared distance no
+ * larger than `d2`, then it will either be included on an existing cluster or
+ * become the seed for a new one, depending on what is more appropriate. If it
+ * doesn't make the density requirement, it may still be added to a cluster if
+ * it's close enough to a point already assigned to a cluster (i.e. if its
+ * squared distance to it is less than `d2`).
+ */
+Clusters DBSCAN(double d2, std::size_t density, const Cluster &points);
+
+} // namespace dbscan
+
+#endif
