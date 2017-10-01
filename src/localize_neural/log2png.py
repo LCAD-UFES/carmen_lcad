a9e82af3b112071391d7226ce26a1c325b97d7b0
Avelino Forechi <avelino@lcad.inf.ufes.br>
2017-10-01 02:24:29 -0300

    refactoring on scripts to export images from logs

--- src/localize_neural/log2png.py
+++ src/localize_neural/log2png.py
@@ -1,9 +1,9 @@
 import os
+import sys
 import cv2
 import argparse
 import threading
 import numpy as np
-from pprint import pprint
 
 
 def check_setup(input_list, output_dir):
@@ -13,60 +13,97 @@ def check_setup(input_list, output_dir):
         raise Exception('The output_dir is not a valid directory!')    
 
 
-def main(input_list, output_dir, max_threads, camera_id, dst_size):
-    check_setup(input_list, output_dir)
+def save_any_img(img_left, img_right, img_size, dst_size, timestamp, camera_id, output_dir, max_height=None):
+    img_left = img_left.reshape(img_size)[:, :, ::-1]  # transpose channels RGB -> BGR
+    img_right = img_right.reshape(img_size)[:, :, ::-1]  # transpose channels RGB -> BGR
 
-    with open(input_list, 'rb') as f:
-        image_list = [line.strip().split() for line in f.readlines()]
-        image_list = [
-            {
-                'path': item[1].decode('utf-8'), 
-                'size': (int(item[3]), int(item[2]), 3), 
-                'bytes': int(item[4]),
-                'timestamp': item[6].decode('utf-8'), 
-            } 
-            for item in image_list
-        ]
+    ori_size = img_size[:2][::-1]  # transpose height x width -> width x height
 
-    if len(image_list) == 0:
-        raise Exception('The input_list has 0 lines!')
+    if dst_size is None:
+        dst_size = ori_size
 
-    mythreads = []
+    if dst_size != ori_size:
+        img_left = cv2.resize(img_left, dst_size)
+        img_right = cv2.resize(img_right, dst_size)
+
+    if max_height is not None:
+        img_left = img_left[0:max_height, 0:-1]
+        img_right = img_right[0:max_height, 0:-1]
+
+    img_left_fname = '{0}.bb{1}.l.png'.format(timestamp, camera_id)
+    img_right_fname = '{0}.bb{1}.r.png'.format(timestamp, camera_id)
+    print('Saving images {} and {} into {}'.format(img_left_fname, img_right_fname, output_dir))
+    cv2.imwrite(os.path.join(output_dir, img_left_fname), img_left)
+    cv2.imwrite(os.path.join(output_dir, img_right_fname), img_right)
+
+
+def save_old_img(image, output_dir, camera_id, dst_size=None, max_height=None):
+    img_left = np.fromstring(image['left'], count=image['bytes'], dtype=np.uint8)
+    img_right = np.fromstring(image['right'], count=image['bytes'], dtype=np.uint8)
+
+    save_any_img(img_left, img_right, image['size'], dst_size, image['timestamp'], camera_id, output_dir, max_height)
+
+
+def save_new_img(image, output_dir, camera_id, dst_size=None, max_height=None):
+    img_handler = open(image['path'], 'rb')
+
+    img_left = np.fromfile(img_handler, count=image['bytes'], dtype=np.uint8)
+    img_right = np.fromfile(img_handler, count=image['bytes'], dtype=np.uint8)
+
+    save_any_img(img_left, img_right, image['size'], dst_size, image['timestamp'], camera_id, output_dir, max_height)
+
+
+def read_old_log(input_list, output_dir, max_threads, max_lines, camera_id, dst_size=None, max_height=None):
     total = 0
-    for i, image in enumerate(image_list):
-        t = threading.Thread(target=thread_save_images, args=(image, output_dir, max_threads, camera_id, dst_size))
+    mythreads = []
+    f = open(input_list, 'rb')
+    line = f.readline()
+    while line and total < max_lines:
+        item = line.strip().split()
+        image = {
+                'right': item[5].decode('hex'),
+                'left': item[6].decode('hex'),
+                'size': (int(item[2]), int(item[1]), 3),
+                'bytes': int(item[3]),
+                'timestamp': item[7].decode('utf-8')
+        }
+        line = f.readline()
+        t = threading.Thread(target=save_old_img, args=(image, output_dir, camera_id, dst_size, max_height))
         mythreads.append(t)
         t.start()
-        if (len(mythreads) >= max_threads) or (i == len(image_list) - 1):
+        total += 1
+        if (len(mythreads) >= max_threads) or not (line and total < max_lines):
             for t in mythreads:
                 t.join()
-            total += len(mythreads)
             mythreads[:] = []  # clear the thread's list
-            print('Saved {}/{} images already...'.format(total, len(image_list)))
+            print('Saved {} images already...'.format(total))
+    f.close()
 
 
-def thread_save_images(image, output_dir, max_threads, camera_id, dst_size=None):
-    with open(image['path'], 'rb') as img_handler:
-            img_left = np.fromfile(img_handler, count=image['bytes'], dtype=np.uint8)
-            img_right = np.fromfile(img_handler, count=image['bytes'], dtype=np.uint8)
-            img_left = img_left.reshape(image['size'])[:,:,::-1]
-            img_right = img_right.reshape(image['size'])[:,:,::-1]
-            
-            if dst_size is None:
-                dst_size = img_right.shape[:2] #height x width
-            else:
-                dst_size = dst_size[::-1] #transpose width x height -> height x width
-                
-            if dst_size != img_right.shape[:2]:
-                img_left = cv2.resize(img_left, dst_size)
-                img_right = cv2.resize(img_right, dst_size)
-            
-            img_left_fname = '{0}.bb{1}.l.png'.format(image['timestamp'], camera_id)
-            img_right_fname = '{0}.bb{1}.r.png'.format(image['timestamp'], camera_id)
-            if max_threads <= 10:
-                print('Saving images {} and {} into {}'.format(img_left_fname, img_right_fname, output_dir))
-            cv2.imwrite(os.path.join(output_dir, img_left_fname), img_left)
-            cv2.imwrite(os.path.join(output_dir, img_right_fname), img_right)
+def read_new_log(input_list, output_dir, max_threads, max_lines, camera_id, dst_size=None, max_height=None):
+    total = 0
+    mythreads = []
+    f = open(input_list, 'rb')
+    line = f.readline()
+    while line and total < max_lines:
+        item = line.strip().split()
+        image = {
+            'path': item[1].decode('utf-8'),
+            'size': (int(item[3]), int(item[2]), 3),
+            'bytes': int(item[4]),
+            'timestamp': item[6].decode('utf-8'),
+        }
+        line = f.readline()
+        t = threading.Thread(target=save_new_img, args=(image, output_dir, camera_id, dst_size, max_height))
+        mythreads.append(t)
+        t.start()
+        total += 1
+        if (len(mythreads) >= max_threads) or not (line and total < max_lines):
+            for t in mythreads:
+                t.join()
+            mythreads[:] = []  # clear the thread's list
+            print('Saved {} images already...'.format(total))
+    f.close()
 
 
 if __name__ == '__main__':
@@ -75,11 +112,22 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input_list', type=str, required=True)
     parser.add_argument('-o', '--output_dir', type=str, required=True)
     parser.add_argument('-c', '--camera_id', type=int, required=True)
+    parser.add_argument('-f', '--log_format', type=int, required=True)
+    parser.add_argument('-l', '--max_lines', type=int, required=False, default=sys.maxint)
     parser.add_argument('-t', '--max_threads', type=int, required=False, default=10)
-    parser.add_argument('-s', '--size', type=str, required=False, default='none')
+    parser.add_argument('-m', '--max_height', type=int, required=False, default=0)
+    parser.add_argument('-s', '--image_size', type=str, required=False, default='none')
     argv = vars(parser.parse_args())
 
-    dst_size = tuple(map(int, argv['size'].split('x'))) if argv['size'] != 'none' else None
-    print('Destination size: {}'.format(dst_size)) 
-    main(argv['input_list'], argv['output_dir'], argv['max_threads'], argv['camera_id'], dst_size)
+    check_setup(argv['input_list'], argv['output_dir'])
+
+    max_height = argv['max_height'] if argv['max_height'] > 0 else None
+    dst_size = tuple(map(int, argv['image_size'].split('x'))) if argv['image_size'] != 'none' else None
+
+    print('Destination size: {}'.format(dst_size))
+
+    if argv['log_format'] == 1:     # 1-New Log Format
+        read_new_log(argv['input_list'], argv['output_dir'], argv['max_threads'], argv['max_lines'], argv['camera_id'], dst_size, max_height)
+    else:   # 0-Old Log Format
+        read_old_log(argv['input_list'], argv['output_dir'], argv['max_threads'], argv['max_lines'], argv['camera_id'], dst_size, max_height)
 
--- src/localize_neural/log2png.sh
+++ src/localize_neural/log2png.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-#"/dados/log_voltadaufes-20160721.txt"
-declare -a logs=(
+declare -a logs=("/dados/log_voltadaufes-20160721.txt"
 "/dados/log_voltadaufes-20160721_ambiental_1.txt"
 "/dados/log_voltadaufes-20160721_ambiental.txt"
 "/dados/log_voltadaufes-20160825.txt" 
@@ -15,10 +14,12 @@ declare -a logs=(
 "/dados/log_guarapari-20170403-2.txt"
 "/dados/log_guarapari-20170403.txt"
 "/dados/log_volta_da_ufes-20170926.txt"
-)
+"/dados/log_ponte-20170220.txt"
+"/dados/log_ponte-20161228.txt"
+"/dados/log_volta_da_ufes_localizer-20170303-2.txt"
+"/dados/log_volta_da_ufes_localizer-20170303.txt")
 
-#"/dados/ufes/20160721"
-declare -a dirs=(
+declare -a dirs=("/dados/ufes/20160721"
 "/dados/ufes/20160721-01"
 "/dados/ufes/20160721-00"
 "/dados/ufes/20160825"
@@ -32,52 +33,33 @@ declare -a dirs=(
 "/dados/ufes/20170403-02"
 "/dados/ufes/20170403"
 "/dados/ufes/20170926"
-)
+"/dados/ufes/20170220"
+"/dados/ufes/20161228"
+"/dados/ufes/20170303-02"
+"/dados/ufes/20170303")
 
-#8
-declare -a cams=(
-8
-8
-3
-3
-3
-3
-3
-3
-3
-3
-3
-3
-3
-)
+declare -a cams=(8 8 8
+3 3 3 3 3 3 3 3 3 3 3
+3 3 3 3)
 
-#"640x310"
-declare -a crops=(
-"640x310"
-"640x310"
-"640x380"
-"640x380" 
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-"640x380"
-)
+declare -a crops=(310 310 310
+380 380 380 380 380 380 380 380 380 380 380
+380 380 380 380)
+
+declare -a fmts=(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1)
 
 for i in "${!dirs[@]}"; do
-    #mkdir -p ${dirs[$i]}
+    mkdir -p ${dirs[$i]}
     echo "Saving at ${dirs[$i]}"
- 
-    #fgrep BUMBLEBEE_BASIC_STEREOIMAGE_IN_FILE${cams[$i]} ${logs[$i]} > /tmp/log2png.txt
-    #python log2png.py -i /tmp/log2png.txt -o ${dirs[$i]} -s 640x480 -c ${cams[$i]}
- 
-    rm -f ${dirs[$i]}/*crop.png
-    find ${dirs[$i]} -name '*.png' | parallel 'convert {.}.png -set colorspace RGB -gravity north -crop ${crops[$i]}+0+0 {.}.crop.png'
     
+    msg="BUMBLEBEE_BASIC_STEREOIMAGE"
+    if [ ${fmts[$i]} -eq 1 ]; then
+        msg="BUMBLEBEE_BASIC_STEREOIMAGE_IN_FILE"
+    fi
+    
+    echo "fgrep ${msg}${cams[$i]} ${logs[$i]} > /dados/log2png${i}.txt"
+    python log2png.py -i /dados/log2png${i}.txt -o ${dirs[$i]} -s 640x480 -c ${cams[$i]} -f ${fmts[$i]} -m ${crops[$i]}
+ 
     #~/sps-stereo/build/spsstereo ${dirs[$i]}
 done
 
