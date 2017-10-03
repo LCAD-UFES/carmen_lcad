865d1c926f72f0c957b07fa41b912c035ba1ca61
albertodesouza <albertodesouza@lcad.inf.ufes.br>
2017-10-02 23:50:39 -0300

    melhorias em ojIARASim

--- sharedlib/OpenJAUS/ojIARASim/src/pd.c
+++ sharedlib/OpenJAUS/ojIARASim/src/pd.c
@@ -44,6 +44,7 @@
 #include <ncurses.h>
 #include <termios.h>
 #include <unistd.h>
+#include <torc.h>
 #include "pd.h"
 
 #define CONTROLLER_STATUS_TIMEOUT_SEC 	1.5
@@ -51,131 +52,19 @@
 #define CONTROLLER_STATUS_QUEUE_SIZE			1
 #define CONTROLLER_STATUS_PRESENCE_VECTOR	0
 
-// USER: Insert any private function prototypes here
-void pdSendReportWrenchEffort(OjCmpt pd);
-void pdStandbyState(OjCmpt pd);
-void pdReadyState(OjCmpt pd);
-void pdProcessMessage(OjCmpt pd, JausMessage message);
-
 typedef struct
 {
 	SetWrenchEffortMessage setWrenchEffort;
 	SetDiscreteDevicesMessage setDiscreteDevices;
+	ReportWheelsSpeedMessage setWheelsSpeed;
+
 	ReportWrenchEffortMessage reportWrenchEffort;
+	ReportDiscreteDevicesMessage reportDiscreteDevices;
+	ReportWheelsSpeedMessage reportWheelsSpeed;
 	ReportComponentStatusMessage controllerStatus;
 	int controllerSc;
 }PdData;
 
-OjCmpt pdCreate(void)
-{
-	OjCmpt cmpt;
-	PdData *data;
-	JausAddress pdAddr;
-
-	cmpt = ojCmptCreate("pd", JAUS_PRIMITIVE_DRIVER, PD_THREAD_DESIRED_RATE_HZ);
-
-	if (!cmpt)
-	{
-		printf("Could not create primitive driver. Is ojNodeManager running?\n");
-		return (NULL);
-	}
-
-	ojCmptAddService(cmpt, JAUS_PRIMITIVE_DRIVER);
-	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_SET_WRENCH_EFFORT, 0xFF);
-	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_SET_DISCRETE_DEVICES, 0xFF);
-	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_QUERY_PLATFORM_SPECIFICATIONS, 0xFF);
-	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_QUERY_WRENCH_EFFORT, 0xFF);
-	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_PLATFORM_SPECIFICATIONS, 0xFF);
-	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_WRENCH_EFFORT, 0xFF);
-//	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_DISCRETE_DEVICES, 0xFF);
-	ojCmptAddSupportedSc(cmpt, JAUS_REPORT_WRENCH_EFFORT);
-
-	ojCmptSetMessageProcessorCallback(cmpt, pdProcessMessage);
-	ojCmptSetStateCallback(cmpt, JAUS_STANDBY_STATE, pdStandbyState);
-	ojCmptSetStateCallback(cmpt, JAUS_READY_STATE, pdReadyState);
-	ojCmptSetState(cmpt, JAUS_READY_STATE);
-
-	pdAddr = ojCmptGetAddress(cmpt);
-
-	data = (PdData*)malloc(sizeof(PdData));
-	data->setWrenchEffort = setWrenchEffortMessageCreate();
-	data->setDiscreteDevices = setDiscreteDevicesMessageCreate();
-	data->reportWrenchEffort = reportWrenchEffortMessageCreate();
-	data->controllerStatus = reportComponentStatusMessageCreate();
-	data->controllerSc = -1;
-	jausAddressCopy(data->reportWrenchEffort->source, pdAddr);
-
-	jausAddressDestroy(pdAddr);
-
-	ojCmptSetUserData(cmpt, (void *)data);
-
-	if (ojCmptRun(cmpt))
-	{
-		ojCmptDestroy(cmpt);
-		return NULL;
-	}
-
-	return cmpt;
-}
-
-void pdDestroy(OjCmpt pd)
-{
-	PdData *data;
-
-	data = (PdData*)ojCmptGetUserData(pd);
-
-	if(ojCmptIsIncomingScActive(pd, data->controllerSc))
-	{
-		ojCmptTerminateSc(pd, data->controllerSc);
-	}
-	ojCmptRemoveSupportedSc(pd, JAUS_REPORT_WRENCH_EFFORT);
-	ojCmptDestroy(pd);
-
-
-	setWrenchEffortMessageDestroy(data->setWrenchEffort);
-	setDiscreteDevicesMessageDestroy(data->setDiscreteDevices);
-	reportWrenchEffortMessageDestroy(data->reportWrenchEffort);
-	reportComponentStatusMessageDestroy(data->controllerStatus);
-	free(data);
-}
-
-// The series of functions below allow public access to essential component information
-// Access:		Public (All)
-JausBoolean pdGetControllerScStatus(OjCmpt pd)
-{
-	PdData *data;
-
-	data = (PdData*)ojCmptGetUserData(pd);
-
-	return ojCmptIsIncomingScActive(pd, data->controllerSc);
-}
-
-JausState pdGetControllerState(OjCmpt pd)
-{
-	PdData *data;
-
-	data = (PdData*)ojCmptGetUserData(pd);
-
-	return data->controllerStatus->primaryStatusCode;
-}
-
-SetWrenchEffortMessage pdGetWrenchEffort(OjCmpt pd)
-{
-	PdData *data;
-
-	data = (PdData*)ojCmptGetUserData(pd);
-
-	return data->setWrenchEffort;
-}
-
-SetDiscreteDevicesMessage pdGetDiscreteDevices(OjCmpt pd)
-{
-	PdData *data;
-
-	data = (PdData*)ojCmptGetUserData(pd);
-
-	return data->setDiscreteDevices;
-}
 
 // Function: pdProcessMessage
 // Access:		Private
@@ -234,6 +123,7 @@ void pdProcessMessage(OjCmpt pd, JausMessage message)
 			{
 				setWrenchEffortMessageDestroy(data->setWrenchEffort);
 				data->setWrenchEffort = setWrenchEffort;
+				// Mandar comandos para a IARA aqui
 			}
 			break;
 
@@ -241,8 +131,23 @@ void pdProcessMessage(OjCmpt pd, JausMessage message)
 			setDiscreteDevices = setDiscreteDevicesMessageFromJausMessage(message);
 			if(setDiscreteDevices)
 			{
-				setDiscreteDevicesMessageDestroy(data->setDiscreteDevices);
-				data->setDiscreteDevices = setDiscreteDevices;
+				if ((1 << JAUS_DEVICES_PV_PROPULSION_BIT) & setDiscreteDevices->presenceVector)
+				{
+					data->setDiscreteDevices->mainPropulsion = setDiscreteDevices->mainPropulsion;
+					data->setDiscreteDevices->mainFuelSupply = setDiscreteDevices->mainFuelSupply;
+ 				}
+				if ((1 << JAUS_DEVICES_PV_PARKING_BIT) & setDiscreteDevices->presenceVector)
+				{
+					data->setDiscreteDevices->parkingBrake = setDiscreteDevices->parkingBrake;
+					data->setDiscreteDevices->horn = setDiscreteDevices->horn;
+					// Mandar comandos para a IARA aqui
+ 				}
+				if ((1 << JAUS_DEVICES_PV_GEAR_BIT) & setDiscreteDevices->presenceVector)
+				{
+					data->setDiscreteDevices->gear = setDiscreteDevices->gear;
+					// Mandar comandos para a IARA aqui
+ 				}
+				setDiscreteDevicesMessageDestroy(setDiscreteDevices);
 			}
 			break;
 
@@ -272,6 +177,109 @@ void pdProcessMessage(OjCmpt pd, JausMessage message)
 	}
 }
 
+void pdSendReportWrenchEffort(OjCmpt pd)
+{
+	PdData *data;
+	JausMessage txMessage;
+	ServiceConnection scList;
+	ServiceConnection sc;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	scList = ojCmptGetScSendList(pd, JAUS_REPORT_WRENCH_EFFORT);
+	sc = scList;
+	while(sc)
+	{
+		jausAddressCopy(data->reportWrenchEffort->destination, sc->address);
+		data->reportWrenchEffort->presenceVector = sc->presenceVector;
+		data->reportWrenchEffort->sequenceNumber = sc->sequenceNumber;
+		data->reportWrenchEffort->properties.scFlag = JAUS_SERVICE_CONNECTION_MESSAGE;
+
+		data->reportWrenchEffort->propulsiveLinearEffortXPercent = data->setWrenchEffort->propulsiveLinearEffortXPercent;
+		data->reportWrenchEffort->propulsiveLinearEffortYPercent = data->setWrenchEffort->propulsiveLinearEffortYPercent;
+		data->reportWrenchEffort->propulsiveLinearEffortZPercent = data->setWrenchEffort->propulsiveLinearEffortZPercent;
+		data->reportWrenchEffort->propulsiveRotationalEffortXPercent = data->setWrenchEffort->propulsiveRotationalEffortXPercent;
+		data->reportWrenchEffort->propulsiveRotationalEffortYPercent = data->setWrenchEffort->propulsiveRotationalEffortYPercent;
+		data->reportWrenchEffort->propulsiveRotationalEffortZPercent = data->setWrenchEffort->propulsiveRotationalEffortZPercent;
+		data->reportWrenchEffort->resistiveLinearEffortXPercent = data->setWrenchEffort->resistiveLinearEffortXPercent;
+		data->reportWrenchEffort->resistiveLinearEffortYPercent = data->setWrenchEffort->resistiveLinearEffortYPercent;
+		data->reportWrenchEffort->resistiveLinearEffortZPercent = data->setWrenchEffort->resistiveLinearEffortXPercent;
+		data->reportWrenchEffort->resistiveRotationalEffortXPercent = data->setWrenchEffort->resistiveRotationalEffortXPercent;
+		data->reportWrenchEffort->resistiveRotationalEffortYPercent = data->setWrenchEffort->resistiveRotationalEffortYPercent;
+		data->reportWrenchEffort->resistiveRotationalEffortZPercent = data->setWrenchEffort->resistiveRotationalEffortZPercent;
+
+		txMessage = reportWrenchEffortMessageToJausMessage(data->reportWrenchEffort);
+		ojCmptSendMessage(pd, txMessage);
+		jausMessageDestroy(txMessage);
+
+		sc = sc->nextSc;
+	}
+
+	ojCmptDestroySendList(scList);
+}
+
+void pdSendReportDiscreteDevices(OjCmpt pd)
+{
+	PdData *data;
+	JausMessage txMessage;
+	ServiceConnection scList;
+	ServiceConnection sc;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	scList = ojCmptGetScSendList(pd, JAUS_REPORT_DISCRETE_DEVICES);
+	sc = scList;
+	while(sc)
+	{
+		jausAddressCopy(data->reportDiscreteDevices->destination, sc->address);
+		data->reportDiscreteDevices->presenceVector = sc->presenceVector;
+		data->reportDiscreteDevices->sequenceNumber = sc->sequenceNumber;
+		data->reportDiscreteDevices->properties.scFlag = JAUS_SERVICE_CONNECTION_MESSAGE;
+
+		data->reportDiscreteDevices->mainPropulsion = data->setDiscreteDevices->mainPropulsion;
+		data->reportDiscreteDevices->mainFuelSupply = data->setDiscreteDevices->mainFuelSupply;
+		data->reportDiscreteDevices->horn = data->setDiscreteDevices->horn;
+		data->reportDiscreteDevices->parkingBrake = data->setDiscreteDevices->parkingBrake;
+		data->reportDiscreteDevices->gear = data->setDiscreteDevices->gear;
+
+		txMessage = reportDiscreteDevicesMessageToJausMessage(data->reportDiscreteDevices);
+		ojCmptSendMessage(pd, txMessage);
+		jausMessageDestroy(txMessage);
+
+		sc = sc->nextSc;
+	}
+
+	ojCmptDestroySendList(scList);
+}
+
+void pdSendReportWheelsSpeed(OjCmpt pd)
+{
+	PdData *data;
+	JausMessage txMessage;
+	ServiceConnection scList;
+	ServiceConnection sc;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	scList = ojCmptGetScSendList(pd, JAUS_REPORT_WHEELS_SPEED);
+	sc = scList;
+	while(sc)
+	{
+		jausAddressCopy(data->reportWheelsSpeed->destination, sc->address);
+		data->reportWheelsSpeed->sequenceNumber = sc->sequenceNumber;
+		data->reportWheelsSpeed->properties.scFlag = JAUS_SERVICE_CONNECTION_MESSAGE;
+
+		*(data->reportWheelsSpeed) = *(data->setWheelsSpeed);
+
+		txMessage = reportWheelsSpeedMessageToJausMessage(data->reportWheelsSpeed);
+		ojCmptSendMessage(pd, txMessage);
+		jausMessageDestroy(txMessage);
+
+		sc = sc->nextSc;
+	}
+
+	ojCmptDestroySendList(scList);
+}
 
 void pdStandbyState(OjCmpt pd)
 {
@@ -284,6 +292,7 @@ void pdStandbyState(OjCmpt pd)
 	data = (PdData*)ojCmptGetUserData(pd);
 
 	pdSendReportWrenchEffort(pd);
+	pdSendReportDiscreteDevices(pd);
 
 	if(TRUE)//	vehicleSimGetState() == VEHICLE_SIM_READY_STATE	)
 	{
@@ -312,6 +321,7 @@ void pdReadyState(OjCmpt pd)
 	data = (PdData*)ojCmptGetUserData(pd);
 
 	pdSendReportWrenchEffort(pd);
+	pdSendReportDiscreteDevices(pd);
 
 	if(FALSE)//	vehicleSimGetState() != VEHICLE_SIM_READY_STATE )
 	{
@@ -372,46 +382,145 @@ void pdReadyState(OjCmpt pd)
 
 //		vehicleSimSetCommand(0, 80, 0);
 	}
+
+	// Ler estado da IARA e reporta-los aqui
+//	data->setWheelsSpeed->leftFront += 0.001;
+	if (data->setWheelsSpeed->leftFront > 9.0)
+		data->setWheelsSpeed->leftFront = 0.0;
+	data->setWheelsSpeed->leftRear = data->setWheelsSpeed->leftFront;
+	data->setWheelsSpeed->rightFront = data->setWheelsSpeed->leftFront;
+	data->setWheelsSpeed->rightRear = data->setWheelsSpeed->leftFront;
+	pdSendReportWheelsSpeed(pd);
 }
 
-void pdSendReportWrenchEffort(OjCmpt pd)
+OjCmpt pdCreate(void)
+{
+	OjCmpt cmpt;
+	PdData *data;
+	JausAddress pdAddr;
+
+	cmpt = ojCmptCreate("pd", JAUS_PRIMITIVE_DRIVER, PD_THREAD_DESIRED_RATE_HZ);
+
+	if (!cmpt)
+	{
+		printf("Could not create primitive driver. Is ojNodeManager running?\n");
+		return (NULL);
+	}
+
+	ojCmptAddService(cmpt, JAUS_PRIMITIVE_DRIVER);
+
+	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_SET_WRENCH_EFFORT, 0xFF);
+	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_SET_DISCRETE_DEVICES, 0xFF);
+	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_QUERY_PLATFORM_SPECIFICATIONS, 0xFF);
+	ojCmptAddServiceInputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_QUERY_WRENCH_EFFORT, 0xFF);
+
+	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_PLATFORM_SPECIFICATIONS, 0xFF);
+	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_WRENCH_EFFORT, 0xFF);
+	ojCmptAddServiceOutputMessage(cmpt, JAUS_PRIMITIVE_DRIVER, JAUS_REPORT_DISCRETE_DEVICES, 0xFF);
+
+	ojCmptAddSupportedSc(cmpt, JAUS_REPORT_WRENCH_EFFORT);
+	ojCmptAddSupportedSc(cmpt, JAUS_REPORT_DISCRETE_DEVICES);
+	ojCmptAddSupportedSc(cmpt, JAUS_REPORT_WHEELS_SPEED);
+
+	ojCmptSetMessageProcessorCallback(cmpt, pdProcessMessage);
+	ojCmptSetStateCallback(cmpt, JAUS_STANDBY_STATE, pdStandbyState);
+	ojCmptSetStateCallback(cmpt, JAUS_READY_STATE, pdReadyState);
+	ojCmptSetState(cmpt, JAUS_READY_STATE);
+
+	pdAddr = ojCmptGetAddress(cmpt);
+
+	data = (PdData*)malloc(sizeof(PdData));
+	data->setWrenchEffort = setWrenchEffortMessageCreate();
+	data->setDiscreteDevices = setDiscreteDevicesMessageCreate();
+	data->setWheelsSpeed = reportWheelsSpeedMessageCreate();
+
+	data->reportWrenchEffort = reportWrenchEffortMessageCreate();
+	data->reportDiscreteDevices = reportDiscreteDevicesMessageCreate();
+	data->reportWheelsSpeed = reportWheelsSpeedMessageCreate();
+
+	data->controllerStatus = reportComponentStatusMessageCreate();
+	data->controllerSc = -1;
+	jausAddressCopy(data->reportWrenchEffort->source, pdAddr);
+
+	jausAddressDestroy(pdAddr);
+
+	ojCmptSetUserData(cmpt, (void *)data);
+
+	if (ojCmptRun(cmpt))
+	{
+		ojCmptDestroy(cmpt);
+		return NULL;
+	}
+
+	return cmpt;
+}
+
+void pdDestroy(OjCmpt pd)
 {
 	PdData *data;
-	JausMessage txMessage;
-	ServiceConnection scList;
-	ServiceConnection sc;
 
 	data = (PdData*)ojCmptGetUserData(pd);
 
-	scList = ojCmptGetScSendList(pd, JAUS_REPORT_WRENCH_EFFORT);
-	sc = scList;
-	while(sc)
+	if(ojCmptIsIncomingScActive(pd, data->controllerSc))
 	{
-		jausAddressCopy(data->reportWrenchEffort->destination, sc->address);
-		data->reportWrenchEffort->presenceVector = sc->presenceVector;
-		data->reportWrenchEffort->sequenceNumber = sc->sequenceNumber;
-		data->reportWrenchEffort->properties.scFlag = JAUS_SERVICE_CONNECTION_MESSAGE;
-
-		data->reportWrenchEffort->propulsiveLinearEffortXPercent = data->setWrenchEffort->propulsiveLinearEffortXPercent;
-		data->reportWrenchEffort->propulsiveLinearEffortYPercent = data->setWrenchEffort->propulsiveLinearEffortYPercent;
-		data->reportWrenchEffort->propulsiveLinearEffortZPercent = data->setWrenchEffort->propulsiveLinearEffortZPercent;
-		data->reportWrenchEffort->propulsiveRotationalEffortXPercent = data->setWrenchEffort->propulsiveRotationalEffortXPercent;
-		data->reportWrenchEffort->propulsiveRotationalEffortYPercent = data->setWrenchEffort->propulsiveRotationalEffortYPercent;
-		data->reportWrenchEffort->propulsiveRotationalEffortZPercent = data->setWrenchEffort->propulsiveRotationalEffortZPercent;
-		data->reportWrenchEffort->resistiveLinearEffortXPercent = data->setWrenchEffort->resistiveLinearEffortXPercent;
-		data->reportWrenchEffort->resistiveLinearEffortYPercent = data->setWrenchEffort->resistiveLinearEffortYPercent;
-		data->reportWrenchEffort->resistiveLinearEffortZPercent = data->setWrenchEffort->resistiveLinearEffortXPercent;
-		data->reportWrenchEffort->resistiveRotationalEffortXPercent = data->setWrenchEffort->resistiveRotationalEffortXPercent;
-		data->reportWrenchEffort->resistiveRotationalEffortYPercent = data->setWrenchEffort->resistiveRotationalEffortYPercent;
-		data->reportWrenchEffort->resistiveRotationalEffortZPercent = data->setWrenchEffort->resistiveRotationalEffortZPercent;
-
-		txMessage = reportWrenchEffortMessageToJausMessage(data->reportWrenchEffort);
-		ojCmptSendMessage(pd, txMessage);
-		jausMessageDestroy(txMessage);
-
-		sc = sc->nextSc;
+		ojCmptTerminateSc(pd, data->controllerSc);
 	}
+	ojCmptRemoveSupportedSc(pd, JAUS_REPORT_WRENCH_EFFORT);
+	ojCmptDestroy(pd);
 
-	ojCmptDestroySendList(scList);
+
+	setWrenchEffortMessageDestroy(data->setWrenchEffort);
+	setDiscreteDevicesMessageDestroy(data->setDiscreteDevices);
+	reportWrenchEffortMessageDestroy(data->reportWrenchEffort);
+	reportDiscreteDevicesMessageDestroy(data->reportDiscreteDevices);
+	reportWheelsSpeedMessageDestroy(data->reportWheelsSpeed);
+	reportComponentStatusMessageDestroy(data->controllerStatus);
+	free(data);
 }
 
+// The series of functions below allow public access to essential component information
+// Access:		Public (All)
+JausBoolean pdGetControllerScStatus(OjCmpt pd)
+{
+	PdData *data;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	return ojCmptIsIncomingScActive(pd, data->controllerSc);
+}
+
+JausState pdGetControllerState(OjCmpt pd)
+{
+	PdData *data;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	return data->controllerStatus->primaryStatusCode;
+}
+
+SetWrenchEffortMessage pdGetWrenchEffort(OjCmpt pd)
+{
+	PdData *data;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	return data->setWrenchEffort;
+}
+
+SetDiscreteDevicesMessage pdGetDiscreteDevices(OjCmpt pd)
+{
+	PdData *data;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	return data->setDiscreteDevices;
+}
+
+ReportWheelsSpeedMessage pdGetWheelsSpeed(OjCmpt pd)
+{
+	PdData *data;
+
+	data = (PdData*)ojCmptGetUserData(pd);
+
+	return data->reportWheelsSpeed;
+}
