5c43afa889831a98a25dc27c058728eaaed6026d
car01 <car01@lcad.inf.ufes.br>
2017-10-24 11:41:56 -0200

    melhorias em ojIARASim

--- sharedlib/OpenJAUS/ojNodeManager/run_IARA_driver.bat
+++ sharedlib/OpenJAUS/ojNodeManager/run_IARA_driver.bat
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ip=192.168.25.59
+ip=192.168.0.14
 port=3794
 data_received=`nping --tcp -c 1 -p $port $ip | grep RCVD`
 
 while [ -z "$data_received" ]; do
-	echo "vazio"
+#	echo "vazio"
 	data_received=`nping --tcp -c 1 -p $port $ip | grep RCVD`
 done
-echo "valor: " $data_received
+#echo "valor: " $data_received
 
