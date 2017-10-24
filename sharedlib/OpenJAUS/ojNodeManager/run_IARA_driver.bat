c5376624518f5140683babec4719e347de85643c
car01 <car01@lcad.inf.ufes.br>
2017-10-24 11:58:00 -0200

    melhorias em ojIARASim

--- sharedlib/OpenJAUS/ojNodeManager/run_IARA_driver.bat
+++ sharedlib/OpenJAUS/ojNodeManager/run_IARA_driver.bat
@@ -1,4 +1,5 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/OpenJAUS/libopenJaus/lib:~/OpenJAUS/libjaus/lib:~/OpenJAUS/ojTorc/lib
 /home/pi/OpenJAUS/ojNodeManager/bin/ojNodeManager /home/pi/OpenJAUS/ojNodeManager/nodeManager_IARA.conf a >> /home/pi/ojNodeManager.log 2>&1 &
+sleep 1
 /home/pi/OpenJAUS/ojIARASim/bin/ojIARASim can0 can1 &>> /home/pi/ojIARASim.log 2>&1
 
