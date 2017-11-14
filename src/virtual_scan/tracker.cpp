edae48329e97aaa2e045e44b3d881bb8fe1f1f03
Claudine <claudinebadue@gmail.com>
2017-11-14 17:30:56 -0200

    Alteracoes no modulo virtual_scan

--- src/virtual_scan/tracker.cpp
+++ src/virtual_scan/tracker.cpp
@@ -9,14 +9,20 @@ Tracker::Tracker(int n):
 	tracks_n(new Tracks()),
 	tracks_star(new Tracks()),
 	uniform(0.0, 1.0),
-	
 {
 }
 
-bool Tracker::keep(Tracks *new_tracks)
+
+double Tracks::P()
+{
+	
+}
+
+
+bool Tracker::keep(Tracks *tracks_new)
 {
 	double U = uniform(rd);
-	
+	return (U < (tracks_new->P() / tracks_n->P()));
 }
 
 Tracks *Tracker::track(virtual_scan_box_model_hypotheses_t *box_model_hypotheses)
@@ -24,13 +30,13 @@ Tracks *Tracker::track(virtual_scan_box_model_hypotheses_t *box_model_hypotheses
 	update_neighborhood_graph(neighborhood_graph, box_model_hypotheses);
 	for (int n = 0; n < n_mc; n++)
 	{
-		Tracks *new_tracks = tracks_n->propose(neighborhood_graph);
+		Tracks *tracks_new = tracks_n->propose(neighborhood_graph);
 		
-		if (keep(new_tracks)) 
+		if (keep(tracks_new)) 
 		{
 			if (tracks_n != tracks_star)
 				delete tracks_n;
-			tracks_n = new_tracks;
+			tracks_n = tracks_new;
 			if (prob(tracks_n) > prob (tracks_star))
 			{
 				delete tracks_star;
@@ -38,7 +44,7 @@ Tracks *Tracker::track(virtual_scan_box_model_hypotheses_t *box_model_hypotheses
 			}		
 		}
 		else 
-			delete new_tracks;	
+			delete tracks_new;	
 	}
 	
 	return tracks_star;
