b6279462a0305e6b03f726fd24bf71b9e403adc2
car3 <newuser@car03.lcad.lan>
2017-12-15 18:38:19 -0200

    ENet results scripts - minor fixes

--- sharedlib/ENet/results/plot_accuracy.sh
+++ sharedlib/ENet/results/plot_accuracy.sh
@@ -7,11 +7,11 @@ EXAMPLES=`grep -m 1 -e "examples" $1 | cut -d' ' -f 8`
 BATCH=`grep -m 1 -e "batch_size:" $1 | awk '{$1=$1}1' | cut -d' ' -f 2`
 EPOCH=$(( $EXAMPLES / $BATCH + 2 * $EXAMPLES / $BATCH % 2 ))
 MAX_ITER=`grep -m 1 -e "max_iter:" $1 | cut -d: -f 2 | cut -d# -f 1 | awk '{$1=$1}1'`
-DISPLAY=`grep -m 1 -e "display" $1 | cut -d' ' -f 2`
+DISPLAY_RESULTS=`grep -m 1 -e "display" $1 | cut -d' ' -f 2`
 echo "set xlabel \"Batch # (1 batch = $BATCH examples, 1 epoch = $EPOCH batches, max_iter = $MAX_ITER)\" " > accuracy_$1.gp  
 echo "set ylabel \"Accuracy per Batch\" " >> accuracy_$1.gp  
 echo "set grid xtics ytics" >> accuracy_$1.gp
-echo "plot [][0.0:1.0]\"accuracy_$1\" u (\$0 * $DISPLAY):11 w l t \"$1\" " >> accuracy_$1.gp 
+echo "plot [][0.0:1.0]\"accuracy_$1\" u (\$0 * $DISPLAY_RESULTS):11 w l t \"$1\" " >> accuracy_$1.gp 
 echo "pause -1" >> accuracy_$1.gp
 echo "Press any key to finish..."
 gnuplot accuracy_$1.gp 
