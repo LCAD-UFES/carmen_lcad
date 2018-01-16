b690051d1305bb7819969a66c4981bf195733af6
Helio Perroni Filho <mail@xperroni.me>
2018-01-16 14:46:34 -0200

    bugfix: Allowing for Tracks variations to fail
    In some cases (e.g. right at the beginning when the current Tracks
    object is empty and only one Subgraph object exists) it may be
    impossible for Tracks::propose to produce any change. Originally this
    possibility wasn't contemplated in the code. Fixed by allowing
    Tracks::propose to report failure after trying every possible change (in
    random order), in which case the Tracker::track method aborts and
    returns the current tracking state.
    
    Signed-off-by: Helio Perroni Filho <mail@xperroni.me>

--- src/virtual_scan/tracker.cpp
+++ src/virtual_scan/tracker.cpp
@@ -24,7 +24,12 @@ ObstaclePose::S Tracker::track(carmen_mapper_virtual_scan_message *message)
 	Tracks::P tracks_n = tracks;
 	for (size_t n = 0; n < N_MC; n++)
 	{
+		// Try to create a variation of the current Tracls object,
+		// giving up on the MCMC operation if it can't be created.
 		Tracks::P tracks_new = tracks_n->propose(graph);
+		if (tracks_new.use_count() == 0)
+			break;
+
 		double U = uniform(RD);
 		if (U < (tracks_new->PwZ() / tracks_n->PwZ())) 
 		{
