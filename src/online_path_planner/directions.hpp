c93fcb907ed424bbedc22325ca78975a3bfa92f2
Lucas Coutinho de Souza Oliveira <lucascs.oliveira@gmail.com>
2018-03-13 18:44:54 -0300

    Initial commit for online_path_planner

---
+++ src/online_path_planner/directions.hpp
@@ -0,0 +1,13 @@
+#ifndef DIRECTIONS_HPP
+#define DIRECTIONS_HPP
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char* get_directions(const char* origin, const char* destination, const char* key);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
