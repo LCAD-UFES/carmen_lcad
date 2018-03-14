93f1705dfc67a4544c31b310924e08a04bab39ca
pepeaze <pepeaze@gmail.com>
2018-03-14 17:33:06 -0300

    mudança de nome na struct de ponto

--- src/rddf_graph/rddf_graph_utils.h
+++ src/rddf_graph/rddf_graph_utils.h
@@ -21,31 +21,37 @@
 extern "C" {
 #endif
 
-typedef struct t_point
+typedef struct rddf_graph_node
 {
 	int x;
 	int y;
+	rddf_graph_node *prox;
 
-} t_point;
+} rddf_graph_node;
 
 typedef struct type_list
 {
-	t_point p;
+	rddf_graph_node p;
 	type_list* prox;
 
 } t_list;
+
+using namespace std;
 
 cv::Mat rotate(cv::Mat src, cv::Point pt, double angle);
 void road_map_to_image(carmen_map_p map, cv::Mat *road_map_img);
 void road_map_to_image_black_and_white(carmen_map_p map, cv::Mat *road_map_img, const int class_bits);
 void road_map_find_center(carmen_map_p map);
 int **alloc_matrix(int r, int c);
-t_list *create_list();
-t_list *insert_in_list (t_list *l, t_point p);
-void print_list (t_list *l);
+//t_list *create_list();
+//t_list *insert_in_list (t_list *l, t_point p);
+void print_list (rddf_graph_node *l);
 bool is_center (carmen_map_p map, int x, int y, unsigned short *next_lane_center);
-void check_neighbours(int x, int y, carmen_map_p map, int **already_visited, t_list *list, bool has_point);
-void print_open_set(std::vector<t_point> &open_set);
+//void check_neighbours(int x, int y, carmen_map_p map, int **already_visited, t_list *list, bool has_point);
+void print_open_set(std::vector<rddf_graph_node*> &open_set);
+bool point_is_in_map(carmen_map_p map, int i, int j);
+void expand_neighbours(carmen_map_p map, std::vector<t_list> *open_set, std::vector<t_list> *closed_set, int **already_visited);
+void road_map_find_center(carmen_map_p map);
 
 #ifdef __cplusplus
 }
