26e42ea87a2cbcf01bb49a65ed019ef03b67ebc8
car01 <car01@lcad.inf.ufes.br>
2018-03-28 19:15:47 -0300

    acertos includes ocv convert log

--- src/utilities/convert_log_images/to_png_new_log.cpp
+++ src/utilities/convert_log_images/to_png_new_log.cpp
@@ -3,6 +3,10 @@
 #include <opencv/highgui.h>
 #include <locale.h>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 using namespace cv;
 
 
