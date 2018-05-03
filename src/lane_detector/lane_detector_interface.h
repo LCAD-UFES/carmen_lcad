eea877089833b05a4a61ab45153a7a31d96ff1b9
lcad <marcelo.bp01@gmail.com>
2018-05-03 20:21:08 -0300

    adicionando interface no lane_detector

---
+++ src/lane_detector/lane_detector_interface.h
@@ -0,0 +1,18 @@
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// UNSUBSCRIBES
+void carmen_elas_lane_analysis_unsubscribe(carmen_handler_t handler);
+
+// SUBSCRIBES
+void carmen_elas_lane_analysis_subscribe(carmen_lane_detector_lane_message_t *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+
+// PUBLISHES
+IPC_RETURN_TYPE carmen_lane_publish_message(carmen_lane_detector_lane_message_t *message);
+
+#ifdef __cplusplus
+}
+#endif
