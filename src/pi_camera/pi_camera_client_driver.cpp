68799600d8f2b8fe85519be3f14508f338618234
car01 <car01@lcad.inf.ufes.br>
2018-06-13 14:21:51 -0300

    cliente sempre reconectando OK

--- src/pi_camera/pi_camera_client_driver.cpp
+++ src/pi_camera/pi_camera_client_driver.cpp
@@ -9,23 +9,23 @@
 
 
 char* tcp_ip_address;
-char* port;
 
+#define PORT "3457"
 
 using namespace std;
 using namespace cv;
 
 
 int
-stablished_connection_with_server()
+stablished_connection_with_server(char *cam_config, int cam_config_size)
 {
 	struct addrinfo host_info;       // The struct that getaddrinfo() fills up with data.
 	struct addrinfo *host_info_list; // Pointer to the to the linked list of host_info's.
-	int sock = 0, status;
+	int pi_socket = 0, status;
 
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if ((pi_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		printf("\n--- Socket creation error! ---\n");
+		printf("--- Socket creation error! ---\n");
 		return -1;
 	}
 
@@ -34,38 +34,40 @@ stablished_connection_with_server()
 	host_info.ai_family = AF_UNSPEC;     // IP version not specified. Can be both.
 	host_info.ai_socktype = SOCK_STREAM; // Use SOCK_STREAM for TCP or SOCK_DGRAM for UDP.
 
-	status = getaddrinfo(tcp_ip_address, port, &host_info, &host_info_list);
+	status = getaddrinfo(tcp_ip_address, PORT, &host_info, &host_info_list);
 
 	if (status != 0)
 	{
 		printf("--- Get_Addrinfo ERROR! ---\n");
 		return (-1);
 	}
-	status = connect(sock, host_info_list->ai_addr, host_info_list->ai_addrlen);
+	status = connect(pi_socket, host_info_list->ai_addr, host_info_list->ai_addrlen);
 
 	if(status < 0)
 	{
-		printf("\n--- Connection Failed! ---\n");
+		printf("--- Connection Failed! ---\n");
 		return (-1);
 	}
 
-	printf("--- Connection stablished successfully! ---\n\n");
+	printf("--- Connection established successfully! ---\n");
 
-	return (sock);
+	send(pi_socket, cam_config, cam_config_size, MSG_NOSIGNAL);
+
+	return (pi_socket);
 }
 
 
 int
-trying_to_reconnect(int socket)
+trying_to_reconnect(char *cam_config, int cam_config_size)
 {
-	socket = stablished_connection_with_server();
-	while (socket == -1)
+	int pi_socket = stablished_connection_with_server(cam_config, cam_config_size);
+
+	while (pi_socket == -1)
 	{
-		printf("Server down... Trying to reconnect\n");
-		sleep(1);
-		socket = stablished_connection_with_server();
+		sleep(3);
+		pi_socket = stablished_connection_with_server(cam_config, cam_config_size);
 	}
-	return (socket);
+	return (pi_socket);
 }
 
 
@@ -79,7 +81,8 @@ trying_to_reconnect(int socket)
 
 
 void
-publish_image_message(int pi_socket, int camera_number, carmen_bumblebee_basic_stereoimage_message msg)
+publish_image_message(int pi_socket, int camera_number, carmen_bumblebee_basic_stereoimage_message msg,
+		char *cam_config, int cam_config_size)
 {
 	int valread;
 
@@ -91,7 +94,7 @@ publish_image_message(int pi_socket, int camera_number, carmen_bumblebee_basic_s
 		if (valread == 0)
 		{
 			// Connection lost due to server shutdown.
-			pi_socket = trying_to_reconnect(pi_socket);
+			pi_socket = trying_to_reconnect(cam_config, cam_config_size);
 			continue;
 		}
 		else if ((valread == -1) || (valread != msg.image_size))
@@ -100,8 +103,8 @@ publish_image_message(int pi_socket, int camera_number, carmen_bumblebee_basic_s
 		msg.timestamp = carmen_get_time();
 		carmen_bumblebee_basic_publish_message(camera_number, &msg);
 
-		//imshow("Pi Camera Driver", Mat(image_height, image_width, CV_8UC3, msg.raw_left, 3 * 640));
-		//waitKey(1);
+		imshow("Pi Camera Driver", Mat(msg.height, msg.width, CV_8UC3, msg.raw_left, 3 * 640));
+		waitKey(1);
 	}
 }
 
@@ -152,13 +155,12 @@ read_parameters(int argc, char **argv, carmen_bumblebee_basic_stereoimage_messag
 		{pi_camera_number, (char*)"brightness", CARMEN_PARAM_INT,    &brightness,     0, NULL},
 		{pi_camera_number, (char*)"contrast",   CARMEN_PARAM_INT,    &contrast,       0, NULL},
 		{pi_camera_number, (char*)"ip",         CARMEN_PARAM_STRING, &tcp_ip_address, 0, NULL},
-		{pi_camera_number, (char*)"port",       CARMEN_PARAM_STRING, &port,           0, NULL},
 	};
 
 	int num_items = sizeof(param_list)/sizeof(param_list[0]);
 	carmen_param_install_params(argc, argv, param_list, num_items);
 
-	sprintf(cam_config, "%d*%d*%d*%d*%d*%d*", msg->width, msg->height, frame_rate, brightness, contrast, port);
+	sprintf(cam_config, "%d*%d*%d*%d*%d*", msg->width, msg->height, frame_rate, brightness, contrast);
 
 	return (camera_number);
 }
@@ -173,7 +175,7 @@ initialize_message(carmen_bumblebee_basic_stereoimage_message *msg)
 	msg->raw_right = msg->raw_left;  // This is a monocular camera, both pointers point to the same image
 	msg->host = carmen_get_host();
 
-	printf("\nWidth %d Height %d Image Size %d Is Rectified %d Host %s\n\n", msg->width, msg->height, msg->image_size, msg->isRectified, msg->host);
+	//printf("\nWidth %d Height %d Image Size %d Is Rectified %d Host %s\n\n", msg->width, msg->height, msg->image_size, msg->isRectified, msg->host);
 }
 
 
@@ -192,9 +194,7 @@ main(int argc, char **argv)
 
 	carmen_bumblebee_basic_define_messages(camera_number);
 
-	int pi_socket = stablished_connection_with_server();
+	int pi_socket = stablished_connection_with_server(cam_config, 64);
 
-	send(pi_socket, cam_config, 64, MSG_NOSIGNAL);
-
-	publish_image_message(pi_socket, camera_number, msg);
+	publish_image_message(pi_socket, camera_number, msg, cam_config, 64);
 }
