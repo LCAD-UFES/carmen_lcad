accf2cf54f226494548797421e9d5acb0fff7df5
Filipe Mutz <filipemtz@gmail.com>
2018-09-07 17:03:01 -0300

    hyperparameter adjustment

--- src/rl_motion_planner/rl/envs.py
+++ src/rl_motion_planner/rl/envs.py
@@ -13,7 +13,7 @@ class SimpleEnv:
 
         if self.params['model'] == 'ackerman':
             self.env_size = 100.0
-            self.zoom = 4.0
+            self.zoom = 3.0
             self.max_speed = 10.0
             self.wheel_angle = np.deg2rad(28.)
             self.goal_radius = 2.0
