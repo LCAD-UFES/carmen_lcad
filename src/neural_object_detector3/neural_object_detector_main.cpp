f38f630c267f579626aec9de391a9c99cac7fda9
ranikguidolini <ranikguidolini@gmail.com>
2018-10-27 17:35:13 -0300

    melhorias NOD3

--- src/neural_object_detector3/neural_object_detector_main.cpp
+++ src/neural_object_detector3/neural_object_detector_main.cpp
@@ -13,6 +13,10 @@
 //#include "Darknet.hpp"
 #include "neural_object_detector.hpp"
 
+#include <carmen/darknet.h>
+
+detection *dets;
+
 using namespace std;
 using namespace cv;
 
@@ -668,7 +672,7 @@ call_python_function(int width, int height, unsigned char *image, vector<bbox_t>
 }
 ///////////////
 
-
+/*
 void
 image_handler(carmen_bumblebee_basic_stereoimage_message *image_msg)
 {
@@ -707,7 +711,7 @@ image_handler(carmen_bumblebee_basic_stereoimage_message *image_msg)
 	imshow("NOD", open_cv_image);
 	waitKey(1);
 
-	/*vector<bbox_t> predictions = darknet->detect(open_cv_image, 0.2);  // Arguments (image, threshold)
+	vector<bbox_t> predictions = darknet->detect(open_cv_image, 0.2);  // Arguments (image, threshold)
 	predictions = filter_predictions_of_interest(predictions);
 
 
@@ -745,7 +749,83 @@ image_handler(carmen_bumblebee_basic_stereoimage_message *image_msg)
 		//printf("%d %d %d\n", (int) predictions.size(), (int) points_inside_bbox.size(), (int) filtered_points.size());
 
 		show_detections(open_cv_image, predictions, points, points_inside_bbox, filtered_points, fps, image_msg->width, image_msg->height, crop_x, crop_y, crop_w, crop_h);
-	}*/
+	}
+}
+*/
+
+
+void
+image_handler(carmen_bumblebee_basic_stereoimage_message *image_msg)
+
+//test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filename, float thresh, float hier_thresh, char *outfile, int fullscreen)
+{
+	char *datacfg;
+	char *cfgfile;
+	char *weightfile;
+	char *filename;
+	float thresh;
+	float hier_thresh;
+	char *outfile;
+	int fullscreen;
+
+
+    list *options = read_data_cfg(datacfg);
+    char *name_list = option_find_str(options, "names", "data/names.list");
+    char **names = get_labels(name_list);
+
+    image **alphabet = load_alphabet();
+    network *net = load_network(cfgfile, weightfile, 0);
+    set_batch_network(net, 1);
+    srand(2222222);
+    double time;
+    char buff[256];
+    char *input = buff;
+    float nms=.45;
+    while(1){
+        if(filename){
+            strncpy(input, filename, 256);
+        } else {
+            printf("Enter Image Path: ");
+            fflush(stdout);
+            input = fgets(input, 256, stdin);
+            if(!input) return;
+            strtok(input, "\n");
+        }
+        image im = load_image_color(input,0,0);
+        image sized = letterbox_image(im, net->w, net->h);
+        //image sized = resize_image(im, net->w, net->h);
+        //image sized2 = resize_max(im, net->w);
+        //image sized = crop_image(sized2, -((net->w - sized2.w)/2), -((net->h - sized2.h)/2), net->w, net->h);
+        //resize_network(net, sized.w, sized.h);
+        layer l = net->layers[net->n-1];
+
+
+        float *X = sized.data;
+        time=what_time_is_it_now();
+        network_predict(net, X);
+        printf("%s: Predicted in %f seconds.\n", input, what_time_is_it_now()-time);
+        int nboxes = 0;
+        detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
+        //printf("%d\n", nboxes);
+        //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
+        if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
+        draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
+        free_detections(dets, nboxes);
+        if(outfile){
+            save_image(im, outfile);
+        }
+        else{
+            save_image(im, "predictions");
+#ifdef OPENCV
+            make_window("predictions", 512, 512, 0);
+            show_image(im, "predictions", 0);
+#endif
+        }
+
+        free_image(im);
+        free_image(sized);
+        if (filename) break;
+    }
 }
 
 
@@ -882,3 +962,5 @@ main(int argc, char **argv)
 
     return 0;
 }
+
+
