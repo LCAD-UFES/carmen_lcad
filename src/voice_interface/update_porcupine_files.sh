0e6b43b33e14841ee99e05e3a3be05ce5b817d4e
car01 <alberto@lcad.inf.ufes.br>
2019-07-25 13:10:13 -0300

    update porcupine file script && remove temp files from git

--- src/voice_interface/update_porcupine_files.sh
+++ src/voice_interface/update_porcupine_files.sh
@@ -3,18 +3,18 @@
 echo "Initializing update..."
 
 echo "Removing old file..."
-rm -rf ~/carmen_lcad/lib/libpv_porcupine.a
+rm -rf $CARMEN_HOME/lib/libpv_porcupine.a
 
 echo "Training data.."
-~/carmen_packages/Porcupine/tools/optimizer/linux/x86_64/pv_porcupine_optimizer -r ~/carmen_packages/Porcupine/resources/optimizer_data/ -p linux -o . -w "ok e ara" 
+$CARMEN_PACKAGES/Porcupine/tools/optimizer/linux/x86_64/pv_porcupine_optimizer -r $CARMEN_PACKAGES/Porcupine/resources/optimizer_data/ -p linux -o $CARMEN_PACKAGES/Porcupine/ -w "ok e ara" 
 
 echo "Copying files..."
-cp ~/carmen_packages/Porcupine/ok\ e\ ara_linux.ppn $CARMEN_HOME/data/voice_interface_data/hotword_oi_iara.ppn
-cp ~/carmen_packages/Porcupine/lib/common/porcupine_params.pv $CARMEN_HOME/data/voice_interface_data/
-cp ~/carmen_packages/Porcupine/include/picovoice.h $CARMEN_HOME/src/voice_interface/
-cp ~/carmen_packages/Porcupine/lib/linux/x86_64/libpv_porcupine.a libpv_porcupine.a
+cp $CARMEN_PACKAGES/Porcupine/ok\ e\ ara_linux.ppn $CARMEN_HOME/data/voice_interface_data/hotword_oi_iara.ppn
+cp $CARMEN_PACKAGES/Porcupine/lib/common/porcupine_params.pv $CARMEN_HOME/data/voice_interface_data/
+cp $CARMEN_PACKAGES/Porcupine/include/picovoice.h $CARMEN_HOME/src/voice_interface/
+cp $CARMEN_PACKAGES/Porcupine/lib/linux/x86_64/libpv_porcupine.a libpv_porcupine.a
 
 echo "Linking file..."
-ln -s ~/carmen_packages/Porcupine/libpv_porcupine.a $CARMEN_HOME/lib/libpv_porcupine.a
+ln -s $CARMEN_PACKAGES/Porcupine/libpv_porcupine.a $CARMEN_HOME/lib/libpv_porcupine.a
 
 echo "Porcupine updated!"
