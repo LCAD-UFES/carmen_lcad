19f684cc8284e038cb2967dca6dfe6e161e79e4f
albertodesouza <alberto@lcad.inf.ufes.br>
2019-10-03 17:16:21 -0300

    remocao de ojEcotech4

--- sharedlib/OpenJAUS/ojEcoTech4/nodemanage.bat
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/OpenJAUS/libopenJaus/lib:~/OpenJAUS/libjaus/lib:~/OpenJAUS/ojTorc/lib:~/OpenJAUS/ojIARASim/lib
-/home/pi/OpenJAUS/ojNodeManager/bin/ojNodeManager /home/pi/OpenJAUS/ojNodeManager/nodeManager_EcoTech4.conf a
