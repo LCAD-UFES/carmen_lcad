3602fbfd78409b755d2dcd81f85e009aa24b11ff
gnunesm <gabnunes2112@gmail.com>
2019-11-20 18:42:00 -0300

    code for optimizing spline

---
+++ src/utilities/mount_rddf_predict_python/rddf_predict_optimizer.h
@@ -0,0 +1,44 @@
+#ifndef SRC_UTILITIES_MOUNT_RDDF_PREDICT_PYTHON_RDDF_PREDICT_OPTIMIZER_H_
+#define SRC_UTILITIES_MOUNT_RDDF_PREDICT_PYTHON_RDDF_PREDICT_OPTIMIZER_H_
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <gsl/gsl_errno.h>
+#include <gsl/gsl_spline.h>
+#include <gsl/gsl_multimin.h>
+#include <carmen/carmen.h>
+
+struct SplineControlParams
+{
+	bool valid;
+	double xtotal;
+	double k2;
+	double k3;
+	double k1;
+};
+
+struct ObjectiveFunctionParams
+{
+	SplineControlParams *spline_control_params;
+	unsigned int n_poses;
+	carmen_ackerman_traj_point_t * rddf_poses;
+	double plan_cost;
+	int xtotal;
+};
+
+struct TrajectoryDimensions
+{
+	double dist;	// Distance traveled in polar coordinates
+	double theta;	// Angle in polar coordinates
+	double d_yaw;	// Displacement in yaw
+	double phi_i;	// Initial steering wheel angle
+	double v_i;		// Initial velocity
+
+	struct TrajectoryControlParameters control_parameters;
+};
+
+SplineControlParams
+optimize_spline_knots(carmen_behavior_selector_road_profile_message *last_rddf_poses);
+
+#endif /* SRC_UTILITIES_MOUNT_RDDF_PREDICT_PYTHON_RDDF_PREDICT_OPTIMIZER_H_ */
