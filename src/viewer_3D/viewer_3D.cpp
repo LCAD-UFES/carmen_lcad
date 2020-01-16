897cb8e05827fb009d6ee63494dbdefc669e3ac9
albertodesouza <alberto@lcad.inf.ufes.br>
2020-01-16 11:02:47 -0300

    pi_imu funcionando e calibrada

--- src/viewer_3D/viewer_3D.cpp
+++ src/viewer_3D/viewer_3D.cpp
@@ -2701,11 +2701,7 @@ draw_loop(window *w)
         if (draw_xsens_orientation_flag)
         {
             glColor3f(0.4, 1.0, 0.4);
-//            printf("%lf %lf\n", xsens_orientation.yaw, car_fused_pose.orientation.yaw);
-//            draw_xsens_orientation(xsens_orientation, xsens_yaw_bias, xsens_pose, sensor_board_1_pose, car_fused_pose);
             draw_xsens_orientation(xsens_orientation, magnetic_declination, xsens_pose, sensor_board_1_pose, car_fused_pose);
-//            glColor3f(1.0, 0.4, 0.4);
-//            draw_xsens_orientation(xsens_orientation, 0.0, xsens_pose, sensor_board_1_pose, car_fused_pose);
         }
         //draw_orientation_instruments(car_fused_pose.orientation, 1.0, 1.0, 0.0);
         //draw_orientation_instruments(xsens_orientation, xsens_yaw_bias, 1.0, 0.5, 0.0);
@@ -2991,7 +2987,7 @@ draw_loop_for_picking(window *w)
             draw_laser_rays(laser_points[last_laser_position], get_laser_position(car_fused_pose.position));
         }
 
-        if(draw_moving_objects_flag)
+        if (draw_moving_objects_flag)
         {
            carmen_vector_3D_t offset = get_position_offset();
            offset.z += sensor_board_1_pose.position.z;
