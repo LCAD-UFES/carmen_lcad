b4a14b29f8d2a66d5b4606324d8f3b782bf421ae
Anderson Mozart <andersonmozart75@gmail.com>
2020-03-04 18:22:12 -0300

    A-star, iniciando os módulos básicos

--- src/path_planner_astar/path_planner_astar_main.cpp
+++ src/path_planner_astar/path_planner_astar_main.cpp
@@ -3,6 +3,25 @@
 carmen_point_t *final_goal = NULL;
 carmen_localize_ackerman_globalpos_message *current_globalpos_msg = NULL;
 carmen_robot_ackerman_config_t robot_config;
+carmen_obstacle_distance_mapper_map_message distance_map;
+
+
+state_node*
+create_state_node(double x, double y, double theta, double v, double phi, double g, double h, state_node *parent)
+{
+	state_node *new_state = (state_node*) malloc(sizeof(state_node));
+	new_state->state.x = x;
+	new_state->state.y = y;
+	new_state->state.theta = theta;
+	new_state->state.v = v;
+	new_state->state.phi = phi;
+	new_state->f = g+h;
+	new_state->g = g;
+	new_state->h = h;
+	new_state->parent = parent;
+
+	return (new_state);
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -15,21 +34,8 @@ pos_message_handler(carmen_point_t robot_pose)
 {
 	if (final_goal != NULL)
 	{
-//		velocity = 2;
-		carmen_point_t fgoal;
-//		fgoal.x = 7757757.31;     // Reta LCAD
-//		fgoal.y = -363538.69;
-//		fgoal.theta = 0.628;
-
-//		fgoal.x = 7757833.72;     // Depois da preimeira curva
-//		fgoal.y = -363533.71;
-//		fgoal.theta = -0.692;
-
-		 fgoal.x = 7757865.52;     // Estacionamento Ambiental
-		 fgoal.y = -363539.93;
-		 fgoal.theta = -0.741;
-//       compute_path(&robot_pose,/* final_goal */ &fgoal , robot_config, &distance_map);
-
+		printf("Pos_message_handler: %lf %lf %lf\n", robot_pose.x, robot_pose.y, robot_pose.theta);
+		compute_astar_path(&robot_pose, final_goal, robot_config ,&distance_map);
 		final_goal = NULL;
 	}
 }
@@ -47,8 +53,31 @@ void
 carmen_rddf_play_end_point_message_handler(carmen_rddf_end_point_message *rddf_end_point_message)
 {
 	final_goal = &(rddf_end_point_message->point);
-
+	printf("carmen_rddf_play_end_point: %lf %lf %lf\n", rddf_end_point_message->point.x, rddf_end_point_message->point.y, rddf_end_point_message->point.theta);
 //	printf ("Recebeu Goal!!!!!  %d\n", rddf_end_point_message->number_of_poses);
+}
+
+
+static void
+carmen_obstacle_distance_mapper_compact_map_message_handler(carmen_obstacle_distance_mapper_compact_map_message *message)
+{
+	static carmen_obstacle_distance_mapper_compact_map_message *compact_distance_map = NULL;  // TODO is it possible to remove this line and the if
+
+	if (compact_distance_map == NULL)
+	{
+		carmen_obstacle_distance_mapper_create_new_map(&distance_map, message->config, message->host, message->timestamp);
+		compact_distance_map = (carmen_obstacle_distance_mapper_compact_map_message *) (calloc(1, sizeof(carmen_obstacle_distance_mapper_compact_map_message)));
+		carmen_obstacle_distance_mapper_cpy_compact_map_message_to_compact_map(compact_distance_map, message);
+		carmen_obstacle_distance_mapper_uncompress_compact_distance_map_message(&distance_map, message);
+	}
+	else
+	{
+		carmen_obstacle_distance_mapper_clear_distance_map_message_using_compact_map(&distance_map, compact_distance_map, DISTANCE_MAP_HUGE_DISTANCE);
+		carmen_obstacle_distance_mapper_free_compact_distance_map(compact_distance_map);
+		carmen_obstacle_distance_mapper_cpy_compact_map_message_to_compact_map(compact_distance_map, message);
+		carmen_obstacle_distance_mapper_uncompress_compact_distance_map_message(&distance_map, message);
+	}
+	printf("Mapa recebido!\n");
 }
 
 
@@ -57,7 +86,7 @@ shutdown_module(int signo)
 {
     if (signo == SIGINT) {
         carmen_ipc_disconnect();
-        printf("Subscribe_example: Disconnected.\n");
+        printf("path_planner_astar_main: Disconnected.\n");
         exit(0);
     }
 }
@@ -75,11 +104,27 @@ carmen_rddf_play_subscribe_messages()
 {
 	carmen_localize_ackerman_subscribe_globalpos_message(NULL, (carmen_handler_t) localize_globalpos_handler, CARMEN_SUBSCRIBE_LATEST);
 
+	carmen_obstacle_distance_mapper_subscribe_compact_map_message(NULL, (carmen_handler_t) carmen_obstacle_distance_mapper_compact_map_message_handler, CARMEN_SUBSCRIBE_LATEST);
+
 	carmen_rddf_subscribe_end_point_message(NULL, (carmen_handler_t) carmen_rddf_play_end_point_message_handler, CARMEN_SUBSCRIBE_LATEST);
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+void
+compute_astar_path(carmen_point_t *robot_pose, carmen_point_t *goal_pose, carmen_robot_ackerman_config_t robot_config,
+		carmen_obstacle_distance_mapper_map_message *distance_map)
+{
+	state_node *start_state, *goal_state;
+
+	start_state = create_state_node(robot_pose->x, robot_pose->y, robot_pose->theta, 0.0, 0.0, 0.0, DIST2D_P(robot_pose, goal_pose), NULL);
+	goal_state = create_state_node(goal_pose->x, goal_pose->y, goal_pose->theta, 0.0, 0.0, DBL_MAX, DBL_MAX, NULL);
+	std::vector<state_node> path;
+
+
+}
 
 
 static void
