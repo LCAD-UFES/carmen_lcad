5e3d11878b816a9d7089bc2559a9920867f4c108
Raphael Vivacqua Carneiro <carneiro.raphael@gmail.com>
2020-05-05 23:26:06 -0300

    correcao formula de calculo do phi

--- src/path_planner_astar/path_planner_astar_heap.cpp
+++ src/path_planner_astar/path_planner_astar_heap.cpp
@@ -100,7 +100,7 @@ calculate_phi_ahead(carmen_ackerman_traj_point_t *path, int num_poses)
 			path[i].phi = 0.0;
 			continue;
 		}
-		path[i].phi = L * atan(delta_theta / l);
+		path[i].phi = atan(L * (delta_theta / l));
 	}
 
 	for (int i = 1; i < (num_poses - 1); i++)
