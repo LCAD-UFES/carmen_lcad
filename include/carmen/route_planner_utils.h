fc8a5432b14c0c4ab72902c6260d3702f26b2ae6
pepeaze <pepeaze@gmail.com>
2020-05-13 10:59:25 -0300

    new executable of route planner and its libs

--- include/carmen/route_planner_utils.h
+++ include/carmen/route_planner_utils.h
@@ -5,6 +5,7 @@
 #include <carmen/grid_mapping.h>
 #include <carmen/rddf_interface.h>
 #include <iostream>
+#include <algorithm>
 #include <list>
 #include <string>
 #include <queue>
@@ -40,10 +41,53 @@
 extern "C" {
 #endif
 
+#define new_node (t_adjacent_list*)malloc(sizeof(t_adjacent_list))
+
 using namespace std;
 using namespace cv;
 
+struct type_adjacent_list{
+    int vertex;
+    double cost;
+    struct type_adjacent_list *prox;
+};
+typedef struct type_adjacent_list t_adjacent_list;
+
+
+struct type_a_star_utils{
+    int *g_score;
+    int *distancia;
+    int *anterior;
+    int *closed_set;
+    int *open_set;
+    int *fechado;
+    int anterior_size;
+    int path_g_n;
+    int closed_set_size;
+    int open_set_size;
+};
+typedef struct type_a_star_utils t_a_star_utils;
+
+
+typedef struct{
+    char v;
+    int vertex;
+    int x;
+    int y;
+}t_coords;
+
 void call_osmnx_python_func (Gdc_Coord_3d origin_gdc, Gdc_Coord_3d destination_gdc);
+int* alloc_array (int graph_size);
+float* alloc_array_float (int graph_size);
+int** alloc_matrix (int graph_size);
+t_a_star_utils add_vertex_to_closed_set(t_a_star_utils r, int vertex_id);
+t_a_star_utils add_vertex_to_open_set(t_a_star_utils r, int vertex_id);
+float h_n_manh (int vertex_id, int vertex_end, t_graph graph);
+float h_n_eucl (int vertex_id, int vertex_end, t_graph graph);
+t_a_star_utils a_star(t_adjacent_list** adjacent_list, t_graph graph, int graph_size, int vertex_ini, int vertex_end);
+t_adjacent_list **add_to_list_undir(t_adjacent_list **adjacent_list, int u, int v, int w);
+t_adjacent_list **create_adjacent_list(t_adjacent_list ** adjacent_list, t_graph graph);
+int find_closest_point_in_graph (t_graph graph, carmen_point_t point);
 carmen_point_t set_destination(vector<carmen_annotation_t> annotations);
 void get_annotation_from_rddf(char *allrddf, vector<carmen_annotation_t> &annotations);
 
