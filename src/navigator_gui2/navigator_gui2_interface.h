cf5682f0c36dc0a306e57218385703d1a4d9cd59
Raphael Vivacqua Carneiro <carneiro.raphael@gmail.com>
2020-08-25 00:46:48 -0300

    headers de interface com o front-end

--- src/navigator_gui2/navigator_gui2_interface.h
+++ src/navigator_gui2/navigator_gui2_interface.h
@@ -10,11 +10,22 @@ extern "C"
 
 void carmen_navigator_gui_define_path_message();
 
+void carmen_navigator_gui_subscribe_path_message(carmen_navigator_gui_path_message *msg,
+		carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+
+void carmen_navigator_gui_unsubscribe_path_message(carmen_handler_t handler);
+
 void carmen_navigator_gui_publish_path_message(carmen_navigator_gui_path_message *msg);
 
-void carmen_navigator_gui_subscribe_path_message(carmen_navigator_gui_path_message *msg,
-		carmen_handler_t handler,
-		carmen_subscribe_t subscribe_how);
+void carmen_navigator_gui2_define_map_view_message();
+
+void carmen_navigator_gui2_subscribe_map_view_message(carmen_navigator_gui2_map_view_message *msg,
+		carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+
+void carmen_navigator_gui2_unsubscribe_map_view_message(carmen_handler_t handler);
+
+void carmen_navigator_gui2_publish_map_view_message(carmen_navigator_gui2_map_view_message *msg);
+
 
 #ifdef __cplusplus
 }
--- src/navigator_gui2/navigator_gui2_messages.h
+++ src/navigator_gui2/navigator_gui2_messages.h
@@ -14,6 +14,21 @@ extern "C"
 {
 #endif
 
+
+typedef struct {
+  int width;					/* The x dimension of the image in pixels */
+  int height;					/* The y dimension of the image in pixels */
+  int image_size;				/* width*height*bytes_per_pixel */
+  unsigned char *raw_image;
+  double timestamp;
+  char *host;
+} carmen_navigator_gui2_map_view_message;
+
+
+#define      CARMEN_NAVIGATOR_GUI2_MAP_VIEW_NAME       "carmen_navigator_gui2_map_view"
+#define      CARMEN_NAVIGATOR_GUI2_MAP_VIEW_FMT        "{int,int,int,<ubyte:3>,double,string}"
+
+
 #ifdef __cplusplus
 }
 #endif
