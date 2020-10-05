effc9b3f315e006cf27b94ce994238458dbc626f
Raphael Vivacqua Carneiro <carneiro.raphael@gmail.com>
2020-10-04 22:54:59 -0300

    carmen_rddf_play_updade_annotation_vector

--- src/rddf/rddf_interface.h
+++ src/rddf/rddf_interface.h
@@ -16,6 +16,7 @@ extern "C"
     void carmen_rddf_subscribe_end_point_message(carmen_rddf_end_point_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
     void carmen_rddf_subscribe_waypoints_around_end_point_message(carmen_rddf_waypoints_around_end_point_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
     void carmen_rddf_subscribe_add_annotation_message(carmen_rddf_add_annotation_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+    void carmen_rddf_subscribe_update_annotation_message(carmen_rddf_update_annotation_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
     void carmen_rddf_subscribe_dynamic_annotation_message(carmen_rddf_dynamic_annotation_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
     void carmen_rddf_subscribe_annotation_message(carmen_rddf_annotation_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
     void carmen_rddf_subscribe_traffic_sign_message(carmen_rddf_traffic_sign_message *message, carmen_handler_t handler, carmen_subscribe_t subscribe_how);
@@ -24,6 +25,7 @@ extern "C"
     void carmen_rddf_unsubscribe_end_point_message(carmen_handler_t handler);
     void carmen_rddf_unsubscribe_waypoints_around_end_point_message(carmen_handler_t handler);
     void carmen_rddf_unsubscribe_add_annotation_message(carmen_handler_t handler);
+    void carmen_rddf_unsubscribe_update_annotation_message(carmen_handler_t handler);
     void carmen_rddf_unsubscribe_dynamic_annotation_message(carmen_handler_t handler);
     void carmen_rddf_unsubscribe_annotation_message(carmen_handler_t handler);
     void carmen_rddf_unsubscribe_traffic_sign_message(carmen_handler_t handler);
@@ -34,6 +36,7 @@ extern "C"
     void carmen_rddf_publish_end_point_message(int number_of_poses_desired, carmen_point_t point);
     void carmen_rddf_publish_road_profile_around_end_point_message(carmen_ackerman_traj_point_t *poses_around_end_point, int num_poses);
     void carmen_rddf_publish_add_annotation_message(carmen_vector_3D_t annotation_point, double orientation, char *annotation_description, int annotation_type, int annotation_code);
+    void carmen_rddf_publish_update_annotation_message(crud_t action, carmen_annotation_t old_annotation, carmen_annotation_t new_annotation);
     void carmen_rddf_publish_dynamic_annotation_message(carmen_vector_3D_t annotation_point, double orientation, char *annotation_description, int annotation_type, int annotation_code, double timestamp);
     void carmen_rddf_publish_annotation_message(carmen_annotation_t *annotations, int num_annotations);
     void carmen_rddf_publish_traffic_sign_message(int traffic_sign_state, double traffic_sign_data);
