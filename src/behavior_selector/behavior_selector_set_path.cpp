66f3fdfd2ce56a3259105872d1180b76d9a17338
albertodesouza <alberto@lcad.inf.ufes.br>
2020-10-14 14:51:08 -0300

    codigo para teste do Ranik sem objetos moveis

--- src/behavior_selector/behavior_selector_set_path.cpp
+++ src/behavior_selector/behavior_selector_set_path.cpp
@@ -607,7 +607,7 @@ set_optimum_path(carmen_frenet_path_planner_set_of_paths *current_set_of_paths,
 		carmen_moving_objects_point_clouds_message *current_moving_objects,
 		carmen_ackerman_traj_point_t current_robot_pose_v_and_phi, int who_set_the_goal_v, double timestamp)
 {
-	if (1)//!current_set_of_paths)
+	if (!current_set_of_paths || behavior_selector_use_symotha)
 	{
 		vector<path_collision_info_t> no_collision_info = {};
 		return (no_collision_info);
