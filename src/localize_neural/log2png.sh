1e3b327258e9dd2ceb159d4120d141cd54e8a41c
thiagorjes <thiago_rj_es@hotmail.com>
2020-11-01 10:56:31 -0300

    correção em log2png.sh para criar imagem disparity.png

--- src/localize_neural/log2png.sh
+++ src/localize_neural/log2png.sh
@@ -1,40 +1,64 @@
 #!/usr/bin/env bash
 
+# declare -a logs=(
+# "/dados/log_voltadaufes-20160825.txt" 
+# "/dados/log_voltadaufes-20160830.txt"
+# "/dados/log_voltadaufes-20160906-ponte.txt"
+# "/dados/log_voltadaufes-tracker-20161021.txt"
+# "/dados/log_voltadaufes-tracker-20161112-estacionamento-1.txt"
+# "/dados/log_voltadaufes-tracker-20161112-estacionamento.txt"
+# "/dados/log_ponte-20170220-2.txt"
+# "/dados/log_guarapari-20170403.txt"
+# "/dados/log_ponte-20170220.txt"
+# "/dados/log_ponte-20161228.txt"
+# )
+
+# declare -a dirs=(
+# "/dados/ufes/20160825"
+# "/dados/ufes/20160830"
+# "/dados/ufes/20160906-02"
+# "/dados/ufes/20161021"
+# "/dados/ufes/20161112-01"
+# "/dados/ufes/20161112-00"
+# "/dados/ufes/20170220-02"
+# "/dados/ufes/20170403"
+# "/dados/ufes/20170220"
+# "/dados/ufes/20161228"
+# )
+
+
+
+# declare -a cams=(
+# 3 3 3 3 3 3 3 3 3 3
+# )
+
+# declare -a crops=(
+# 380 380 380 380 380 380 380 380 380 380
+# )
+
+# declare -a fmts=(1 1 1 1 1 1 1 1 1 1)
+
+
+
+
 declare -a logs=(
 "/dados/log_voltadaufes-20160825.txt" 
-"/dados/log_voltadaufes-20160830.txt"
-"/dados/log_voltadaufes-20160906-ponte.txt"
-"/dados/log_voltadaufes-tracker-20161021.txt"
-"/dados/log_voltadaufes-tracker-20161112-estacionamento-1.txt"
-"/dados/log_voltadaufes-tracker-20161112-estacionamento.txt"
-"/dados/log_ponte-20170220-2.txt"
-"/dados/log_guarapari-20170403.txt"
-"/dados/log_ponte-20170220.txt"
-"/dados/log_ponte-20161228.txt"
 )
 
 declare -a dirs=(
 "/dados/ufes/20160825"
-"/dados/ufes/20160830"
-"/dados/ufes/20160906-02"
-"/dados/ufes/20161021"
-"/dados/ufes/20161112-01"
-"/dados/ufes/20161112-00"
-"/dados/ufes/20170220-02"
-"/dados/ufes/20170403"
-"/dados/ufes/20170220"
-"/dados/ufes/20161228"
 )
 
+
 declare -a cams=(
-3 3 3 3 3 3 3 3 3 3
+3
 )
 
 declare -a crops=(
-380 380 380 380 380 380 380 380 380 380
+380
 )
 
-declare -a fmts=(1 1 1 1 1 1 1 1 1 1)
+declare -a fmts=(1)
 
 for i in "${!dirs[@]}"; do
     mkdir -p ${dirs[$i]}
@@ -51,7 +75,7 @@ for i in "${!dirs[@]}"; do
     for left in ${dirs[$i]}/*.l.png; do
         right="${left/l.png/r.png}"
         disp="${left/l.png/d.png}"
-        if [ -f "$disp" ]; then
+        if [ ! -f "$disp" ]; then
             echo "Processing ${left} ${right}"
             ~/deepslam/spsstereo/build/spsstereo $left $right
              mv disparity.png ${disp}
