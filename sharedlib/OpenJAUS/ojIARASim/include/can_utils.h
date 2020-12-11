2bd03c583f4598fdb941b74d803e555192e63031
albertodesouza <alberto@lcad.inf.ufes.br>
2020-12-11 11:21:24 -0300

    melhorias no can_dump para a visita a Ternium

--- sharedlib/OpenJAUS/ojIARASim/include/can_utils.h
+++ sharedlib/OpenJAUS/ojIARASim/include/can_utils.h
@@ -1,5 +1,18 @@
+#ifndef CAN_UTILS_H_
+#define CAN_UTILS_H_
 
-int init_can(char *intf_name);
-int send_frame(int sockfd, struct can_frame *frame);
-int recv_frame(int sockfd, struct can_frame *frame);
-void print_frame(struct can_frame *frame);
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	int init_can(char *intf_name);
+	int send_frame(int sockfd, struct can_frame *frame);
+	int recv_frame(int sockfd, struct can_frame *frame);
+	void print_frame(struct can_frame *frame);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CAN_UTILS_H_ */
