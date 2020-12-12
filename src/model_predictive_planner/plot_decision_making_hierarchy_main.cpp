2e790a4ca8e33ad7d9250b24b1352bd5602e4adb
albertodesouza <alberto@lcad.inf.ufes.br>
2020-12-12 11:38:45 -0300

    melhorias na parada

--- src/model_predictive_planner/plot_decision_making_hierarchy_main.cpp
+++ src/model_predictive_planner/plot_decision_making_hierarchy_main.cpp
@@ -11,7 +11,7 @@ rrt_path_message *mpp_message;
 
 
 void
-plot_trajectories()
+plot_phi_in_trajectories()
 {
 	static bool first_time = true;
 	static FILE *gnuplot_pipeMP;
@@ -25,7 +25,7 @@ plot_trajectories()
 		fprintf(gnuplot_pipeMP, "set yrange [-0.55:0.55]\n");
 		fprintf(gnuplot_pipeMP, "set xlabel 't'\n");
 		fprintf(gnuplot_pipeMP, "set ylabel 'phi'\n");
-		fprintf(gnuplot_pipeMP, "set tics out\n");
+//		fprintf(gnuplot_pipeMP, "set tics out\n");
 		fprintf(gnuplot_pipeMP, "set size ratio -1\n");
 	}
 
@@ -71,6 +71,67 @@ plot_trajectories()
 }
 
 
+void
+plot_v_in_trajectories()
+{
+	static bool first_time = true;
+	static FILE *gnuplot_pipeMP;
+
+	if (first_time)
+	{
+		first_time = false;
+
+		gnuplot_pipeMP = popen("gnuplot", "w"); // -persist to keep last plot after program closes
+		fprintf(gnuplot_pipeMP, "set xrange [0:3]\n");
+		fprintf(gnuplot_pipeMP, "set yrange [-10:10]\n");
+		fprintf(gnuplot_pipeMP, "set xlabel 't'\n");
+		fprintf(gnuplot_pipeMP, "set ylabel 'v'\n");
+//		fprintf(gnuplot_pipeMP, "set tics out\n");
+//		fprintf(gnuplot_pipeMP, "set size ratio -1\n");
+	}
+
+	FILE *mpp_file = fopen("mpp_v.txt", "w");
+	FILE *base_motion_file = fopen("base_motion_v.txt", "w");
+	FILE *robot_motion_file = fopen("robot_motion_v.txt", "w");
+
+	double t;
+	t = 0.0;
+	for (int i = 0; (i < base_motion_command_message->num_motion_commands) && (t < 3.0); i++)
+	{
+		fprintf(base_motion_file, "%lf %lf\n", base_motion_command_message->motion_command[i].v, t);
+		t += base_motion_command_message->motion_command[i].time;
+		fprintf(base_motion_file, "%lf %lf\n", base_motion_command_message->motion_command[i].v, t);
+	}
+
+	t = 0.0;
+	for (int i = 0; (i < robot_motion_command_message->num_motion_commands) && (t < 3.0); i++)
+	{
+		fprintf(robot_motion_file, "%lf %lf\n", robot_motion_command_message->motion_command[i].v, t);
+		t += base_motion_command_message->motion_command[i].time;
+		fprintf(robot_motion_file, "%lf %lf\n", robot_motion_command_message->motion_command[i].v, t);
+	}
+
+	t = 0.0;
+	for (int i = 0; (i < mpp_message->size) && (t < 3.0); i++)
+	{
+		fprintf(mpp_file, "%lf %lf\n", mpp_message->path[i].p1.v, t);
+		t += mpp_message->path[i].time;
+		fprintf(mpp_file, "%lf %lf\n", mpp_message->path[i].p1.v, t);
+	}
+
+	fclose(mpp_file);
+	fclose(base_motion_file);
+	fclose(robot_motion_file);
+
+	fprintf(gnuplot_pipeMP, "plot "
+			"'./mpp_v.txt' using 2:1 w l title 'mpp' lt rgb 'blue',"
+			"'./base_motion_v.txt' using 2:1 w l title 'base' lt rgb 'green',"
+			"'./robot_motion_v.txt' using 2:1 with linespoints title 'robot' lt rgb 'red'\n");
+
+	fflush(gnuplot_pipeMP);
+}
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //																								//
 // Publishers																					//
@@ -109,7 +170,8 @@ base_ackerman_motion_command_message_handler(carmen_base_ackerman_motion_command
 {
 	base_motion_command_message = motion_command_message;
 
-	plot_trajectories();
+	plot_phi_in_trajectories();
+	plot_v_in_trajectories();
 }
 
 
