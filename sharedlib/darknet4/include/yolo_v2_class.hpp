09a1521874d59b02166a14ca9ff175ff8d8ee88d
marcosthiago <marcosthiago@gmail.com>
2021-03-18 18:50:50 -0300

    add bbox.h for tracking

--- sharedlib/darknet4/include/yolo_v2_class.hpp
+++ sharedlib/darknet4/include/yolo_v2_class.hpp
@@ -19,14 +19,15 @@
 
 #define C_SHARP_MAX_OBJECTS 1000
 
-struct bbox_t {
-    unsigned int x, y, w, h;       // (x,y) - top-left corner, (w, h) - width & height of bounded box
-    float prob;                    // confidence - probability that the object was found correctly
-    unsigned int obj_id;           // class of object - from range [0, classes-1]
-    unsigned int track_id;         // tracking id for video (0 - untracked, 1 - inf - tracked object)
-    unsigned int frames_counter;   // counter of frames on which the object was detected
-    float x_3d, y_3d, z_3d;        // center of object (in Meters) if ZED 3D Camera is used
-};
+#include "../bbox.h"
+// struct bbox_t {
+//     unsigned int x, y, w, h;       // (x,y) - top-left corner, (w, h) - width & height of bounded box
+//     float prob;                    // confidence - probability that the object was found correctly
+//     unsigned int obj_id;           // class of object - from range [0, classes-1]
+//     unsigned int track_id;         // tracking id for video (0 - untracked, 1 - inf - tracked object)
+//     unsigned int frames_counter;   // counter of frames on which the object was detected
+//     float x_3d, y_3d, z_3d;        // center of object (in Meters) if ZED 3D Camera is used
+// };
 
 struct image_t {
     int h;                        // height
