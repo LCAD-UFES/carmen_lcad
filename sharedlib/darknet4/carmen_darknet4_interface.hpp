61519d8092252f44d975a20d35b77bf30c9ce7db
marcosthiago <marcosthiago@gmail.com>
2021-06-18 16:10:50 -0300

    darknet4 integration distinction

---
+++ sharedlib/darknet4/carmen_darknet4_interface.hpp
@@ -0,0 +1,16 @@
+#ifndef __DARKNET4_INTERFACE_H__
+#define __DARKNET4_INTERFACE_H__
+
+#include <vector>
+#include "bbox.h"
+
+char **
+get_classes_names(char *classes_names_file);
+
+void*
+load_yolo_network(char *cfg, char *weights, int clear);
+
+std::vector<bbox_t>
+run_YOLO(unsigned char *data, int w, int h, float threshold);
+
+#endif
