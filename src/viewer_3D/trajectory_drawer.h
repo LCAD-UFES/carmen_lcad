a6099dcd9451ab614afe063636ff64450cd5160e
gnunesm <gabnunes2112@gmail.com>
2021-06-28 13:40:40 -0300

    offroad_plan exibido no viewer_3D

--- src/viewer_3D/trajectory_drawer.h
+++ src/viewer_3D/trajectory_drawer.h
@@ -29,7 +29,7 @@ typedef struct _trajectory_drawer
 } trajectory_drawer;
 
 trajectory_drawer *create_trajectory_drawer(double r, double g, double b, carmen_vector_3D_t robot_size,
-		double distance_between_rear_car_and_rear_wheels, carmen_semi_trailer_config_t semi_trailer_config, double path_point_size = 5.0);
+		double distance_between_rear_car_and_rear_wheels, carmen_semi_trailer_config_t semi_trailer_config, double path_point_size = 5.0, double persistence_time = 0.1);
 void destroy_trajectory_drawer(trajectory_drawer *t_drawer);
 void add_trajectory_message(trajectory_drawer *t_drawer, carmen_navigator_ackerman_plan_message *message);
 void add_base_ackerman_trajectory_message(trajectory_drawer *t_drawer, carmen_base_ackerman_motion_command_message *message);
