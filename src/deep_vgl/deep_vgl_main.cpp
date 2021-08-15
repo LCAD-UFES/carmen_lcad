ac6b7cab3e59449ea28fe155f8f485d7a6522da6
thiagorjes <thiago_rj_es@hotmail.com>
2021-08-15 08:32:32 -0300

    ajustes no deep_vgl para gerar imagens a partir do pointcloud

--- src/deep_vgl/deep_vgl_main.cpp
+++ src/deep_vgl/deep_vgl_main.cpp
@@ -33,6 +33,14 @@
 using namespace std;
 using namespace cv;
 
+//#pragma pack(push, 2)
+struct RGB
+{
+	uchar B;
+	uchar G;
+	uchar R;
+};
+
 static int camera;
 static int bumblebee_basic_width;
 static int bumblebee_basic_height;
@@ -111,8 +119,8 @@ infer_pose(carmen_point_t *pose, double width, double height,
 		   int dx, int dy, int w, int h,
 		   unsigned char *image_raw, double timestamp)
 {
+	printf("infering pose\n");
 	image img = convert_image_msg_to_darknet_image(width, height, image_raw);
-	free(image_raw);
 	image img_without_car_hood = crop_image(img, dx, dy, w, h); // crop_image() nao faz free()
 	image resized_img_without_car_hood = resize_min(img_without_car_hood, net.w);
 	image cropped_resized_img_without_car_hood = crop_image(resized_img_without_car_hood, (resized_img_without_car_hood.w - net.w) / 2, (resized_img_without_car_hood.h - net.h) / 2, net.w, net.h);
@@ -147,8 +155,7 @@ infer_pose(carmen_point_t *pose, double width, double height,
 	char predicted_image_file_name[2048];
 	sscanf(learned_poses[selected_pose_label], "%lf %lf %lf %s", &(pose->x), &(pose->y), &(pose->theta), predicted_image_file_name);
 	printf("confidence %lf, %lf %lf %lf %s\n", predictions[selected_pose_label], pose->x, pose->y, pose->theta, predicted_image_file_name);
-	//	printf("confidence %lf, %s\n", predictions[selected_pose_label], learned_poses[selected_pose_label]);
-
+	
 	Mat pose_image = imread(predicted_image_file_name, IMREAD_COLOR);
 	if (predictions[selected_pose_label] < 0.00)
 		pose_image = Mat::zeros(Size(pose_image.cols, pose_image.rows), pose_image.type());
@@ -247,6 +254,7 @@ void publish_gps_xyz(double x, double y, double theta, double confidence, double
 
 void velodyne_partial_scan_handler(carmen_velodyne_partial_scan_message *lidar)
 {
+	printf("acquiring velodyne message...\n");
 	carmen_point_t pose;
 
 	double timestamp = lidar->timestamp;
@@ -262,285 +270,39 @@ void velodyne_partial_scan_handler(carmen_velodyne_partial_scan_message *lidar)
 	int number_of_pixels = vertical_resolution * (end - ini);
 
 	double range[32];
-	double by500 = 1 / 500;
-	double by25 = 1 / 25;
+	double by500 = (double)(1 / 500.0); // para poupar tempo no loop
+	double by25 = (double)(1 / 25.0);	// para poupar tempo no loop
 
-	Mat synthetic_image;
-    synthetic_image = Mat::zeros(Size((end-ini),32), CV_8UC1);
-	int R,G,B;
-
-	// fiz assim para evitar cascateamento de loop, pois eram poucas linhas
-	for (int i = ini; i < end; i++)
+	Mat synthetic_image = Mat::zeros(Size((end - ini), 32), CV_8UC3);
+	int ray_order[32] = {31,29,27,25,23,21,19,17,15,13,11,9,7,5,3,1,30,28,26,24,22,20,18,16,14,12,10,8,6,4,2,0};
+	for (int r=0; r < 32; r++)
 	{
-		range[0] = ((int)((lidar->partial_scan[i].distance[31]) * by500) * 765 * by25);
-		B = 0 ? range[0] < 511 : range[0] - 510;
-		G = 0 ? range[0] < 256 : range[0] - B - 255;
-		R = 255 ? range[0] > 255 : range[0];
-		if( range[0] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,0))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,0))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,0))[2] = R;
-		range[1] = ((int)((lidar->partial_scan[i].distance[29]) * by500) * 765 * by25);
-		B = 0 ? range[1] < 511 : range[1] - 510;
-		G = 0 ? range[1] < 256 : range[1] - B - 255;
-		R = 255 ? range[1] > 255 : range[1];
-		if( range[1] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,1))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,1))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,1))[2] = R;
-		range[2] = ((int)((lidar->partial_scan[i].distance[27]) * by500) * 765 * by25);
-		B = 0 ? range[2] < 511 : range[2] - 510;
-		G = 0 ? range[2] < 256 : range[2] - B - 255;
-		R = 255 ? range[2] > 255 : range[2];
-		if( range[2] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,2))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,2))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,2))[2] = R;
-		range[3] = ((int)((lidar->partial_scan[i].distance[25]) * by500) * 765 * by25);
-		B = 0 ? range[3] < 511 : range[3] - 510;
-		G = 0 ? range[3] < 256 : range[3] - B - 255;
-		R = 255 ? range[3] > 255 : range[3];
-		if( range[3] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,3))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,3))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,3))[2] = R;
-		range[4] = ((int)((lidar->partial_scan[i].distance[23]) * by500) * 765 * by25);
-		B = 0 ? range[4] < 511 : range[4] - 510;
-		G = 0 ? range[4] < 256 : range[4] - B - 255;
-		R = 255 ? range[4] > 255 : range[4];
-		if( range[4] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,4))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,4))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,4))[2] = R;
-		range[5] = ((int)((lidar->partial_scan[i].distance[21]) * by500) * 765 * by25);
-		B = 0 ? range[5] < 511 : range[5] - 510;
-		G = 0 ? range[5] < 256 : range[5] - B - 255;
-		R = 255 ? range[5] > 255 : range[5];
-		if( range[5] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,5))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,5))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,5))[2] = R;
-		range[6] = ((int)((lidar->partial_scan[i].distance[19]) * by500) * 765 * by25);
-		B = 0 ? range[6] < 511 : range[6] - 510;
-		G = 0 ? range[6] < 256 : range[6] - B - 255;
-		R = 255 ? range[6] > 255 : range[6];
-		if( range[6] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,6))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,6))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,6))[2] = R;
-		range[7] = ((int)((lidar->partial_scan[i].distance[17]) * by500) * 765 * by25);
-		B = 0 ? range[7] < 511 : range[7] - 510;
-		G = 0 ? range[7] < 256 : range[7] - B - 255;
-		R = 255 ? range[7] > 255 : range[7];
-		if( range[7] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,7))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,7))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,7))[2] = R;
-		range[8] = ((int)((lidar->partial_scan[i].distance[15]) * by500) * 765 * by25);
-		B = 0 ? range[8] < 511 : range[8] - 510;
-		G = 0 ? range[8] < 256 : range[8] - B - 255;
-		R = 255 ? range[8] > 255 : range[8];
-		if( range[8] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,8))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,8))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,8))[2] = R;
-		range[9] = ((int)((lidar->partial_scan[i].distance[13]) * by500) * 765 * by25);
-		B = 0 ? range[9] < 511 : range[9] - 510;
-		G = 0 ? range[9] < 256 : range[9] - B - 255;
-		R = 255 ? range[9] > 255 : range[9];
-		if( range[9] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,9))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,9))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,9))[2] = R;
-		range[10] = ((int)((lidar->partial_scan[i].distance[11]) * by500) * 765 * by25);
-		B = 0 ? range[10] < 511 : range[10] - 510;
-		G = 0 ? range[10] < 256 : range[10] - B - 255;
-		R = 255 ? range[10] > 255 : range[10];
-		if( range[10] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,10))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,10))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,10))[2] = R;
-		range[11] = ((int)((lidar->partial_scan[i].distance[9]) * by500) * 765 * by25);
-		B = 0 ? range[11] < 511 : range[11] - 510;
-		G = 0 ? range[11] < 256 : range[11] - B - 255;
-		R = 255 ? range[11] > 255 : range[11];
-		if( range[11] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,11))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,11))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,11))[2] = R;
-		range[12] = ((int)((lidar->partial_scan[i].distance[7]) * by500) * 765 * by25);
-		B = 0 ? range[12] < 511 : range[12] - 510;
-		G = 0 ? range[12] < 256 : range[12] - B - 255;
-		R = 255 ? range[12] > 255 : range[12];
-		if( range[12] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,12))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,12))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,12))[2] = R;
-		range[13] = ((int)((lidar->partial_scan[i].distance[5]) * by500) * 765 * by25);
-		B = 0 ? range[13] < 511 : range[13] - 510;
-		G = 0 ? range[13] < 256 : range[13] - B - 255;
-		R = 255 ? range[13] > 255 : range[13];
-		if( range[13] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,13))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,13))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,13))[2] = R;
-		range[14] = ((int)((lidar->partial_scan[i].distance[3]) * by500) * 765 * by25);
-		B = 0 ? range[14] < 511 : range[14] - 510;
-		G = 0 ? range[14] < 256 : range[14] - B - 255;
-		R = 255 ? range[14] > 255 : range[14];
-		if( range[14] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,14))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,14))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,14))[2] = R;
-		range[15] = ((int)((lidar->partial_scan[i].distance[1]) * by500) * 765 * by25);
-		B = 0 ? range[15] < 511 : range[15] - 510;
-		G = 0 ? range[15] < 256 : range[15] - B - 255;
-		R = 255 ? range[15] > 255 : range[15];
-		if( range[15] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,151))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,151))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,151))[2] = R;
-		range[16] = ((int)((lidar->partial_scan[i].distance[30]) * by500) * 765 * by25);
-		B = 0 ? range[16] < 511 : range[16] - 510;
-		G = 0 ? range[16] < 256 : range[16] - B - 255;
-		R = 255 ? range[16] > 255 : range[16];
-		if( range[16] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,16))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,16))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,16))[2] = R;
-		range[17] = ((int)((lidar->partial_scan[i].distance[28]) * by500) * 765 * by25);
-		B = 0 ? range[17] < 511 : range[17] - 510;
-		G = 0 ? range[17] < 256 : range[17] - B - 255;
-		R = 255 ? range[17] > 255 : range[17];
-		if( range[17] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,17))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,17))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,17))[2] = R;
-		range[18] = ((int)((lidar->partial_scan[i].distance[26]) * by500) * 765 * by25);
-		B = 0 ? range[18] < 511 : range[18] - 510;
-		G = 0 ? range[18] < 256 : range[18] - B - 255;
-		R = 255 ? range[18] > 255 : range[18];
-		if( range[18] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,18))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,18))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,18))[2] = R;
-		range[19] = ((int)((lidar->partial_scan[i].distance[24]) * by500) * 765 * by25);
-		B = 0 ? range[19] < 511 : range[19] - 510;
-		G = 0 ? range[19] < 256 : range[19] - B - 255;
-		R = 255 ? range[19] > 255 : range[19];
-		if( range[19] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,19))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,19))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,19))[2] = R;
-		range[20] = ((int)((lidar->partial_scan[i].distance[22]) * by500) * 765 * by25);
-		B = 0 ? range[20] < 511 : range[20] - 510;
-		G = 0 ? range[20] < 256 : range[20] - B - 255;
-		R = 255 ? range[20] > 255 : range[20];
-		if( range[20] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,20))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,20))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,20))[2] = R;
-		range[21] = ((int)((lidar->partial_scan[i].distance[20]) * by500) * 765 * by25);
-		B = 0 ? range[21] < 511 : range[21] - 510;
-		G = 0 ? range[21] < 256 : range[21] - B - 255;
-		R = 255 ? range[21] > 255 : range[21];
-		if( range[21] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,21))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,21))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,21))[2] = R;
-		range[22] = ((int)((lidar->partial_scan[i].distance[18]) * by500) * 765 * by25);
-		B = 0 ? range[22] < 511 : range[22] - 510;
-		G = 0 ? range[22] < 256 : range[22] - B - 255;
-		R = 255 ? range[22] > 255 : range[22];
-		if( range[22] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,22))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,22))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,22))[2] = R;
-		range[23] = ((int)((lidar->partial_scan[i].distance[16]) * by500) * 765 * by25);
-		B = 0 ? range[23] < 511 : range[23] - 510;
-		G = 0 ? range[23] < 256 : range[23] - B - 255;
-		R = 255 ? range[23] > 255 : range[23];
-		if( range[23] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,23))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,23))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,23))[2] = R;
-		range[24] = ((int)((lidar->partial_scan[i].distance[14]) * by500) * 765 * by25);
-		B = 0 ? range[24] < 511 : range[24] - 510;
-		G = 0 ? range[24] < 256 : range[24] - B - 255;
-		R = 255 ? range[24] > 255 : range[24];
-		if( range[24] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,24))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,24))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,24))[2] = R;
-		range[25] = ((int)((lidar->partial_scan[i].distance[12]) * by500) * 765 * by25);
-		B = 0 ? range[25] < 511 : range[25] - 510;
-		G = 0 ? range[25] < 256 : range[25] - B - 255;
-		R = 255 ? range[25] > 255 : range[25];
-		if( range[25] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,25))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,25))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,25))[2] = R;
-		range[26] = ((int)((lidar->partial_scan[i].distance[10]) * by500) * 765 * by25);
-		B = 0 ? range[26] < 511 : range[26] - 510;
-		G = 0 ? range[26] < 256 : range[26] - B - 255;
-		R = 255 ? range[26] > 255 : range[26];
-		if( range[26] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,26))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,26))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,26))[2] = R;
-		range[27] = ((int)((lidar->partial_scan[i].distance[8]) * by500) * 765 * by25);
-		B = 0 ? range[27] < 511 : range[27] - 510;
-		G = 0 ? range[27] < 256 : range[27] - B - 255;
-		R = 255 ? range[27] > 255 : range[27];
-		if( range[27] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,27))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,27))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,27))[2] = R;
-		range[28] = ((int)((lidar->partial_scan[i].distance[6]) * by500) * 765 * by25);
-		B = 0 ? range[28] < 511 : range[28] - 510;
-		G = 0 ? range[28] < 256 : range[28] - B - 255;
-		R = 255 ? range[28] > 255 : range[28];
-		if( range[28] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,28))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,28))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,28))[2] = R;
-		range[29] = ((int)((lidar->partial_scan[i].distance[4]) * by500) * 765 * by25);
-		B = 0 ? range[29] < 511 : range[29] - 510;
-		G = 0 ? range[29] < 256 : range[29] - B - 255;
-		R = 255 ? range[29] > 255 : range[29];
-		if( range[29] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,29))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,29))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,29))[2] = R;
-		range[30] = ((int)((lidar->partial_scan[i].distance[2]) * by500) * 765 * by25);
-		B = 0 ? range[30] < 511 : range[30] - 510;
-		G = 0 ? range[30] < 256 : range[30] - B - 255;
-		R = 255 ? range[30] > 255 : range[30];
-		if( range[30] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,30))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,30))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,30))[2] = R;
-		range[31] = ((int)((lidar->partial_scan[i].distance[0]) * by500) * 765 * by25);
-		B = 0 ? range[31] < 511 : range[31] - 510;
-		G = 0 ? range[31] < 256 : range[31] - B - 255;
-		R = 255 ? range[31] > 255 : range[31];
-		if( range[31] == 0) B = G = R = 255;
-		synthetic_image.at<Vec3b>(Point(i,31))[0] = B;
-		synthetic_image.at<Vec3b>(Point(i,31))[1] = G;
-		synthetic_image.at<Vec3b>(Point(i,31))[2] = R;
-		
+		cv::Vec3b* ptr = synthetic_image.ptr<cv::Vec3b>(r);
+		for (int i = ini; i < end; i++)
+		{
+			int R, G, B;
+			range[r] = ((int)((lidar->partial_scan[i].distance[ray_order[r]]) * by500) * 765 * by25);
+			B = range[r] < 511 ? 0 : range[r] - 510;
+			G = range[r] < 256 ? 0 : range[r] - B - 255;
+			R = range[r] > 255 ? 255 : range[r];
+			if (range[r] == 0)
+				B = G = R = 255;
+			ptr[i-ini] = Vec3b(R,G,B);
+		}
 	}
 	
 	//resize de (shots x 32) para o (crop_width x crop_height)
-	resize(synthetic_image,synthetic_image,Size(crop_width, crop_height),0,0, CV_INTER_LINEAR);
+	Mat resized_image;
+	cv::resize(synthetic_image,resized_image,Size(crop_width, crop_height), INTER_NEAREST);
+	
 	// inferir pose
 	double confidence = infer_pose(&pose, crop_width, crop_height,
-									0, 0, 640, 380,
-									synthetic_image.data, lidar->timestamp);
+								   0, 0, 640, 480,
+								   resized_image.data, lidar->timestamp);
 	// publicar pose
 	publish_gps_xyz(pose.x, pose.y, pose.theta, confidence, lidar->timestamp);
 	// liberar memoria
-	free(synthetic_image.data);
+	//free(synthetic_image.data);
 }
 
 void bumblebee_basic_handler(carmen_bumblebee_basic_stereoimage_message *stereo_image)
@@ -624,12 +386,14 @@ void initialize_structures(char *cfgfile, char *weightfile, char *learned_poses_
 	calculate_binary_weights(net);
 
 	learned_poses = get_labels(learned_poses_filename);
+	printf("all done on darknet\n");
 }
 
 void subscribe_messages()
 {
 	if (use_lidar == 1)
 	{
+		printf("subscribing deep_vgl on velodyne\n");
 		carmen_velodyne_subscribe_partial_scan_message(NULL, (carmen_handler_t)velodyne_partial_scan_handler, CARMEN_SUBSCRIBE_LATEST);
 	}
 	else
