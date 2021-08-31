775903b2e1063435df1f709061d7180afeb10687
SARA <sara@sara.car>
2021-08-31 02:04:19 -0300

    new map ala b - prints libcontrol comentados - utilities iptables

--- sharedlib/libcontrol/pid/pid.cpp
+++ sharedlib/libcontrol/pid/pid.cpp
@@ -263,9 +263,9 @@ carmen_libpid_steering_PID_controler_FUZZY(double atan_desired_curvature, double
 
 	u_t = carmen_clamp(-100.0, u_t, 100.0);
 
-	 fprintf(stdout, "STEERING (cc, dc, e, i, d, s): %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
-	 	atan_current_curvature, atan_desired_curvature, error_t, integral_t, derivative_t, u_t, t);
-	 fflush(stdout);
+//	 fprintf(stdout, "STEERING (cc, dc, e, i, d, s): %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
+//	 	atan_current_curvature, atan_desired_curvature, error_t, integral_t, derivative_t, u_t, t);
+//	 fflush(stdout);
 
 	return u_t;
 }
@@ -474,11 +474,11 @@ carmen_libpid_velocity_PID_controler(double *throttle_command, double *brakes_co
 	*throttle_command = carmen_clamp(0.0, *throttle_command, 100.0);
 	*brakes_command = carmen_clamp(g_brake_gap, *brakes_command, 100.0);
 
-	 fprintf(stdout, "VELOCITY (st, cv, dv, e, t, b, i, d, ts): %d, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
-	 	g_velocity_PID_controler_state, current_velocity, desired_velocity, error_t,
-	 	*throttle_command, *brakes_command,
-	 	integral_t, derivative_t, carmen_get_time());
-	 fflush(stdout);
+//	 fprintf(stdout, "VELOCITY (st, cv, dv, e, t, b, i, d, ts): %d, %lf, %lf, %lf, %lf, %lf, %lf, %lf, %lf\n",
+//	 	g_velocity_PID_controler_state, current_velocity, desired_velocity, error_t,
+//	 	*throttle_command, *brakes_command,
+//	 	integral_t, derivative_t, carmen_get_time());
+//	 fflush(stdout);
 }
 
 
---
+++ src/utilities/scripts/restore-iptable.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+iptables-restore < /etc/iptables.sav
