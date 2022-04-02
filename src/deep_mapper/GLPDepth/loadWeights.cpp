d10a74b527b231630b2c193d0cb5c077b19615b6
marcosthiago <marcosthiago@gmail.com>
2022-04-02 18:18:57 -0300

    load with libtorch

--- src/deep_mapper/GLPDepth/loadWeights.cpp
+++ src/deep_mapper/GLPDepth/loadWeights.cpp
@@ -7,6 +7,8 @@
 
 using namespace cv;
 
+torch::jit::script::Module module;
+
 at::Tensor matToTensor(cv::Mat frame, int h, int w, int c)
 {
   cv::cvtColor(frame, frame, CV_BGR2RGB);
@@ -33,14 +35,12 @@ cv::Mat tensorToOpenCv(at::Tensor out_tensor, int h, int w, int c)
   return resultImg;
 }
 
-int main(int argc, const char *argv[])
+int 
+initialize_glpdepth()
 {
-
-  torch::jit::script::Module module;
-  char *pPath;
   try
   {
-
+    char *pPath;
     char *glpdepthPath;
     pPath = getenv("CARMEN_HOME");
     glpdepthPath = (char *)"/src/deep_mapper/GLPDepth/ckpt/best_model_kitti.pt";
@@ -55,20 +55,22 @@ int main(int argc, const char *argv[])
   }
   catch (const c10::Error &e)
   {
-    std::cerr << "error loading the model\n";
+    std::cerr << "GLPDepth: error loading the model\n";
     return -1;
   }
+  return 1;
+}
+
+
+unsigned char* 
+glpdepth_process_image(Mat img, int cut_param, int down_param)
+{
   at::Tensor tensor;
   int new_w;
   int new_h;
+  /* Transform image to tensor */
   try
   {
-
-    std::string image_path = "/src/deep_mapper/GLPDepth/input/000005.png";
-    std::stringstream ss;
-    ss << pPath << image_path.c_str();
-    Mat img = imread(ss.str(), IMREAD_COLOR);
-
     cv::Mat image_resized;
     int w = img.cols;
     int h = img.rows;
@@ -80,32 +82,28 @@ int main(int argc, const char *argv[])
     image_resized.convertTo(image_resized_float, CV_32F, 1.0 / 255);
     tensor = matToTensor(image_resized_float, new_h, new_w, 3);
     tensor = tensor.contiguous();
-
-    // std::cout << tensor << std::endl;
   }
   catch (const c10::Error &e)
   {
     std::cerr << "error loading mat\n";
-    return -1;
+    return NULL;
   }
+
   std::vector<torch::jit::IValue> inputs;
   try
   {
     // Create a vector of inputs.
     inputs.push_back(tensor);
-
-    // Execute the model and turn its output into a tensor.
   }
   catch (const c10::Error &e)
   {
     std::cerr << "error loading the inputs\n";
-    return -1;
+    return NULL;
   }
-  // at::Tensor output;
-
+  // Execute the model and turn its output into a tensor.
   torch::NoGradGuard no_grad;
-  auto output = module.forward(inputs).toGenericDict();
-  auto pred_d = output.at("pred_d").toTensor();
+  c10::impl::GenericDict output = module.forward(inputs).toGenericDict();
+  at::Tensor pred_d = output.at("pred_d").toTensor();
   pred_d = pred_d.squeeze();
   pred_d = pred_d * 256.0;
   pred_d = (pred_d / pred_d.max()) * 255;
@@ -115,20 +113,38 @@ int main(int argc, const char *argv[])
   int height = pred_d.sizes()[1];
 
   std::cout << pred_d << std::endl;
+
+  for(unsigned int i = 0; i < cut_param; i++)
+  {
+    for(unsigned int j = 0; j < width; j++)
+    {
+      pred_d[i][j] = 1000;
+    }
+  }
+  for(unsigned int i = height - down_param; i < height; i++)
+  {
+    for(unsigned int j = 0; j < width; j++)  
+    {
+      pred_d[i][j] = 0;
+    }
+  }
+
+  
   cv::Mat color_mat;
   cv::Mat output_mat(cv::Size{ height, width }, CV_8U, pred_d.data_ptr<uchar>());
   applyColorMap(output_mat, color_mat, cv::COLORMAP_RAINBOW);
-  //auto TensorList = output.toTensor();
   
-  // at::Tensor new_output = output[0].toTensor();
-  // new_output = new_output.contiguous();
-  std::cout << pred_d << std::endl;
-
-  //cv::Mat image = tensorToOpenCv(pred_d, new_h, new_w, 1);
-
-  cv::namedWindow("Profundidade"); // Create a window
-  cv::imshow("Profundidade", color_mat); // Show our image inside the created window.
+  cv::namedWindow("GLPDepth"); // Create a window
+  cv::imshow("GLPDepth", color_mat); // Show our image inside the created window.
   cv::waitKey(0);
 
-  std::cout << "ok\n";
+  return pred_d.data_ptr<uchar>();
+}
+
+
+int main(int argc, const char *argv[])
+{
+
+  
+  
 }
