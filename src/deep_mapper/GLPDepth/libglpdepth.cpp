08d6ae4a0bf3ae3a5601d8b2d42feb2b58842b28
marcosthiago <marcosthiago@gmail.com>
2022-04-04 21:55:06 -0300

    GLP Depth with libtorch

--- src/deep_mapper/GLPDepth/libglpdepth.cpp
+++ src/deep_mapper/GLPDepth/libglpdepth.cpp
@@ -1,121 +1,142 @@
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <Python.h>
+#include <torch/script.h> // One-stop header.
+
+#include <iostream>
+#include <memory>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include "libglpdepth.h"
-#include <numpy/arrayobject.h>
 #include <stdlib.h> /* getenv */
 
-#define NUMPY_IMPORT_ARRAY_RETVAL
+using namespace cv;
 
-PyObject *python_libglpdepth_process_image_function;
+torch::jit::script::Module module;
 
-void initialize_python_path_glpdepth()
+at::Tensor matToTensor(cv::Mat frame, int h, int w, int c)
 {
-	char* pyPath;
-	char* pPath;
-	char* glpdepthPath;
-	pyPath = (char *) "PYTHONPATH=";
-  	pPath = getenv ("CARMEN_HOME");
-	glpdepthPath = (char *) "/src/deep_mapper/GLPDepth";
-    char * path = (char *) malloc(1 + strlen(pyPath) + strlen(pPath)+ strlen(glpdepthPath));
-	strcpy(path, pyPath);
+  cv::cvtColor(frame, frame, CV_BGR2RGB);
+  frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
+  auto input_tensor = torch::from_blob(frame.data, {1, h, w, c});
+  input_tensor = input_tensor.permute({0, 3, 1, 2});
+
+  torch::DeviceType device_type = torch::kCPU;
+  //    if (torch::cuda::is_available()) {
+  // device_type = torch::kCUDA;
+  //    }
+  input_tensor = input_tensor.to(device_type);
+  return input_tensor;
+}
+
+cv::Mat tensorToOpenCv(at::Tensor out_tensor, int h, int w, int c)
+{
+  out_tensor = out_tensor.squeeze().detach().permute({1, 2, 0});
+  out_tensor = out_tensor.mul(255).clamp(0, 255).to(torch::kU8);
+  out_tensor = out_tensor.to(torch::kCPU);
+  cv::Mat resultImg(h, w, CV_8UC3);
+  // cv::Mat resultImg(h, w, CV_8UC1);
+  std::memcpy((void *)resultImg.data, out_tensor.data_ptr(), sizeof(torch::kU8) * out_tensor.numel());
+  return resultImg;
+}
+
+void 
+initialize_glpdepth()
+{
+  try
+  {
+    char *pPath;
+    char *glpdepthPath;
+    pPath = getenv("CARMEN_HOME");
+    glpdepthPath = (char *)"/src/deep_mapper/GLPDepth/ckpt/best_model_kitti.pt";
+    char *path = (char *)malloc(1 + strlen(pPath) + strlen(glpdepthPath));
     strcat(path, pPath);
     strcat(path, glpdepthPath);
-	putenv(path);
+
+    // Deserialize the ScriptModule from a file using torch::jit::load().
+    module = torch::jit::load(path);
+    module.eval();
+    module.to(at::kCPU);
+  }
+  catch (const c10::Error &e)
+  {
+    std::cerr << "GLPDepth: error loading the model\n";
+  }
 }
 
-void
-initialize_python_context_glpdepth()
+
+unsigned char* 
+libglpdepth_process_image(cv::Mat img, int cut_param, int down_param)
 {
-	initialize_python_path_glpdepth();
-	Py_Initialize();
-	import_array();
-	PyObject *python_module = PyImport_ImportModule("run_glpdepth");
-	
-	if (PyErr_Occurred())
-		        PyErr_Print();
+  at::Tensor tensor;
+  int new_w;
+  int new_h;
+  /* Transform image to tensor */
+  try
+  {
+    cv::Mat image_resized;
+    int w = img.cols;
+    int h = img.rows;
+    new_h = round(h / 32) * 32;
+    new_w = round(w / 32) * 32;
 
-	if (python_module == NULL)
-	{
-		Py_Finalize();
-		exit(printf("Error: The python_module run_glpdepth could not be loaded.\nMaybe PYTHONPATH is not set.\n"));
-	}
+    cv::resize(img, image_resized, cv::Size(new_w, new_h));
+    cv::Mat image_resized_float;
+    image_resized.convertTo(image_resized_float, CV_32F, 1.0 / 255);
+    tensor = matToTensor(image_resized_float, new_h, new_w, 3);
+    tensor = tensor.contiguous();
+  }
+  catch (const c10::Error &e)
+  {
+    std::cerr << "error loading mat\n";
+    return NULL;
+  }
 
-	if (PyErr_Occurred())
-		        PyErr_Print();
+  std::vector<torch::jit::IValue> inputs;
+  try
+  {
+    // Create a vector of inputs.
+    inputs.push_back(tensor);
+  }
+  catch (const c10::Error &e)
+  {
+    std::cerr << "error loading the inputs\n";
+    return NULL;
+  }
+  // Execute the model and turn its output into a tensor.
+  torch::NoGradGuard no_grad;
+  c10::impl::GenericDict output = module.forward(inputs).toGenericDict();
+  at::Tensor pred_d = output.at("pred_d").toTensor();
+  pred_d = pred_d.squeeze();
+  pred_d = pred_d * 256.0;
+  pred_d = (pred_d / pred_d.max()) * 255;
+  pred_d = pred_d.toType(torch::kU8);
+  
+  int width = pred_d.sizes()[0];
+  int height = pred_d.sizes()[1];
 
-	PyObject *python_initialize_function = PyObject_GetAttrString(python_module, (char *) "initialize");
+  std::cout << pred_d << std::endl;
 
-	if (PyErr_Occurred())
-		        PyErr_Print();
-	if (python_initialize_function == NULL || !PyCallable_Check(python_initialize_function))
-	{
-		Py_Finalize();
-		exit (printf("Error: Could not load the python_initialize_function.\n"));
-	}
-	PyObject *python_arguments = Py_BuildValue("()");
+  for(unsigned int i = 0; i < cut_param; i++)
+  {
+    for(unsigned int j = 0; j < width; j++)
+    {
+      pred_d[i][j] = 1000;
+    }
+  }
+  for(unsigned int i = height - down_param; i < height; i++)
+  {
+    for(unsigned int j = 0; j < width; j++)  
+    {
+      pred_d[i][j] = 0;
+    }
+  }
 
-	if (PyErr_Occurred())
-		        PyErr_Print();
+  
+  cv::Mat color_mat;
+  cv::Mat output_mat(cv::Size{ height, width }, CV_8U, pred_d.data_ptr<uchar>());
+  applyColorMap(output_mat, color_mat, cv::COLORMAP_RAINBOW);
+  
+  cv::namedWindow("GLPDepth"); // Create a window
+  cv::imshow("GLPDepth", color_mat); // Show our image inside the created window.
+  cv::waitKey(0);
 
-	PyObject_CallObject(python_initialize_function, python_arguments);
-
-	if (PyErr_Occurred())
-		        PyErr_Print();
-
-	// Py_DECREF(python_arguments);
-	// Py_DECREF(python_initialize_function);
-
-	python_libglpdepth_process_image_function = PyObject_GetAttrString(python_module, (char *) "glp_process_image");
-	
-	if (PyErr_Occurred())
-		        PyErr_Print();
-
-	if (python_libglpdepth_process_image_function == NULL || !PyCallable_Check(python_libglpdepth_process_image_function))
-	{
-		// Py_DECREF(python_module);
-		Py_Finalize();
-		exit (printf("Error: Could not load the glp_process_image.\n"));
-	}
-
-	if (PyErr_Occurred())
-		        PyErr_Print();
-
-	printf("Success: Loaded GLPDepth\n");
-
+  return pred_d.data_ptr<uchar>();
 }
-
-unsigned char*
-libglpdepth_process_image(int width, int height, unsigned char *image, int cut_param, int down_param)
-{
-	// printf("libglpdepth_process_image\n");
-	npy_intp dims[3] = {height, width, 3};
-	PyObject* numpyArray = PyArray_SimpleNewFromData(3, dims, NPY_UBYTE, image);
-
-	int time[1];
-	time[0] = cut_param;
-	npy_intp dimcut[1] = {1};
-	
-	PyObject* numpyCutParam = PyArray_SimpleNewFromData(1, dimcut, NPY_INT, &time[0]);
-	int down[1];
-	down[0] = down_param;
-	PyObject* numpyDownParam = PyArray_SimpleNewFromData(1, dimcut, NPY_INT, &down[0]);
-
-	if (PyErr_Occurred())
-		        PyErr_Print();
-	PyArrayObject* python_result_array = (PyArrayObject*) PyObject_CallFunctionObjArgs(python_libglpdepth_process_image_function, numpyArray, numpyCutParam, numpyDownParam, NULL);
-
-	if (PyErr_Occurred())
-	        PyErr_Print();
-
-	unsigned char*result_array = (unsigned char*)PyArray_DATA(python_result_array);
-
-
-	if (PyErr_Occurred())
-        PyErr_Print();
-
-	Py_DECREF(numpyArray);
-	Py_DECREF(numpyCutParam);
-
-	return result_array;
-}
-
