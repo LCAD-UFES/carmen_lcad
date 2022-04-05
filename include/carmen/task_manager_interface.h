ac0a8c01af8ac1e31fe439f228f9781ef115aa73
albertodesouza <alberto@lcad.inf.ufes.br>
2022-04-04 23:31:17 -0300

    buttons and leds funcionando para a PoC Portocel

--- include/carmen/task_manager_interface.h
+++ include/carmen/task_manager_interface.h
@@ -8,6 +8,11 @@ extern "C" {
 #include <carmen/carmen.h>
 #include "task_manager_messages.h"
 
+
+void
+carmen_task_manager_subscribe_state_message(carmen_task_manager_state_message *msg,
+				     carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+
 void
 carmen_task_manager_subscribe_set_collision_geometry_message(carmen_task_manager_set_collision_geometry_message *msg,
 				     carmen_handler_t handler, carmen_subscribe_t subscribe_how);
@@ -19,6 +24,11 @@ carmen_task_manager_subscribe_desired_engage_state_message(carmen_task_manager_d
 void
 carmen_task_manager_subscribe_set_semi_trailer_type_and_beta_message(carmen_task_manager_set_semi_trailer_type_and_beta_message *msg,
 				     carmen_handler_t handler, carmen_subscribe_t subscribe_how);
+
+
+void
+carmen_task_manager_publish_state_message(carmen_task_manager_mission_level_state_t mission_state, char *mission_filename,
+											carmen_robot_and_trailer_pose_t final_goal, char *info);
 
 void
 carmen_task_manager_publish_set_collision_geometry_message(int geometry, double timestamp);
