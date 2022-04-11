be3418a6171d17462705dca045ad366e1952eb22
vinibc <viniciusbc1@gmail.com>
2022-04-11 15:46:50 -0300

    alteracoes ouster para funcionar com os0 e os1 automatico

--- src/sensors/ouster/ouster_config.h
+++ src/sensors/ouster/ouster_config.h
@@ -15,7 +15,7 @@ enum IntensityType
 // @filipe: these values can be automatically obtained from the sensor. How to make them available to other modules besides the driver?
 const int W = 1024;
 //const int H = ouster::OS1::pixels_per_column;
-const ouster::OS1::lidar_mode mode = ouster::OS1::MODE_1024x10;
+const ouster::OS1::lidar_mode mode = ouster::OS1::MODE_1024x20;
 
 // in degrees
 const double ouster64_altitude_angles[64] = {
@@ -42,8 +42,8 @@ const double ouster64_azimuth_offsets[64] = {
 };
 
 
-// in degrees - Carmen-ini lidar0_vertical
-const double ouster32_altitude_angles[32] = {
+// in degrees - Carmen-ini lidar<ID>_vertical
+const double ousterOS132_205_altitude_angles[32] = {
 	20.6, 19.34, 18.07, 16.8, 15.49, 14.19, 12.84, 11.48,
 	10.13, 8.74, 7.36, 5.97, 4.56, 3.17, 1.77, 0.34,
 	-1.06, -2.47, -3.87, -5.27, -6.66, -8.050000000000001, -9.449999999999999, -10.81,
@@ -51,7 +51,7 @@ const double ouster32_altitude_angles[32] = {
 };
 
 // in degrees
-const double ouster32_azimuth_offsets[32] = {
+const double ousterOS132_205_azimuth_offsets[32] = {
 	-1.40, -1.41, -1.40, -1.39, -1.41, -1.4, -1.41, -1.41,
 	-1.41, -1.42, -1.40, -1.40, -1.41, -1.41, -1.41, -1.42,
 	-1.41, -1.41, -1.40, -1.40, -1.40, -1.40, -1.41, -1.40,
@@ -59,39 +59,56 @@ const double ouster32_azimuth_offsets[32] = {
 };
 
 
-// in degrees - Carmen-ini lidar0_vertical 192.168.1.207
-//const double ousterOS032_altitude_angles[32] = {
-//		43.88, 40.91, 37.95, 35.01, 32.08, 29.17, 26.27, 23.38,
-//		20.52, 17.67, 14.84, 12.01, 9.19, 6.38, 3.57, 0.77,
-//		-2.04, -4.85, -7.65, -10.46, -13.29, -16.12, -18.96, -21.81,
-//		-24.68, -27.56, -30.46, -33.38, -36.32, -39.29, -42.28, -45.32
-//};
+// in degrees - Carmen-ini lidar<ID>_vertical
+const double ousterOS132_208_altitude_angles[32] = {
+	20.6, 19.34, 18.07, 16.8, 15.49, 14.19, 12.84, 11.48,
+	10.13, 8.74, 7.36, 5.97, 4.56, 3.17, 1.77, 0.34,
+	-1.06, -2.47, -3.87, -5.27, -6.66, -8.050000000000001, -9.449999999999999, -10.81,
+	-12.16, -13.51, -14.84, -16.15, -17.45, -18.74, -19.97, -21.22
+};
+
+// in degrees
+const double ousterOS132_208_azimuth_offsets[32] = {
+	-1.40, -1.41, -1.40, -1.39, -1.41, -1.4, -1.41, -1.41,
+	-1.41, -1.42, -1.40, -1.40, -1.41, -1.41, -1.41, -1.42,
+	-1.41, -1.41, -1.40, -1.40, -1.40, -1.40, -1.41, -1.40,
+	-1.40, -1.39, -1.40, -1.40, -1.41, -1.41, -1.38, -1.4
+};
+
+
+// in degrees 192.168.1.207 Carmen-ini lidar<ID>_vertical
+const double ousterOS032_207_altitude_angles[32] = {
+		43.88, 40.91, 37.95, 35.01, 32.08, 29.17, 26.27, 23.38,
+		20.52, 17.67, 14.84, 12.01, 9.19, 6.38, 3.57, 0.77,
+		-2.04, -4.85, -7.65, -10.46, -13.29, -16.12, -18.96, -21.81,
+		-24.68, -27.56, -30.46, -33.38, -36.32, -39.29, -42.28, -45.32
+};
 
 
 // in degrees 192.168.1.207
-//const double ousterOS032_azimuth_offsets[32] = {
-//		-3.28, -3.19, -3.11, -3.04, -2.98, -2.93, -2.88, -2.85,
-//		-2.83, -2.80, -2.79, -2.78, -2.77, -2.77, -2.77, -2.78,
-//		-2.80, -2.82, -2.84, -2.88, -2.90, -2.95, -2.99, -3.04,
-//		-3.11, -3.16, -3.25, -3.35, -3.44, -3.56, -3.7, -3.86
-//};
+const double ousterOS032_207_azimuth_offsets[32] = {
+		-3.28, -3.19, -3.11, -3.04, -2.98, -2.93, -2.88, -2.85,
+		-2.83, -2.80, -2.79, -2.78, -2.77, -2.77, -2.77, -2.78,
+		-2.80, -2.82, -2.84, -2.88, -2.90, -2.95, -2.99, -3.04,
+		-3.11, -3.16, -3.25, -3.35, -3.44, -3.56, -3.7, -3.86
+};
 
-// in degrees - Carmen-ini lidar0_vertical 192.168.1.206
-//const double ousterOS032_altitude_angles[32] = {
-//		43.97, 40.96, 37.99, 35.01, 32.07, 29.15, 26.24, 23.34,
-//		20.47, 17.6, 14.76, 11.93, 9.109999999999999, 6.3, 3.5, 0.7,
-//		-2.1, -4.9, -7.7, -10.51, -13.32, -16.15, -18.99, -21.82,
-//		-24.67, -27.53, -30.42, -33.33, -36.24, -39.19, -42.17, -45.18
-//};
+// in degrees 192.168.1.206 Carmen-ini lidar<ID>_vertical
+const double ousterOS032_206_altitude_angles[32] = {
+		43.97, 40.96, 37.99, 35.01, 32.07, 29.15, 26.24, 23.34,
+		20.47, 17.6, 14.76, 11.93, 9.109999999999999, 6.3, 3.5, 0.7,
+		-2.1, -4.9, -7.7, -10.51, -13.32, -16.15, -18.99, -21.82,
+		-24.67, -27.53, -30.42, -33.33, -36.24, -39.19, -42.17, -45.18
+};
 
 
 // in degrees 192.168.1.206
-//const double ousterOS032_azimuth_offsets[32] = {
-//		-3.19, -3.1, -3.03, -2.97, -2.92, -2.87, -2.85, -2.81,
-//		-2.79, -2.77, -2.76, -2.76, -2.76, -2.77, -2.79, -2.79,
-//		-2.81, -2.83, -2.86, -2.89, -2.93, -2.97, -3.02, -3.08,
-//		-3.14, -3.21, -3.3, -3.41, -3.51, -3.63, -3.79, -3.95
-//};
+const double ousterOS032_206_azimuth_offsets[32] = {
+		-3.19, -3.1, -3.03, -2.97, -2.92, -2.87, -2.85, -2.81,
+		-2.79, -2.77, -2.76, -2.76, -2.76, -2.77, -2.79, -2.79,
+		-2.81, -2.83, -2.86, -2.89, -2.93, -2.97, -3.02, -3.08,
+		-3.14, -3.21, -3.3, -3.41, -3.51, -3.63, -3.79, -3.95
+};
 
 
 const char* 
@@ -102,5 +119,22 @@ intensity_type_to_string(int intensity_type)
     else if (intensity_type == NOISE) return "NOISE";
     else return "INVALID";
 }
+
+
+const double*
+get_OS32_azimuth_offsets(char* ouster_ip)
+{
+	if (strcmp(ouster_ip, "192.168.1.205") == 0)
+		return ousterOS132_205_azimuth_offsets;
+	if (strcmp(ouster_ip, "192.168.1.206") == 0)
+		return ousterOS032_206_azimuth_offsets;
+	if (strcmp(ouster_ip, "192.168.1.207") == 0)
+		return ousterOS032_207_azimuth_offsets;
+	if (strcmp(ouster_ip, "192.168.1.208") == 0)
+		return ousterOS132_208_azimuth_offsets;
+
+	return NULL;
+}
+
 
 #endif
