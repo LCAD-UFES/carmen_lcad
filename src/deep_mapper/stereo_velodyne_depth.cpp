dea12e4de47d72c7fdd70cdde6ad0645ca71a37c
marcosthiago <marcosthiago@gmail.com>
2022-05-31 18:53:51 -0300

    process deepmapper

--- src/deep_mapper/stereo_velodyne_depth.cpp
+++ src/deep_mapper/stereo_velodyne_depth.cpp
@@ -3,7 +3,6 @@
 #include <carmen/libadabins.h>
 #include <carmen/libdpt.h>
 #include <carmen/libglpdepth.h>
-#include <carmen/libnewcrfs.h>
 #include <string.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -14,6 +13,10 @@
 #include <carmen/camera_drivers_interface.h>
 #include <carmen/param_interface.h>
 #include <carmen/param_messages.h>
+
+#include <carmen/velodyne_camera_calibration.h>
+#include <fstream>
+#include <tf.h>
 
 using namespace std;
 using namespace cv;
@@ -26,14 +29,9 @@ static double horizontal_camera_angle;
 static double horizontal_start_angle;
 static double vertical_camera_angle;
 static double range_multiplier_factor;
-static int vertical_roi_ini;
-static int vertical_roi_end;
 static int vertical_top_cut;
 static int vertical_down_cut;
 static int lidar_num;
-
-static int horizontal_roi_ini;
-static int horizontal_roi_end;
 
 static int camera_width = 0;
 static int camera_height = 0;
@@ -51,8 +49,85 @@ static Mat MapX, MapY;
 static char **modules;
 static int num_modules;
 
-char *neural_network = (char *)"adabins";
+char *neural_network = (char *)"glpdepth";
 unsigned char *depth_pred;
+const static double sorted_vertical_angles[32] =
+	{
+		-30.67, -29.33, -28.0, -26.67, -25.33, -24.0, -22.67, -21.33, -20.0,
+		-18.67, -17.33, -16.0, -14.67, -13.33, -12.0, -10.67, -9.3299999, -8.0,
+		-6.6700001, -5.3299999, -4.0, -2.6700001, -1.33, 0.0, 1.33, 2.6700001, 4.0,
+		5.3299999, 6.6700001, 8.0, 9.3299999, 10.67};
+const static double sorted_lidar8_vertical_angles[480] = {-25.5000, -25.3938, -25.2875, -25.1813, -25.0750, -24.9688, -24.8625, -24.7563, -24.6500, -24.5438, -24.4375, -24.3313, -24.2250, -24.1188, -24.0125, -23.9063, -23.8000, -23.6938, -23.5875, -23.4813, -23.3750, -23.2688, -23.1625, -23.0563, -22.9500, -22.8438, -22.7375, -22.6313, -22.5250, -22.4188, -22.3125, -22.2063, -22.1000, -21.9938, -21.8875, -21.7813, -21.6750, -21.5688, -21.4625, -21.3563, -21.2500, -21.1438, -21.0375, -20.9313, -20.8250, -20.7188, -20.6125, -20.5063, -20.4000, -20.2938, -20.1875, -20.0813, -19.9750, -19.8688, -19.7625, -19.6563, -19.5500, -19.4438, -19.3375, -19.2313, -19.1250, -19.0188, -18.9125, -18.8063, -18.7000, -18.5938, -18.4875, -18.3813, -18.2750, -18.1688, -18.0625, -17.9563, -17.8500, -17.7438, -17.6375, -17.5313, -17.4250, -17.3188, -17.2125, -17.1063, -17.0000, -16.8938, -16.7875, -16.6813, -16.5750, -16.4688, -16.3625, -16.2563, -16.1500, -16.0438, -15.9375, -15.8313, -15.7250, -15.6188, -15.5125, -15.4063, -15.3000, -15.1938, -15.0875, -14.9813, -14.8750, -14.7688, -14.6625, -14.5563, -14.4500, -14.3438, -14.2375, -14.1313, -14.0250, -13.9188, -13.8125, -13.7063, -13.6000, -13.4938, -13.3875, -13.2813, -13.1750, -13.0688, -12.9625, -12.8563, -12.7500, -12.6438, -12.5375, -12.4313, -12.3250, -12.2188, -12.1125, -12.0063, -11.9000, -11.7938, -11.6875, -11.5813, -11.4750, -11.3688, -11.2625, -11.1563, -11.0500, -10.9438, -10.8375, -10.7313, -10.6250, -10.5188, -10.4125, -10.3063, -10.2000, -10.0938, -9.9875, -9.8813, -9.7750, -9.6688, -9.5625, -9.4563, -9.3500, -9.2438, -9.1375, -9.0313, -8.9250, -8.8188, -8.7125, -8.6063, -8.5000, -8.3938, -8.2875, -8.1813, -8.0750, -7.9688, -7.8625, -7.7563, -7.6500, -7.5438, -7.4375, -7.3313, -7.2250, -7.1188, -7.0125, -6.9063, -6.8000, -6.6938, -6.5875, -6.4813, -6.3750, -6.2688, -6.1625, -6.0563, -5.9500, -5.8438, -5.7375, -5.6313, -5.5250, -5.4188, -5.3125, -5.2063, -5.1000, -4.9938, -4.8875, -4.7813, -4.6750, -4.5688, -4.4625, -4.3563, -4.2500, -4.1438, -4.0375, -3.9313, -3.8250, -3.7188, -3.6125, -3.5063, -3.4000, -3.2938, -3.1875, -3.0813, -2.9750, -2.8688, -2.7625, -2.6563, -2.5500, -2.4438, -2.3375, -2.2313, -2.1250, -2.0188, -1.9125, -1.8063, -1.7000, -1.5938, -1.4875, -1.3813, -1.2750, -1.1688, -1.0625, -0.9563, -0.8500, -0.7438, -0.6375, -0.5313, -0.4250, -0.3188, -0.2125, -0.1063, -0.0000, 0.1062, 0.2125, 0.3187, 0.4250, 0.5312, 0.6375, 0.7437, 0.8500, 0.9562, 1.0625, 1.1687, 1.2750, 1.3812, 1.4875, 1.5937, 1.7000, 1.8062, 1.9125, 2.0187, 2.1250, 2.2312, 2.3375, 2.4437, 2.5500, 2.6562, 2.7625, 2.8687, 2.9750, 3.0812, 3.1875, 3.2937, 3.4000, 3.5062, 3.6125, 3.7187, 3.8250, 3.9312, 4.0375, 4.1437, 4.2500, 4.3562, 4.4625, 4.5687, 4.6750, 4.7812, 4.8875, 4.9937, 5.1000, 5.2062, 5.3125, 5.4187, 5.5250, 5.6312, 5.7375, 5.8437, 5.9500, 6.0562, 6.1625, 6.2687, 6.3750, 6.4812, 6.5875, 6.6937, 6.8000, 6.9062, 7.0125, 7.1187, 7.2250, 7.3312, 7.4375, 7.5437, 7.6500, 7.7562, 7.8625, 7.9687, 8.0750, 8.1812, 8.2875, 8.3937, 8.5000, 8.6062, 8.7125, 8.8187, 8.9250, 9.0312, 9.1375, 9.2437, 9.3500, 9.4562, 9.5625, 9.6687, 9.7750, 9.8812, 9.9875, 10.0937, 10.2000, 10.3062, 10.4125, 10.5187, 10.6250, 10.7312, 10.8375, 10.9437, 11.0500, 11.1562, 11.2625, 11.3687, 11.4750, 11.5812, 11.6875, 11.7937, 11.9000, 12.0062, 12.1125, 12.2187, 12.3250, 12.4312, 12.5375, 12.6437, 12.7500, 12.8562, 12.9625, 13.0687, 13.1750, 13.2812, 13.3875, 13.4937, 13.6000, 13.7062, 13.8125, 13.9187, 14.0250, 14.1312, 14.2375, 14.3437, 14.4500, 14.5562, 14.6625, 14.7687, 14.8750, 14.9812, 15.0875, 15.1937, 15.3000, 15.4062, 15.5125, 15.6187, 15.7250, 15.8312, 15.9375, 16.0437, 16.1500, 16.2562, 16.3625, 16.4687, 16.5750, 16.6812, 16.7875, 16.8937, 17.0000, 17.1062, 17.2125, 17.3187, 17.4250, 17.5312, 17.6375, 17.7437, 17.8500, 17.9562, 18.0625, 18.1687, 18.2750, 18.3812, 18.4875, 18.5937, 18.7000, 18.8062, 18.9125, 19.0187, 19.1250, 19.2312, 19.3375, 19.4437, 19.5500, 19.6562, 19.7625, 19.8687, 19.9750, 20.0812, 20.1875, 20.2937, 20.4000, 20.5062, 20.6125, 20.7187, 20.8250, 20.9312, 21.0375, 21.1437, 21.2500, 21.3562, 21.4625, 21.5687, 21.6750, 21.7812, 21.8875, 21.9937, 22.1000, 22.2062, 22.3125, 22.4187, 22.5250, 22.6312, 22.7375, 22.8437, 22.9500, 23.0562, 23.1625, 23.2687, 23.3750, 23.4812, 23.5875, 23.6937, 23.8000, 23.9062, 24.0125, 24.1187, 24.2250, 24.3312, 24.4375, 24.5437, 24.6500, 24.7562, 24.8625, 24.9687, 25.0750, 25.1812, 25.2875, 25.3937};
+inline double round(double val)
+{
+	if (val < 0)
+		return ceil(val - 0.5);
+	return floor(val + 0.5);
+}
+
+void write_pointcloud_txt(carmen_velodyne_partial_scan_message *velodyne_message, char *dove)
+{
+	int i, j, line;
+	int horizontal_resolution = velodyne_message->number_of_32_laser_shots;
+	int vertical_resolution = 32;
+
+	double timestamp = velodyne_message->timestamp;
+
+	ofstream point_cloud_file;
+	char *pPath = getenv("CARMEN_HOME");
+	point_cloud_file.open(std::string(pPath) + "/src/deep_mapper/samples/" + std::to_string(timestamp) + "_" + std::string(dove) + ".pcd");
+	point_cloud_file << "#horizontal_resolution\t" << horizontal_resolution << "\n";
+	for (j = horizontal_resolution - 1, line = 0; j > 0; j--)
+	{
+		if (velodyne_message->partial_scan[j].angle >= -47 && velodyne_message->partial_scan[j].angle <= 47)
+		{
+			for (i = vertical_resolution - 1; i > 0; i--, line++)
+			{
+				double horizontal_angle = carmen_normalize_theta(-velodyne_message->partial_scan[j].angle);
+				double vertical_angle = carmen_normalize_theta(carmen_degrees_to_radians(sorted_vertical_angles[i]));
+				
+				double range = (((double)velodyne_message->partial_scan[i].distance[j]) / 500.0);
+				tf::Point point = spherical_to_cartesian(horizontal_angle, vertical_angle, range);
+				double p_x = round(point.x() * 100.0) / 100.0;
+				double p_y = round(point.y() * 100.0) / 100.0;
+				double p_z = round(point.z() * 100.0) / 100.0;
+				point_cloud_file << p_x << "\t" << p_y << "\t" << p_z << "\t" << range << "\t" << velodyne_message->partial_scan[j].angle << "\t" << sorted_vertical_angles[i] << "\n";
+			}
+			point_cloud_file << "# New slice\n";
+		}
+	}
+	point_cloud_file.close();
+}
+void write_pointcloud_variable_txt(carmen_velodyne_variable_scan_message *velodyne_message, char *dove)
+{
+	int i, j, line;
+	int horizontal_resolution = 640;
+	int vertical_resolution = 480;
+
+	double timestamp = velodyne_message->timestamp;
+
+	ofstream point_cloud_file;
+	char *pPath = getenv("CARMEN_HOME");
+	point_cloud_file.open(std::string(pPath) + "/src/deep_mapper/samples/" + std::to_string(timestamp) + "_" + std::string(dove) + ".pcd");
+
+	for (j = horizontal_resolution - 1, line = 0; j > 0; j--)
+	{
+		for (i = vertical_resolution - 1; i > 0; i--, line++)
+		{
+			double horizontal_angle = carmen_normalize_theta(-velodyne_message->partial_scan[j].angle) / 180.0;
+			double range = velodyne_message->partial_scan[j].distance[i]/480.0;
+			double vertical_angle = carmen_normalize_theta(carmen_degrees_to_radians(sorted_lidar8_vertical_angles[i]));
+			tf::Point point = spherical_to_cartesian(horizontal_angle, vertical_angle, range);
+			double p_x = round(point.x() * 100.0) / 100.0;
+			double p_y = round(point.y() * 100.0) / 100.0;
+			double p_z = round(point.z() * 100.0) / 100.0;
+			
+			point_cloud_file << p_x << "\t" << p_y << "\t" << p_z << "\t" << range << "\t" << velodyne_message->partial_scan[j].angle << "\t" << sorted_lidar8_vertical_angles[i] << "\n";
+		}
+		point_cloud_file << "# New slice\n";
+	}
+	point_cloud_file.close();
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -64,29 +139,23 @@ unsigned char *depth_pred;
 
 void convert_depth_to_velodyne_beams(unsigned char *depth, int vertical_resolution,
 									 int horizontal_resolution, carmen_velodyne_shot *stereo_velodyne_scan,
-									 double range_max, int vertical_roi_ini, int vertical_roi_end, int horizontal_roi_ini, int horizontal_roi_end, unsigned char *image)
+									 double range_max, unsigned char *image)
 {
-	int i, j;
-	double inc_vertical, inc_horizontal, x, y;
-	int width = horizontal_roi_end - horizontal_roi_ini;
-	int height = vertical_roi_end - vertical_roi_ini;
+	int i, j, x, y;
 
-	inc_vertical = ((double)height / (double)vertical_resolution);
-	inc_horizontal = ((double)width / (double)horizontal_resolution);
 	unsigned short int *points = (unsigned short int *)depth;
 
-	double stereo_stride_x = 1.0;
-	double stereo_stride_y = 1.0;
-
 	double angle = horizontal_start_angle; // Horizontal angle
-	double delta_angle = horizontal_camera_angle / (double)width;
-	for (x = horizontal_roi_ini, j = horizontal_resolution / stereo_stride_x - 1; x < horizontal_roi_end; x += stereo_stride_x * inc_horizontal, j--)
+	double delta_angle = horizontal_camera_angle / (double)horizontal_resolution;
+	for (x = 0, j = horizontal_resolution - 1; x < horizontal_resolution; x += 1, j--)
 	{
 		stereo_velodyne_scan[j].angle = angle;
 		angle += delta_angle;
-		for (y = vertical_roi_ini, i = vertical_resolution / stereo_stride_y - 1; y < vertical_roi_end; y += stereo_stride_y * inc_vertical, i--)
+		for (y = 0, i = vertical_resolution - 1; y < vertical_resolution; y += 1, i--)
 		{
-			double horizontal_angle = stereo_velodyne_scan[j].angle * M_PI / 180.0;
+			//double horizontal_angle = carmen_normalize_theta(-stereo_velodyne_scan[j].angle) / 180.0;
+			// double horizontal_angle = stereo_velodyne_scan[j].angle * M_PI / 180.0;
+			double horizontal_angle = carmen_normalize_theta(-stereo_velodyne_scan[j].angle)/180.0;
 			double range = points[(int)(y * (double)horizontal_resolution + x)] * (2 - cos(abs(horizontal_angle)));
 			range = range > range_max ? 0.0 : range;
 			stereo_velodyne_scan[j].distance[i] = (unsigned short)(range * range_multiplier_factor);
@@ -102,6 +171,11 @@ void convert_depth_to_velodyne_beams(unsigned char *depth, int vertical_resoluti
 //                                                                                           //
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
+void velodyne_partial_scan_message_handler(carmen_velodyne_partial_scan_message *velodyne_message)
+{
+	//write_pointcloud_txt(velodyne_message, (char *)"velodyne");
+}
+
 void bumblebee_basic_handler(carmen_bumblebee_basic_stereoimage_message *stereo_image)
 {
 	// printf("bumblebee_basic_handler\n");
@@ -116,25 +190,25 @@ void bumblebee_basic_handler(carmen_bumblebee_basic_stereoimage_message *stereo_
 		depth_pred = libdpt_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
 	if (!strcmp(neural_network, "glpdepth"))
 		depth_pred = libglpdepth_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
-	if (!strcmp(neural_network, "newcrfs"))
-		depth_pred = libnewcrfs_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
 
 	// cv::Rect myROI(0, 200, stereo_image->width, stereo_image->height - 200);
 	// open_cv_image = open_cv_image(myROI);
 	cv::Mat imgdepth = cv::Mat(open_cv_image.rows, open_cv_image.cols, CV_16U, depth_pred);
 
-	convert_depth_to_velodyne_beams(depth_pred, vertical_resolution, horizontal_resolution, scan, range_max, vertical_roi_ini,
-									vertical_roi_end, horizontal_roi_ini, horizontal_roi_end, image_gray);
+	convert_depth_to_velodyne_beams(depth_pred, vertical_resolution,
+									horizontal_resolution, scan,
+									range_max, image_gray);
 
 	velodyne_partial_scan.partial_scan = scan;
-	velodyne_partial_scan.number_of_shots = horizontal_roi_end - horizontal_roi_ini;
+	velodyne_partial_scan.number_of_shots = horizontal_resolution;
 	velodyne_partial_scan.host = carmen_get_host();
 	velodyne_partial_scan.timestamp = stereo_image->timestamp;
+	//write_pointcloud_variable_txt(&velodyne_partial_scan, (char *)"lidar8");
 
 	carmen_velodyne_publish_variable_scan_message(&velodyne_partial_scan, lidar_num);
 
 	// cv::imshow("Bumblebee Image", open_cv_image);
-	cv::imshow(neural_network, imgdepth * 256);
+	cv::imshow(neural_network, imgdepth * 255);
 	waitKey(1);
 }
 
@@ -155,24 +229,25 @@ void image_handler(camera_message *msg)
 		depth_pred = libdpt_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
 	if (!strcmp(neural_network, "glpdepth"))
 		depth_pred = libglpdepth_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
-	if (!strcmp(neural_network, "newcrfs"))
-		depth_pred = libnewcrfs_process_image(open_cv_image.cols, open_cv_image.rows, open_cv_image.data, vertical_top_cut, vertical_down_cut);
+
 	// img(cv::Rect(xMin,yMin,xMax-xMin,yMax-yMin)).copyTo(croppedImg);
 	// cv::Rect myROI(0, camera_height - vertical_resolution, stereo_image->width, camera_height - (camera_height - vertical_resolution));
 	// open_cv_image = open_cv_image(myROI);
 	cv::Mat imgdepth = cv::Mat(open_cv_image.rows, open_cv_image.cols, CV_16U, depth_pred);
 
-	convert_depth_to_velodyne_beams(depth_pred, vertical_resolution, horizontal_resolution, scan, range_max, vertical_roi_ini,
-									vertical_roi_end, horizontal_roi_ini, horizontal_roi_end, image_gray);
+	convert_depth_to_velodyne_beams(depth_pred, vertical_resolution,
+									horizontal_resolution, scan,
+									range_max, image_gray);
 
 	velodyne_partial_scan.partial_scan = scan;
-	velodyne_partial_scan.number_of_shots = horizontal_roi_end - horizontal_roi_ini;
+	velodyne_partial_scan.number_of_shots = horizontal_resolution;
 	velodyne_partial_scan.host = carmen_get_host();
 	velodyne_partial_scan.timestamp = msg->timestamp;
+	//write_pointcloud_variable_txt(&velodyne_partial_scan, (char *)"lidar8");
 	carmen_velodyne_publish_variable_scan_message(&velodyne_partial_scan, lidar_num);
 
 	// cv::imshow("Camera Driver Image", open_cv_image);
-	cv::imshow(neural_network, imgdepth * 256);
+	cv::imshow(neural_network, imgdepth * 500);
 	waitKey(1);
 }
 
@@ -383,10 +458,6 @@ int read_parameters(int argc, char **argv)
 		{(char *)stereo_string, (char *)"vertical_resolution", CARMEN_PARAM_INT, &vertical_resolution, 1, NULL},
 		{(char *)stereo_string, (char *)"horizontal_resolution", CARMEN_PARAM_INT, &horizontal_resolution, 1, NULL},
 		{(char *)stereo_string, (char *)"range_max", CARMEN_PARAM_DOUBLE, &range_max, 1, NULL},
-		{(char *)stereo_string, (char *)"vertical_roi_ini", CARMEN_PARAM_INT, &vertical_roi_ini, 1, NULL},
-		{(char *)stereo_string, (char *)"vertical_roi_end", CARMEN_PARAM_INT, &vertical_roi_end, 1, NULL},
-		{(char *)stereo_string, (char *)"horizontal_roi_ini", CARMEN_PARAM_INT, &horizontal_roi_ini, 1, NULL},
-		{(char *)stereo_string, (char *)"horizontal_roi_end", CARMEN_PARAM_INT, &horizontal_roi_end, 1, NULL},
 		{(char *)stereo_string, (char *)"width", CARMEN_PARAM_INT, &camera_width, 1, NULL},
 		{(char *)stereo_string, (char *)"height", CARMEN_PARAM_INT, &camera_height, 1, NULL},
 		{(char *)stereo_string, (char *)"horizontal_camera_angle", CARMEN_PARAM_DOUBLE, &horizontal_camera_angle, 1, NULL},
@@ -406,7 +477,7 @@ int read_parameters(int argc, char **argv)
 		{(char *)camera_string, (char *)"p1", CARMEN_PARAM_DOUBLE, &p1, 0, NULL},
 		{(char *)camera_string, (char *)"p2", CARMEN_PARAM_DOUBLE, &p2, 0, NULL}};
 
-	if (vertical_resolution > vertical_roi_end - vertical_roi_ini)
+	if (vertical_resolution > vertical_resolution - 0)
 		carmen_die("The stereo_velodyne_vertical_resolution is bigger than stereo point cloud height");
 
 	num_items = sizeof(param_list) / sizeof(param_list[0]);
@@ -458,10 +529,6 @@ int main(int argc, char **argv)
 	{
 		initialize_python_context_glpdepth();
 	}
-	if (!strcmp(neural_network, "newcrfs"))
-	{
-		initialize_python_context_newcrfs();
-	}
 
 	init_stereo_velodyne();
 
@@ -474,6 +541,9 @@ int main(int argc, char **argv)
 	{
 		camera_drivers_subscribe_message(camera, NULL, (carmen_handler_t)image_handler, CARMEN_SUBSCRIBE_LATEST);
 	}
+	carmen_velodyne_subscribe_partial_scan_message(NULL,
+												   (carmen_handler_t)velodyne_partial_scan_message_handler,
+												   CARMEN_SUBSCRIBE_LATEST);
 
 	carmen_ipc_dispatch();
 
