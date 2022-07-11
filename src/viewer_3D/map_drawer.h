b3d6437ffef08434084a108f9e6bf620c3f3e00a
braiancalot <calot665@hotmail.com>
2022-07-11 11:46:46 -0300

    new maps modes, printscreen shortcut and map view optimization

--- src/viewer_3D/map_drawer.h
+++ src/viewer_3D/map_drawer.h
@@ -21,9 +21,13 @@ typedef struct map_drawer map_drawer;
 
 map_drawer* create_map_drawer(int argc, char** argv);
 void destroy_map_drawer(map_drawer* m_drawer);
-void add_map_message(map_drawer* m_drawer, carmen_map_t map,
+void add_map_message(map_drawer* m_drawer, carmen_mapper_map_message *message,
 		carmen_vector_3D_t offset, carmen_pose_3D_t car_fused_pose, double map_zoom);
-void add_map_level1_message(map_drawer* m_drawer, carmen_mapper_map_message *message,
+//void add_map_level1_message(map_drawer* m_drawer, carmen_mapper_map_message *message,
+//		carmen_vector_3D_t offset, carmen_pose_3D_t car_fused_pose, double map_zoom);
+void add_compact_cost_map_message(map_drawer* m_drawer, carmen_map_t map,
+		carmen_vector_3D_t offset, carmen_pose_3D_t car_fused_pose, double map_zoom);
+void add_localize_map_message(map_drawer* m_drawer, carmen_map_server_localize_map_message *message,
 		carmen_vector_3D_t offset, carmen_pose_3D_t car_fused_pose, double map_zoom);
 void draw_map(map_drawer *m_drawer, carmen_vector_3D_t offset, carmen_pose_3D_t car_fused_pose, double map_zoom);
 
