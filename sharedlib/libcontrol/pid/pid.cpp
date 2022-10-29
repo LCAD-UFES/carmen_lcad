f476e94716bdec6f0fd41400b4b875d2491f1a3b
albertodesouza <@>
2022-10-28 21:00:00 -0300

    Astru2 pronto para apresentacao!

--- sharedlib/libcontrol/pid/pid.cpp
+++ sharedlib/libcontrol/pid/pid.cpp
@@ -54,9 +54,9 @@ static double g_brake_gap;
 static double g_max_brake_effort;
 static double g_maximum_steering_command_rate;
 
-static double g_throttle_gap = 0.0;
+static double g_throttle_gap = 20.0;
 
-static double g_fuzzy_factor = 1.0;
+static double g_fuzzy_factor = 0.5;
 
 static int robot_model_id = 0;
 
