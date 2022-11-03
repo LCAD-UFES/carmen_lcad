8ab3d9775eeff87ffdbc5b876355fd373c9e7bbd
Pedro <pepeaze@gmail.com>
2022-11-03 14:33:05 -0300

    .

--- src/utilities/publish_pose/util_publish_dummy_gps.cpp
+++ src/utilities/publish_pose/util_publish_dummy_gps.cpp
@@ -79,6 +79,7 @@ publish_carmen_gps_gpgga_message(int gps_number)
 
 		err = IPC_publishData (CARMEN_GPS_GPGGA_MESSAGE_NAME, carmen_gpgga_ptr);
 		carmen_test_ipc(err, "Could not publish", CARMEN_GPS_GPGGA_MESSAGE_NAME);
+		printf("mensagem gga publicada!\n");
 
 		return (1);
 	}
@@ -97,6 +98,7 @@ publish_carmen_gps_gphdt_message(int gps_number)
 		carmen_gphdt_ptr->nr = gps_number;
 		err = IPC_publishData (CARMEN_GPS_GPHDT_MESSAGE_NAME, carmen_gphdt_ptr);
 		carmen_test_ipc(err, "Could not publish", CARMEN_GPS_GPHDT_MESSAGE_NAME);
+		printf("mensagem hdt publicada!\n");
 	}
 }
 
@@ -146,30 +148,33 @@ char *carmen_string_to_publish_gps_gpgga_message(char *string,
 
 	current_pos = carmen_next_word(current_pos);
 
-	gps_msg->nr               = CLF_READ_INT(&current_pos);
-	gps_msg->utc              = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->latitude_dm      = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->latitude         = carmen_global_convert_degmin_to_double(gps_msg->latitude_dm);
+	gps_msg->nr               = CLF_READ_INT(&current_pos); fprintf(stderr, " gps number:     %d\n", gps_msg->nr);
+	gps_msg->utc              = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " utc:            %lf\n", gps_msg->utc);
+	gps_msg->latitude_dm      = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " latitude (DM):  %.7lf\n", gps_msg->latitude_dm);
+	gps_msg->latitude         = carmen_global_convert_degmin_to_double(gps_msg->latitude_dm); fprintf(stderr, " latitude:       %.9lf\n", gps_msg->latitude);
 	current_pos = carmen_next_word(current_pos);
-	gps_msg->lat_orient       = CLF_READ_CHAR(&current_pos);
-	gps_msg->longitude_dm     = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->longitude        = carmen_global_convert_degmin_to_double(gps_msg->longitude_dm);
+	gps_msg->lat_orient       = CLF_READ_CHAR(&current_pos); fprintf(stderr, " lat_orient:     %c\n", gps_msg->lat_orient);
+	gps_msg->longitude_dm     = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " longitude (DM): %.7lf\n", gps_msg->longitude_dm);
+	gps_msg->longitude        = carmen_global_convert_degmin_to_double(gps_msg->longitude_dm); fprintf(stderr, " longitude:      %.9lf\n", gps_msg->longitude);
 	current_pos = carmen_next_word(current_pos);
-	gps_msg->long_orient      = CLF_READ_CHAR(&current_pos);
-	gps_msg->gps_quality      = CLF_READ_INT(&current_pos);
-	gps_msg->num_satellites   = CLF_READ_INT(&current_pos);
-	gps_msg->hdop             = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->sea_level        = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->altitude         = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->geo_sea_level    = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->geo_sep          = CLF_READ_DOUBLE(&current_pos);
-	gps_msg->data_age         = CLF_READ_INT(&current_pos);
-	gps_msg->timestamp        = CLF_READ_DOUBLE(&current_pos);
+	gps_msg->long_orient      = CLF_READ_CHAR(&current_pos); fprintf(stderr, " long_orient:    %c\n", gps_msg->long_orient);
+	gps_msg->gps_quality      = CLF_READ_INT(&current_pos); fprintf(stderr, " gps_quality:    %d\n", gps_msg->gps_quality);
+	gps_msg->num_satellites   = CLF_READ_INT(&current_pos); fprintf(stderr, " num_satellites: %d\n", gps_msg->num_satellites);
+	gps_msg->hdop             = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " hdop:           %lf\n", gps_msg->hdop);
+	gps_msg->sea_level        = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " sea_level:      %lf\n", gps_msg->sea_level);
+	gps_msg->altitude         = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " altitude:       %lf\n", gps_msg->altitude);
+	gps_msg->geo_sea_level    = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " geo_sea_level:  %lf\n", gps_msg->geo_sea_level);
+	gps_msg->geo_sep          = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " geo_sep:        %lf\n", gps_msg->geo_sep);
+	gps_msg->data_age         = CLF_READ_INT(&current_pos); fprintf(stderr, " data_age:       %d\n", gps_msg->data_age);
+	gps_msg->timestamp        = CLF_READ_DOUBLE(&current_pos); fprintf(stderr, " timestamp:       %lf\n", gps_msg->timestamp);
+	fflush(stderr);
+	gps_msg->host = (char*) malloc(512*sizeof(char));
 	copy_host_string(&gps_msg->host, &current_pos);
 
 	gps_number = gps_msg->nr;
 
-	carmen_gpgga_ptr->timestamp = carmen_get_time();
+	gps_msg->timestamp = carmen_get_time();
+
 	publish_carmen_gps_gpgga_message(gps_number);
 
 	return current_pos;
@@ -186,11 +191,12 @@ char *carmen_string_to_publish_gps_gphdt_message(char *string,
 	gps_msg->heading          = CLF_READ_DOUBLE(&current_pos);
 	gps_msg->valid            = CLF_READ_INT(&current_pos);
 	gps_msg->timestamp        = CLF_READ_DOUBLE(&current_pos);
+	gps_msg->host = (char*) malloc(512*sizeof(char));
 	copy_host_string(&gps_msg->host, &current_pos);
 
 	if(gps_number == 1)
 	{
-		carmen_gphdt_ptr->timestamp = carmen_gpgga_ptr->timestamp;
+		gps_msg->timestamp = gps_msg->timestamp;
 		publish_carmen_gps_gphdt_message(gps_number);
 	}
 
@@ -206,25 +212,35 @@ main(int argc, char **argv)
 	ipc_initialize_messages();
 	int size = 8000;
 	char line[size];
+	int num_messages_per_second;
 
 	fprintf(stderr, "INFO: ************************\n");
 	fprintf(stderr, "INFO: ********* DUMMY GPS*****\n");
 	fprintf(stderr, "INFO: ************************\n");
 
 	FILE *dummy_gps_file = fopen(argv[1], "r");
+	num_messages_per_second = atoi(argv[2]);
 
-	while (fgets(line, size, dummy_gps_file) != NULL)
+	double sleep_time = (1.0 / (double) num_messages_per_second) * 10e5;
+
+	while (1)
 	{
-		if(strncmp(line, "NMEAGGA", 8) == 0)
+		while (fgets(line, size, dummy_gps_file) != NULL)
 		{
-			carmen_string_to_publish_gps_gpgga_message(line, carmen_gpgga_ptr);
+			if(strncmp(line, "NMEAGGA", 7) == 0)
+			{
+				printf("%s\n", line);
+				carmen_string_to_publish_gps_gpgga_message(line, carmen_gpgga_ptr);
+
+			}
+			else if(strncmp(line, "NMEAHDT", 7) == 0)
+			{
+				carmen_string_to_publish_gps_gphdt_message(line, carmen_gphdt_ptr);
+			}
 
 		}
-		else if(strncmp(line, "NMEAHDT", 8) == 0)
-		{
-			carmen_string_to_publish_gps_gphdt_message(line, carmen_gphdt_ptr);
-		}
-
+		usleep((int) sleep_time);
+		printf("Published GPS!\n");
 	}
 
 	return 0;
