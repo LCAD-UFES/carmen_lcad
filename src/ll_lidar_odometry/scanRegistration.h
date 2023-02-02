999f1d7aa05897cf17e88a6a3a786dd98b1ad679
Lume Robotics S.A <marcelo.bp01@gmail.com>
2023-02-02 17:27:43 -0300

    life longer lidar odometry inicio

---
+++ src/ll_lidar_odometry/scanRegistration.h
@@ -0,0 +1,16 @@
+#ifndef SCAN_REGISTRATION_H
+#define SCAN_REGISTRATION_H
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <vector>
+#include "common.h"
+
+std::vector<pcl::PointCloud<PointType>> laserCloudHandler(pcl::PointCloud<PointType>::Ptr laserCloudMsg);
+
+
+
+#endif
+
