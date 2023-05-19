da2e33bba4a13aaec772e7ae1fe520ecd92d6684
leoscarton <leonardo.scarton29@gmail.com>
2023-05-19 11:30:49 -0300

    Added aruco_vs_velodyne.cpp prototype

---
+++ src/aruco/aruco_vs_velodyne.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "aruco_interface.h"
+#include "aruco_messages.h"
+#include <carmen/carmen.h>
+#include <localize_ackerman_interface.h>
+#include <localize_ackerman_messages.h>
+
+//#include "gnuplot_i.hpp"
+
+std::ofstream arq_aruco("dados_aruco.txt");
+std::ofstream arq_velodyne("dados_velodyne.txt");
+
+double tsi_aruco = 0.0;
+double tsi_velodyne = 0.0;
+bool aruco_start = false;
+bool velodyne_start = false;
+
+void shutdown_module(int signo)
+{
+    if (signo == SIGINT)
+    {
+        carmen_ipc_disconnect();
+
+        printf("Signal %d received, exiting program ...\n", signo);
+        exit(0);
+    }
+}
+
+/*
+void handler(carmen_aruco_message *msg)
+{
+    printf("timestamp: %lf\n", msg->timestamp);
+    // std::cout << "n_poses: "  << msg->n_poses << std::endl;
+    for (size_t i = 0; i < msg->n_poses; i++)
+    {
+        // std::cout << "n_markers_detected: " << msg->poses[i].n_markers_detected << std::endl;
+        if (msg->poses[i].n_markers_detected == 1)
+            std::cout << "id: ";
+        else
+            std::cout << "ids: ";
+        for (size_t j = 0; j < msg->poses[i].n_markers_detected; j++)
+            std::cout << msg->poses[i].ids_markers_detected[j] << " ";
+        std::cout << std::endl;
+        
+        std::cout << "R: " << msg->poses[i].rvec[0] << " " << msg->poses[i].rvec[1] << " " << msg->poses[i].rvec[2] << " " << std::endl;
+        std::cout << "T: "  << msg->poses[i].tvec[0] << " " << msg->poses[i].tvec[1] << " " << msg->poses[i].tvec[2] << " " << std::endl;
+    }
+    std::cout << std::endl;
+}
+*/
+/*
+void grafico_beta()
+{
+    std::ifstream arq_aruco1("dados_aruco.txt");
+    std::ifstream arq_velodyne1("dados_velodyne.txt");
+
+    if(arq_aruco1.is_open() && arq_velodyne1.is_open(()))
+    {
+        std::vector<float> beta_aruco;
+        std::vector<float> beta_velodyne;
+        std::string aux;
+
+        while(std::getline(arq_aruco1, aux))
+        {
+            beta_aruco.push_back(std::stof(aux));
+        }
+
+        while(std::getline(arq_velodyne1, aux))
+        {
+            beta_velodyne.push_back(std::stof(aux));
+        }
+    }
+}
+*/
+
+void handler_aruco(carmen_aruco_message *msg)
+{
+    for (size_t i = 0; i < msg->n_poses; i++)
+    {   
+        std::cout << "Beta - Aruco:" << std::endl;
+        std::cout << "R: " << msg->poses[i].rvec[0] << " " << msg->poses[i].rvec[1] << " " << msg->poses[i].rvec[2] << std::endl;
+        std::cout << std::endl;
+
+        if(arq_aruco.is_open())
+        {
+            if(!aruco_start)
+            {
+                tsi_aruco = msg->timestamp;
+                aruco_start = true;
+            }
+            //arq_aruco << msg->poses[i].rvec[0] << " " << msg->poses[i].rvec[1] << " " << msg->poses[i].rvec[2] << std::endl;
+            arq_aruco << msg->timestamp - tsi_aruco << "," << msg->poses[i].rvec[2] << std::endl;
+        }
+    }
+}
+
+void handler_velodyne(carmen_localize_ackerman_globalpos_message *msg)
+{
+    if(msg->num_trailers == 1)
+    {
+        std::cout << "Beta - Velodyne:" << std::endl;
+        std::cout << msg->trailer_theta[0] << std::endl;
+        std::cout << std::endl;
+
+        if(arq_velodyne.is_open()) // && ((msg->timestamp - timestamp_inicial)>=0) )
+        {
+            if(!velodyne_start)
+            {
+                tsi_velodyne = msg->timestamp;
+                velodyne_start = true;
+            }
+            arq_velodyne << msg->timestamp - tsi_velodyne << "," << msg->trailer_theta[0] << std::endl;
+        }
+    }
+}
+
+
+int main(int argc, char **argv)
+{   
+    carmen_ipc_initialize(argc, argv);
+    carmen_param_check_version(argv[0]);
+
+    signal(SIGINT, shutdown_module);
+
+    aruco_subscribe_message(1, NULL, (carmen_handler_t) handler_aruco, CARMEN_SUBSCRIBE_LATEST);
+    carmen_localize_ackerman_subscribe_globalpos_message(NULL, (carmen_handler_t) handler_velodyne, CARMEN_SUBSCRIBE_LATEST);
+
+    carmen_ipc_dispatch();
+    
+    arq_aruco.close();
+    arq_velodyne.close();
+
+    return 0;
+}
