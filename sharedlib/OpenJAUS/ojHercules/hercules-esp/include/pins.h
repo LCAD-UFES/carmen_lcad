53d83c4dec08057686ea97eed73418fba237da59
Breno-de-Angelo <brenodeangelo@gmai.com>
2023-12-07 15:06:13 -0300

    Projeto hercules-esp

---
+++ sharedlib/OpenJAUS/ojHercules/hercules-esp/include/pins.h
@@ -0,0 +1,6 @@
+#ifndef PINS_H
+#define PINS_H
+
+#define LEFT_MOTOR_PWM 5
+
+#endif // PINS_H
---
+++ sharedlib/OpenJAUS/ojHercules/hercules-esp/main/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pins.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+    printf("LEFT_MOTOR_PWM: %d\n", LEFT_MOTOR_PWM);
+}
