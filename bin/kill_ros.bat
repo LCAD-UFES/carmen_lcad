08afa479ec1bde662a2fd4dfc8926ebf16605ac8
EduardoPia <eduardo.p.abreu@edu.ufes.br>
2024-11-13 11:10:24 -0300

    mudancas process ct de portas abertas e killros

--- bin/kill_ros.bat
+++ bin/kill_ros.bat
@@ -1,3 +1,4 @@
-./carmenkillall.sh hercules/process-yolo-sensorbox3.ini
+./carmenkillall.sh hercules/process-hercules-yolo-sensorbox3.ini
+./carmenkillall.sh process-volta_da_ufes-pid.ini
 killall roslaunch rosmaster  rosout linear_actuator_controller can_to_ros_main vel_to_can_main open_manipulator_teleop_topic
 killall task_manager
