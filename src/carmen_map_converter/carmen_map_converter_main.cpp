f5b4712982d71b357f3214ae463f7f826ad192cc
Breno-de-Angelo <brenodeangelo@gmail.com>
2024-11-28 15:17:25 -0300

    teste compilando

---
+++ src/carmen_map_converter/carmen_map_converter_main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <carmen/carmen.h>
+#include <carmen/global.h>
+#include <carmen/base_ackerman_messages.h>
+#include <carmen/base_ackerman_interface.h>
+#include <prob_measurement_model.h>
+#include <prob_motion_model.h>
+#include <prob_monte_carlo.h>
+#include <prob_transforms.h>
+#include <prob_map.h>
+#include <carmen/fused_odometry_interface.h>
+#include <carmen/rotation_geometry.h>
+#include <carmen/velodyne_interface.h>
+#include <carmen/mapper_interface.h>
+#include <carmen/localize_ackerman_core.h>
+#include <carmen/localize_ackerman_messages.h>
+#include <carmen/localize_ackerman_interface.h>
+#include <carmen/localize_ackerman_velodyne.h>
+#include <carmen/xsens_interface.h>
+#include <carmen/mapper.h>
+#include <carmen/global.h>
+
+
+int oi=0;
+int main(int argc, char **argv)
+{
+	printf("Hello, World!\n");
+	return 0;
+}
