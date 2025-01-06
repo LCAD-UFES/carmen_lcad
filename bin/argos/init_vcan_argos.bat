fdbb6d52b6d298a4fe9ba4dcc57f55b579a5ee74
EduardoPia <dudupiassaroli2@gmail.com>
2025-01-06 14:56:32 -0300

    colocando ojTorc para funcionar com webrtc

--- bin/argos/init_vcan_argos.bat
+++ bin/argos/init_vcan_argos.bat
@@ -2,4 +2,4 @@ sudo ip link add dev vcan0 type vcan
 sudo ip link set up vcan0
 sudo ip link add dev vcan1 type vcan
 sudo ip link set up vcan1
-ifconfig
+
--- bin/kill_ros.bat
+++ bin/kill_ros.bat
@@ -2,3 +2,4 @@
 ./carmenkillall.sh process-volta_da_ufes-pid.ini
 killall roslaunch rosmaster  rosout linear_actuator_controller can_to_ros_main vel_to_can_main open_manipulator_teleop_topic
 killall task_manager
+killall can_to_wirelesscontroller_webrtc _ros2_daemon
