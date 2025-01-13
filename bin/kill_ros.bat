7ffc9e919dff75770258a27727b3d344860912fa
EduardoPia <dudupiassaroli2@gmail.com>
2025-01-13 13:47:00 -0300

    configurando process playback webrtc

--- bin/kill_ros.bat
+++ bin/kill_ros.bat
@@ -1,5 +1,8 @@
 ./carmenkillall.sh hercules/process-hercules-yolo-sensorbox3.ini
+./carmenkillall.sh argos/process-argos-playback-webrtc.ini
 ./carmenkillall.sh process-volta_da_ufes-pid.ini
 killall roslaunch rosmaster  rosout linear_actuator_controller can_to_ros_main vel_to_can_main open_manipulator_teleop_topic
+killall pointcloud_to_laserscan_node robot_state_publisher rviz2 joy_node teleop_node twist_mux async_slam_toolbox_node can_to_wirelesscontroller_webrtc _ros2_daemon 
 killall task_manager
-killall can_to_wirelesscontroller_webrtc _ros2_daemon
+pkill -f ros2
+pkill -f /opt/ros/foxy/lib
