b0cfbc5b7545878f76684769af68d5de34be0ab6
EduardoPia <dudupiassaroli2@gmail.com>
2025-02-12 16:43:25 -0300

    subindo process argos

--- bin/kill_ros.bat
+++ bin/kill_ros.bat
@@ -4,5 +4,6 @@
 killall roslaunch rosmaster  rosout linear_actuator_controller can_to_ros_main vel_to_can_main open_manipulator_teleop_topic
 killall pointcloud_to_laserscan_node robot_state_publisher rviz2 joy_node teleop_node twist_mux async_slam_toolbox_node can_to_wirelesscontroller_webrtc _ros2_daemon 
 killall task_manager go2_driver_node
+killall argos_drivers
 pkill -f ros2
 pkill -f /opt/ros/foxy/lib
