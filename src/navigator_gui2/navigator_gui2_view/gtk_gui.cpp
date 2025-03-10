5f0f0bd0455a19be18849275473cc9bf829c5ff8
albertodesouza <alberto@lcad.inf.ufes.br>
2025-03-10 18:22:47 -0300

    adicao de mecanismos de simulacao do Unitree Go1

--- src/navigator_gui2/navigator_gui2_view/gtk_gui.cpp
+++ src/navigator_gui2/navigator_gui2_view/gtk_gui.cpp
@@ -3405,6 +3405,18 @@ namespace View
 		if (!nav_panel_config->show_true_pos || (simulator_trueposition.map == NULL))
 			return;
 
+		static FILE *poses = NULL;
+
+//		if (nav_panel_config->show_lasers)
+//		{
+//			if (!poses)
+//				poses = fopen("poses.txt", "w");
+//
+//			fprintf(poses, "robot.pose.x %lf, robot.pose.y, %lf, simulator_trueposition.pose.x, %lf, simulator_trueposition.pose.y, %lf\n",
+//					robot.pose.x, robot.pose.y, simulator_trueposition.pose.x, simulator_trueposition.pose.y);
+//			fflush(poses);
+//		}
+
 		draw_robot_shape(the_map_view, &simulator_trueposition, TRUE, &carmen_blue);
 		draw_robot_shape(the_map_view, &simulator_trueposition, FALSE, &carmen_black);
 
